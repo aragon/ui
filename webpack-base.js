0cb049e50dcb7d6f81b9a22583ba9a5a729dbafc
Pierre Bertet <hello@pierre.world>
2017-10-13 16:08:38 +0100

    webpack base: manually extend the passed configuration
    _.merge() was overwriting array items in some cases, better do it
    explicitely.

--- webpack-base.js
+++ webpack-base.js
@@ -1,46 +1,49 @@
 const path = require('path')
-const merge = require('lodash/merge')
 const postCssNext = require('postcss-cssnext')
 
-const baseConf = (webpack, dir) => ({
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          postcss: [postCssNext()],
-        },
-      },
-      {
-        test: /\.(png|jpg|gif|svg|woff|woff2)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    modules: [path.join(dir, 'node_modules')],
-  },
-  resolveLoader: {
-    modules: [path.join(dir, 'node_modules')],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV) },
-    }),
-  ],
-})
+const extend = (o1, o2) => Object.assign({}, o1, o2)
+const append = (a1, a2) => (a1 || []).concat(a2)
 
-module.exports = (webpack, dir, conf) => merge(baseConf(webpack, dir), conf)
+module.exports = (webpack, dir, conf) =>
+  extend(conf, {
+    module: extend(conf.module, {
+      rules: append(conf.module && conf.module.rules, [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            postcss: [postCssNext()],
+          },
+        },
+        {
+          test: /\.(png|jpg|gif|svg|woff|woff2)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: { limit: 8192 },
+            },
+          ],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ]),
+    }),
+    resolve: extend(conf.resolve, {
+      modules: append(conf.resolve && conf.resolve.modules, [
+        path.join(dir, 'node_modules'),
+      ]),
+    }),
+    resolveLoader: extend(conf.resolveLoader, {
+      modules: append(conf.resolveLoader && conf.resolveLoader.modules, [
+        path.join(dir, 'node_modules'),
+      ]),
+    }),
+    plugins: append(conf.plugins, [
+      new webpack.DefinePlugin({
+        'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV) },
+      }),
+    ]),
+  })
