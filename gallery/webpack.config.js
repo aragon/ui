1c0e5d079f318590d65db8c03bb8acfad07e715f
Pierre Bertet <hello@pierre.world>
2017-10-18 12:18:49 +0100

    Gallery: production build (+ bundle inspection)

--- gallery/webpack.config.js
+++ gallery/webpack.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack')
 const path = require('path')
+const CompressionPlugin = require('compression-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VisualizerPlugin = require('webpack-visualizer-plugin')
 const uiWebpackBase = require('../webpack-base')
 
 module.exports = uiWebpackBase(webpack, __dirname, {
@@ -30,9 +33,24 @@ module.exports = uiWebpackBase(webpack, __dirname, {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({ title: 'Aragon UI', favicon: './favicon.svg' }),
-  ],
+  plugins: (() => {
+    let plugins = [
+      new HtmlWebpackPlugin({ title: 'Aragon UI', favicon: './favicon.svg' }),
+    ]
+    if (process.env.INSPECT_BUNDLE) {
+      plugins = plugins.concat([
+        new VisualizerPlugin({ filename: './bundle-stats.html' }),
+      ])
+    }
+    if (process.env.NODE_ENV === 'production') {
+      plugins = plugins.concat([
+        new webpack.optimize.UglifyJsPlugin({ parallel: true }),
+        new CompressionPlugin(),
+        new ExtractTextPlugin('styles.css'),
+      ])
+    }
+    return plugins
+  })(),
   output: {
     publicPath: '/',
     filename: '[name].bundle.js',
