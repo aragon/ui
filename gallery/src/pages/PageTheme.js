d8a372baadb77a36ed0042b80210552e4520753e
Pierre Bertet <hello@pierre.world>
2017-10-26 13:29:10 +0100

    Add a README for /theme

--- gallery/src/pages/PageTheme.js
+++ gallery/src/pages/PageTheme.js
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 import { theme } from '@aragon/ui'
 import Page from 'comps/Page/Page'
 import ColorGroup from 'comps/ColorGroup/ColorGroup'
+import readme from '@aragon/ui/theme/README.md'
 
 const PageButton = ({ title }) => (
-  <Page title={title}>
+  <Page title={title} readme={readme}>
     <ColorGroup name="Aragon UI Theme" colors={theme} />
   </Page>
 )
