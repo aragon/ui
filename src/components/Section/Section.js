93ae4a863dc5b9994cf0513748e5de6a5ba1adfe
Pierre Bertet <hello@pierre.world>
2017-11-16 15:02:36 +0000

    Section: remove the StyledComponent property

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -5,8 +5,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { gridWidth } from '../../shared-styles'
 
-const StyledSection = styled.section``
-const StyledSectionDiv = StyledSection.withComponent('div')
 const StyledContent = styled.div`
   width: 100%;
   margin: 0 auto;
@@ -16,7 +14,6 @@ const StyledContent = styled.div`
 type Props = {
   large: boolean,
   visual: boolean,
-  StyledComponent: ReactComponentStyled<*>, // Allow to override the styled component
 }
 
 const DefaultProps = {
@@ -24,27 +21,16 @@ const DefaultProps = {
   visual: false,
 }
 
-const Section = ({
-  large,
-  visual,
-  StyledComponent, // Allow to override the styled component
-  className,
-  ...props
-}: Props) => {
-  const SectionComp =
-    StyledComponent || (visual ? StyledSectionDiv : StyledSection)
-
-  return (
-    <SectionComp className={className}>
-      <StyledContent large={large}>
-        <div {...props} />
-      </StyledContent>
-    </SectionComp>
+const Section = ({ large, visual, className, ...props }: Props) => {
+  const containerProps = { className }
+  const content = (
+    <StyledContent large={large}>
+      <div {...props} />
+    </StyledContent>
   )
+  if (visual) return <div {...containerProps}>{content}</div>
+  return <section {...containerProps}>{content}</section>
 }
 
 Section.defaultProps = DefaultProps
-
-Section.StyledSection = StyledSection
-Section.StyledSectionDiv = StyledSectionDiv
 export default Section
