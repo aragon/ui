98602f5c6136b5d4c9f83c82f8f8122ae9690382
Pierre Bertet <hello@pierre.world>
2017-11-16 21:06:18 +0000

    Footer: responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 /* @flow */
 import React from 'react'
-import styled from 'styled-components'
-import { colors, theme } from '../..'
+import styled, { css } from 'styled-components'
+import { LayoutGrid, colors, font, themeDark, breakpoint, grid } from '../..'
 
 import logo from './assets/logo.svg'
 import iconTwitter from './assets/twitter.svg'
@@ -9,8 +9,11 @@ import iconMedium from './assets/medium.svg'
 import iconSlack from './assets/slack.svg'
 import iconMail from './assets/mail.svg'
 
+const medium = css => breakpoint('medium', css)
+const large = css => breakpoint('large', css)
+
 const StyledFooter = styled.footer`
-  padding: 90px 20px 110px;
+  padding: 60px 20px 10px;
   font-size: 15px;
   color: grey;
   background: ${colors.Rain['Shark']};
@@ -20,45 +23,53 @@ const StyledFooter = styled.footer`
     return `
       padding-top: 30px;
       padding-bottom: 30px;
-
-      .part-2 {
-        display: flex;
-        align-items: center;
-      }
-      .part-2 li {
-        margin-bottom: 0;
-        margin-left: 20px;
-      }
       .icon {
         padding-left: 25px;
       }
     `
   }};
 
+  .main {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin: 0 auto;
+  }
+
+  .logo {
+    margin-bottom: 60px;
+  }
+  .menus {
+    display: flex;
+  }
+  .menu-1 {
+    margin-right: 35px;
+  }
+  .social-links {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+  }
+  .social-links li {
+    display: flex;
+  }
+  .icon {
+    overflow: hidden;
+    text-indent: -9999px;
+    padding-left: 30px;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+  }
   li {
-    margin: 0 0 10px;
     list-style: none;
+    line-height: 2;
   }
   a {
     text-decoration: none;
   }
   strong a {
-    color: ${theme.accent};
+    color: ${themeDark.accent};
     font-weight: 400;
-  }
-  .main {
-    display: flex;
-    justify-content: space-between;
-  }
-  .part-1 {
-    display: flex;
-    justify-content: space-between;
-    width: 50%;
-  }
-  .icon {
-    padding-left: 30px;
-    background-repeat: no-repeat;
-    background-position: 0 50%;
   }
   .icon.twitter {
     background-image: url(${iconTwitter});
@@ -72,6 +83,66 @@ const StyledFooter = styled.footer`
   .icon.mail {
     background-image: url(${iconMail});
   }
+  .copyright {
+    margin: 35px 0 0;
+    text-align: center;
+    color: ${themeDark.textTertiary};
+    ${font({ size: 'xsmall' })};
+  }
+
+  ${medium(`
+    .all-links {
+      display: flex;
+      justify-content: space-between;
+    }
+    .social-links {
+      display: block;
+      margin-top: 0;
+      margin-left: 120px;
+    }
+    .social-links li {
+      display: block;
+    }
+    .icon {
+      overflow: visible;
+      text-indent: 0;
+      background-position: 0 50%;
+    }
+  `)};
+
+  ${large(`
+    padding-top: 90px;
+    .main {
+      flex-direction: row;
+      max-width: ${grid(12, 11)}px;
+    }
+    .logo {
+      width: ${grid(3, 3)}px;
+      flex-shrink: 0;
+    }
+    .menus {
+      display: flex;
+      width: ${grid(6, 6)}px;
+    }
+    .menu-1 {
+      width: ${grid(2, 2)}px;
+      margin-right: 0;
+    }
+    .menu-2 {
+      width: ${grid(4, 4)}px;
+    }
+    .social-links {
+      width: ${grid(3)}px;
+      margin-left: 0;
+    }
+    li {
+      margin: 0 0 10px;
+      line-height: 1.5;
+    }
+    .copyright {
+      margin: 70px 0 0;
+    }
+  `)};
 `
 
 type Props = {
@@ -85,63 +156,73 @@ const DefaultProps = {
 const Footer = ({ compact }: Props) => (
   <StyledFooter compact={compact}>
     <div className="main">
-      <div className="part-1">
+      <div className="logo">
         <img src={logo} width="158" height="50" alt="Aragon" />
-        {!compact && (
-          <ul>
-            <li>
-              <a href="">Core</a>
-            </li>
-            <li>
-              <a href="">Network</a>
-            </li>
-            <li>
-              <a href="">About</a>
-            </li>
-            <li>
-              <a href="">FAQs</a>
-            </li>
-          </ul>
-        )}
-        {!compact && (
-          <ul>
-            <li>
-              <a href="">Join us</a>
-            </li>
-            <li>
-              <a href="">Press kit</a>
-            </li>
-            <li>
-              <strong>
-                <a href="">Download</a>
-              </strong>
-            </li>
-          </ul>
-        )}
       </div>
-      <ul className="part-2">
-        <li>
-          <a href="" className="icon twitter">
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a href="" className="icon medium">
-            Medium
-          </a>
-        </li>
-        <li>
-          <a href="" className="icon slack">
-            Slack
-          </a>
-        </li>
-        <li>
-          <a href="" className="icon mail">
-            Email
-          </a>
-        </li>
-      </ul>
+      <div className="all-links">
+        {!compact && (
+          <div className="menus">
+            <nav className="menu-1">
+              <ul>
+                <li>
+                  <a href="">Core</a>
+                </li>
+                <li>
+                  <a href="">Foundation</a>
+                </li>
+                <li>
+                  <a href="">About</a>
+                </li>
+                <li>
+                  <a href="">FAQs</a>
+                </li>
+              </ul>
+            </nav>
+            <nav className="menu-2">
+              <ul>
+                <li>
+                  <a href="">Wiki</a>
+                </li>
+                <li>
+                  <a href="">Join us</a>
+                </li>
+                <li>
+                  <a href="">Press kit</a>
+                </li>
+                <li>
+                  <strong>
+                    <a href="">Download</a>
+                  </strong>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        )}
+        <ul className="social-links">
+          <li>
+            <a href="" className="icon twitter">
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon medium">
+              Medium
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon slack">
+              Slack
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon mail">
+              Email
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
+    <p className="copyright">© Copyright 2017 Aragon One.</p>
   </StyledFooter>
 )
 
