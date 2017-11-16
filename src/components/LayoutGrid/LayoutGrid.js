d90be2f994cba64506dd9e3de0115a70ac11ae8f
Pierre Bertet <hello@pierre.world>
2017-11-16 19:14:43 +0000

    Add a LayoutGrid component (useful for development)

---
+++ src/components/LayoutGrid/LayoutGrid.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { grid } from '../..'
+
+const LayoutGrid = () => (
+  <div
+    style={{
+      position: 'fixed',
+      left: '0',
+      right: '0',
+      top: '0',
+      bottom: '0',
+      width: grid(12),
+      margin: '0 auto',
+      display: 'flex',
+    }}
+  >
+    {[...Array(12)].map((v, i, arr) => (
+      <div
+        key={i}
+        style={{ width: grid(1, i < arr.length - 1 ? 1 : 0) + 'px' }}
+      >
+        <div
+          style={{
+            width: grid(1) + 'px',
+            height: '100%',
+            background: 'rgba(184, 184, 184, 0.5)',
+          }}
+        />
+      </div>
+    ))}
+  </div>
+)
+
+export default LayoutGrid
