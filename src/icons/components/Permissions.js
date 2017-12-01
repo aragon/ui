e4a1d841c9fb4ab858a545ce67b52eb0549496f8
Pierre Bertet <hello@pierre.world>
2017-12-01 14:47:09 +0000

    Icons (#30)
    Icons are SVG images converted to components using svgr [1]. It makes them inline SVGs, which allow to change their color using CSS.
    
    To prevent having all the icons in the final build, they are exported individually (prefixed with `Icon`, e.g. `IconHome`).
    
    [1] https://github.com/smooth-code/svgr

---
+++ src/icons/components/Permissions.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Permissions = props => (
+  <svg width={22} height={22} viewBox="0 0 22 22" {...props}>
+    <g fill="none" fillRule="evenodd">
+      <path d="M0 0h22v22H0z" />
+      <g stroke="currentColor">
+        <path
+          d="M11.036 3.143L3.578 6.357V7.43h14.916V6.357l-7.458-3.214zm6.88 12.393H4.071c-.318 0-.577.242-.577.535v1.072h15V16.07c0-.293-.26-.535-.578-.535z"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path d="M5 7v8.034M8 7v8.275M11 7v8.034M14 7v8.275M17 7v8.275" />
+      </g>
+    </g>
+  </svg>
+)
+
+export default Permissions
