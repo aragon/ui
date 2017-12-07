68b45accaf54807a26eef02b771e11af119aec14
Pierre Bertet <hello@pierre.world>
2017-12-07 18:38:30 +0000

    Flow: fix shared-styles

--- src/shared-styles.js
+++ src/shared-styles.js
@@ -2,8 +2,16 @@
 import React from 'react'
 import Media from 'react-media'
 import { css } from 'styled-components'
+import type { Node } from 'react'
 
-const FONT_SIZES = {
+type FontSize = 'xsmall' | 'small' | 'normal' | 'large' | 'xlarge' | 'xxlarge'
+type FontWeight = 'normal' | 'bold' | 'bolder'
+type SpringName = 'slow' | 'normal' | 'fast'
+type BreakPointName = 'medium' | 'large'
+
+const FONT_SIZES: {
+  [FontSize]: string,
+} = {
   xsmall: '12px',
   small: '14px',
   normal: '15px',
@@ -12,7 +20,9 @@ const FONT_SIZES = {
   xxlarge: '22px',
 }
 
-const FONT_WEIGHTS = {
+const FONT_WEIGHTS: {
+  [FontWeight]: string,
+} = {
   normal: '400',
   bold: '600',
   bolder: '800',
@@ -24,7 +34,13 @@ const GRID = {
   columnWidth: 68,
 }
 
-const SPRINGS = {
+const SPRINGS: {
+  [SpringName]: {|
+    stiffness: number,
+    damping: number,
+    precision: number,
+  |},
+} = {
   slow: { stiffness: 150, damping: 18, precision: 0.001 },
   normal: { stiffness: 190, damping: 22, precision: 0.001 },
   fast: { stiffness: 250, damping: 22, precision: 0.001 },
@@ -32,12 +48,18 @@ const SPRINGS = {
 
 // These breakpoints values represent minimum screen sizes.
 // Small screen sizes should be targetted by default (mobile first).
-const BREAKPOINTS = {
+const BREAKPOINTS: { [BreakPointName]: number } = {
   medium: 768,
   large: 1170,
 }
 
-export const font = ({ size = 'normal', weight = 'normal' }) => {
+export const font = ({
+  size = 'normal',
+  weight = 'normal',
+}: {
+  size: FontSize,
+  weight: FontWeight,
+}) => {
   const fontSize = FONT_SIZES[size] || FONT_SIZES.normal
   const fontWeight = FONT_WEIGHTS[weight] || FONT_WEIGHTS.normal
   return `
@@ -47,15 +69,15 @@ export const font = ({ size = 'normal', weight = 'normal' }) => {
   `
 }
 
-export const grid = (cols, gutters = cols - 1) =>
+export const grid = (cols: number, gutters: number = cols - 1) =>
   GRID.columnWidth * cols + GRID.gutterWidth * gutters
 
-export const spring = name => {
+export const spring = (name: SpringName) => {
   return SPRINGS[name] || SPRINGS.normal
 }
 
 // CSS breakpoints
-export const breakpoint = (name, styles) => css`
+export const breakpoint = (name: BreakPointName, styles: string) => css`
   @media (min-width: ${BREAKPOINTS[name]}px) {
     ${styles};
   }
@@ -68,8 +90,9 @@ export const BreakPoint = ({
   children,
   ...props
 }: {
-  from: string,
-  to: string,
+  from: BreakPointName,
+  to: BreakPointName,
+  children: Node,
 }) => {
   const query = {}
   if (BREAKPOINTS[from]) {
