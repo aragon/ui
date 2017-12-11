b6c022fdb101b3ccb9bf6545f15f15461b62bb67
Pierre Bertet <hello@pierre.world>
2017-12-11 12:42:25 +0000

    Springs: make "fast" less bouncy (#36)

--- src/shared-styles.js
+++ src/shared-styles.js
@@ -27,7 +27,7 @@ const GRID = {
 const SPRINGS = {
   slow: { stiffness: 150, damping: 18, precision: 0.001 },
   normal: { stiffness: 190, damping: 22, precision: 0.001 },
-  fast: { stiffness: 250, damping: 22, precision: 0.001 },
+  fast: { stiffness: 220, damping: 24, precision: 0.001 },
 }
 
 // These breakpoints values represent minimum screen sizes.
