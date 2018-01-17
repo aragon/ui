a700210fd379a8ed05b1067dd9439d81e3f5e8d5
Juwan Wheatley <datwheat@gmail.com>
2018-01-16 22:37:52 -0500

    let appbar demo span to its full potential

--- gallery/src/pages/PageAppBar.js
+++ gallery/src/pages/PageAppBar.js
@@ -3,22 +3,24 @@ import Page from 'comps/Page/Page'
 
 import readme from 'ui-src/components/AragonApp/AppBar.md'
 import { AppBar, Button } from '@aragon/ui'
-import Container from '../components/Page/DemoContainer'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+`
 
 const PageAppBar = ({ title }) => (
   <Page title={title} readme={readme}>
     <Page.Demo>
       <Container>
-        <div>
-          <AppBar
-            title="Your Aragon App"
-            endContent={
-              <Button mode="strong" compact>
-                Log In
-              </Button>
-            }
-          />
-        </div>
+        <AppBar
+          title="Your Aragon App"
+          endContent={
+            <Button mode="strong" compact>
+              Log In
+            </Button>
+          }
+        />
       </Container>
     </Page.Demo>
   </Page>
