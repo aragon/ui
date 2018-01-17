a34126ad0420c56ede0e3fde60d1cbafeb2da34c
Juwan Wheatley <datwheat@gmail.com>
2018-01-16 23:56:31 -0500

    remove unused container

--- gallery/src/pages/PageAragonApp.js
+++ gallery/src/pages/PageAragonApp.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Page from 'comps/Page/Page'
 
 import readme from 'ui-src/components/AragonApp/AragonApp.md'
-import Container from '../components/Page/DemoContainer'
 
 const PageAragonApp = ({ title }) => <Page title={title} readme={readme} />
 
