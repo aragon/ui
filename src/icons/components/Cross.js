0886b44632ee73785e58814a841a5525807585b9
Pierre Bertet <hello@pierre.world>
2018-01-23 18:21:51 +0000

    New icons: Check, Cross, Time (#89)

---
+++ src/icons/components/Cross.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Cross = props => (
+  <svg width={11} height={11} viewBox="0 0 11 11" {...props}>
+    <path
+      d="M10.476 1.524L6.3 5.7l4.176 4.176-1.062 1.062-4.176-4.176-4.176 4.176L0 9.876 4.176 5.7 0 1.524 1.062.462l4.176 4.176L9.414.462z"
+      fill="#FB7777"
+      fillRule="evenodd"
+    />
+  </svg>
+)
+
+export default Cross
