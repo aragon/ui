0fc30e1d5f20e88c792982f06a500c71e8a35448
Brett Sun <qisheng.brett.sun@gmail.com>
2018-02-04 17:48:21 +0100

    SidePanel: don't directly export styling constants

--- src/components/SidePanel/SidePanelSeparator.js
+++ src/components/SidePanel/SidePanelSeparator.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import theme from '../../theme'
-import { HORIZONTAL_PADDING } from './SidePanel'
+import SidePanel from './SidePanel'
 
 const SidePanelSeparator = styled.div`
-  width: calc(100% + ${HORIZONTAL_PADDING * 2}px);
-  margin: 0 -${HORIZONTAL_PADDING}px;
+  width: calc(100% + ${SidePanel.HORIZONTAL_PADDING * 2}px);
+  margin: 0 -${SidePanel.HORIZONTAL_PADDING}px;
   height: 1px;
   background: ${theme.contentBorder};
 `
--- src/components/SidePanel/SidePanelSplit.js
+++ src/components/SidePanel/SidePanelSplit.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import theme from '../../theme'
-import { HORIZONTAL_PADDING } from './SidePanel'
+import SidePanel from './SidePanel'
 
 const SidePanelSplit = ({ children, ...props }) => (
   <Main {...props}>
@@ -12,15 +12,15 @@ const SidePanelSplit = ({ children, ...props }) => (
 
 const Main = styled.div`
   display: flex;
-  width: calc(100% + ${HORIZONTAL_PADDING * 2}px);
-  margin: 0 -${HORIZONTAL_PADDING}px;
+  width: calc(100% + ${SidePanel.HORIZONTAL_PADDING * 2}px);
+  margin: 0 -${SidePanel.HORIZONTAL_PADDING}px;
   border: 1px solid ${theme.contentBorder};
   border-width: 1px 0;
 `
 
 const Part = styled.div`
   width: 50%;
-  padding: 20px ${HORIZONTAL_PADDING}px;
+  padding: 20px ${SidePanel.HORIZONTAL_PADDING}px;
   &:first-child {
     border-right: 1px solid ${theme.contentBorder};
   }
