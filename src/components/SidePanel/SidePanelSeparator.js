c01b5dc3e347795261cc55f3873966cc10aec67d
Pierre Bertet <hello@pierre.world>
2018-02-06 10:54:52 +0100

    Add SidePanelSeparator and SidePanelSplit (#93)
    These components can be used inside of <SidePanel />.
    
    - SidePanelSeparator creates an horizontal line.
    - SidePanelSplit creates a vertical line between its two children.

---
+++ src/components/SidePanel/SidePanelSeparator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import theme from '../../theme'
+import SidePanel from './SidePanel'
+
+const SidePanelSeparator = styled.div`
+  width: calc(100% + ${SidePanel.HORIZONTAL_PADDING * 2}px);
+  margin: 0 -${SidePanel.HORIZONTAL_PADDING}px;
+  height: 1px;
+  background: ${theme.contentBorder};
+`
+
+export default SidePanelSeparator
---
+++ src/components/SidePanel/SidePanelSplit.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+import theme from '../../theme'
+import SidePanel from './SidePanel'
+
+const SidePanelSplit = ({ children, ...props }) => (
+  <Main {...props}>
+    <Part>{children[0]}</Part>
+    <Part>{children[1]}</Part>
+  </Main>
+)
+
+const Main = styled.div`
+  display: flex;
+  width: calc(100% + ${SidePanel.HORIZONTAL_PADDING * 2}px);
+  margin: 0 -${SidePanel.HORIZONTAL_PADDING}px;
+  border: 1px solid ${theme.contentBorder};
+  border-width: 1px 0;
+`
+
+const Part = styled.div`
+  width: 50%;
+  padding: 20px ${SidePanel.HORIZONTAL_PADDING}px;
+  &:first-child {
+    border-right: 1px solid ${theme.contentBorder};
+  }
+`
+
+export default SidePanelSplit
