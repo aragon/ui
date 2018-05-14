db0ee21c2b22cfbd964001257135c84e5f6c8320
Pierre Bertet <hello@pierre.world>
2018-05-14 18:22:04 +0100

    New component: AppView (#164)

---
+++ src/components/AragonApp/AppView.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ExtraPropTypes from 'airbnb-prop-types'
+import styled from 'styled-components'
+import AppBar from './AppBar'
+
+class AppView extends React.Component {
+  static defaultProps = {
+    title: '',
+    padding: 30,
+  }
+  static propTypes = {
+    appBar: ExtraPropTypes.elementType(AppBar),
+    title: PropTypes.string,
+    children: PropTypes.node,
+    padding: PropTypes.number,
+  }
+  render() {
+    const { appBar, title, children, padding } = this.props
+    return (
+      <Main>
+        <Header>{appBar || <AppBar title={title} />}</Header>
+        <ScrollWrapper>
+          <Content padding={padding}>{children}</Content>
+        </ScrollWrapper>
+      </Main>
+    )
+  }
+}
+
+const Main = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: stretch;
+`
+
+const Header = styled.div`
+  flex-shrink: 0;
+`
+
+const ScrollWrapper = styled.div`
+  height: 100%;
+  overflow: auto;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+  padding: ${({ padding }) => `${padding}px`};
+`
+
+export default AppView
