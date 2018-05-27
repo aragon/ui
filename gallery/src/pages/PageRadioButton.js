551ebb99421b8304063e9a1740c68c70967d16b1
Pierre Bertet <hello@pierre.world>
2018-05-27 16:27:31 +0100

    Update demo page

--- gallery/src/pages/PageRadioButton.js
+++ gallery/src/pages/PageRadioButton.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { RadioButton, RadioGroup } from '@aragon/ui'
 import Page from 'comps/Page/Page'
 import Container from 'comps/Page/DemoContainer'
@@ -6,38 +7,34 @@ import readme from 'ui-src/components/Input/RadioButton.md'
 
 class PageRadioButton extends React.Component {
   state = {
-    value: '0',
+    value: 0,
   }
   handleChange = event => {
-    alert(`Selected radio with value: ${event.target.value}`)
-    this.setState({ value: event.target.value })
+    this.setState({ value: Number(event.target.value) })
   }
   render() {
     const { title } = this.props
     const { value } = this.state
     return (
       <Page title={title} readme={readme}>
-        <Page.Demo height={100}>
+        <Page.Demo opaque>
           <Container>
-            <h1>Some radio buttons:</h1>
             <div>
-              <RadioGroup>
-                <RadioButton
-                  checked={value === '0'}
-                  onChange={this.handleChange}
-                  value="0"
-                />
-                <RadioButton
-                  checked={value === '1'}
-                  onChange={this.handleChange}
-                  value="1"
-                />
-                <RadioButton
-                  checked={value === '2'}
-                  onChange={this.handleChange}
-                  value="2"
-                />
-              </RadioGroup>
+              <h1 style={{ marginBottom: '20px' }}>
+                Selected item: {Number(value) + 1}
+              </h1>
+              <div>
+                {[...Array(3)].map((_, i) => (
+                  <Item>
+                    <RadioButton
+                      checked={value === i}
+                      onChange={this.handleChange}
+                      value={i}
+                    />
+                    Item {i + 1}
+                  </Item>
+                ))}
+              </div>
             </div>
           </Container>
         </Page.Demo>
@@ -45,5 +42,16 @@ class PageRadioButton extends React.Component {
     )
   }
 }
+
+const Item = styled.label`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  cursor: pointer;
+  color: #000;
+  &:active {
+    color: #666;
+  }
+`
 
 export default PageRadioButton
