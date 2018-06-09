1c3a385dec6eb96acc595efb5fbc52277a4fd212
Pierre Bertet <hello@pierre.world>
2018-06-09 03:00:05 +0100

    SidePanel: move to react-spring
    Also export a new `springs` object.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable prettier/prettier */
 export { difference, formatHtmlDatetime } from './date'
 export { formatIntegerRange } from './format'
-export { font, grid, spring, breakpoint, BreakPoint, unselectable } from './styles'
+export {
+  font,
+  grid,
+  spring,
+  springs,
+  breakpoint,
+  BreakPoint,
+  unselectable,
+} from './styles'
--- src/utils/styles/spring.js
+++ src/utils/styles/spring.js
@@ -1,18 +1,41 @@
-/* @flow */
-type SpringName = 'slow' | 'normal' | 'fast'
+// This module exports a `springs` object and a `spring()` function:
+//
+//   - `spring()` is deprecated and will be removed in the future.
+//   - `springs` should be used from now on.
 
-const SPRINGS: {
-  [SpringName]: {|
-    stiffness: number,
-    damping: number,
-    precision: number,
-  |},
-} = {
-  slow: { stiffness: 150, damping: 18, precision: 0.001 },
-  normal: { stiffness: 190, damping: 22, precision: 0.001 },
-  fast: { stiffness: 220, damping: 24, precision: 0.001 },
+export const springs = {
+  // Super slow spring, for debugging purposes
+  debug: { tension: 10, friction: 40 },
+
+  // Slow spring, can be used to move large things (e.g. a side panel).
+  lazy: { tension: 50, friction: 10 },
+
+  // These springs (slow, normal, fast) were originally created for
+  // react-motion. While they can be used with react-spring, their use is not
+  // recommended. New springs will be added later as we move everything to
+  // use react-spring.
+  slow: { tension: 150, friction: 18 },
+  normal: { tension: 190, friction: 22 },
+  fast: { tension: 220, friction: 24 },
 }
 
-export const spring = (name: SpringName) => {
-  return SPRINGS[name] || SPRINGS.normal
+// Deprecated, see above
+export const spring = name => {
+  return springs[name] || springs.normal
 }
+
+const reactMotionSprings = Object.entries(springs).reduce(
+  // react-motion aliases:
+  //  stiffness => tension
+  //  damping => friction
+  (springs, [name, spring]) => ({
+    ...springs,
+    [name]: {
+      ...spring,
+      stiffness: spring.tension,
+      damping: spring.friction,
+      precision: 0.001,
+    },
+  }),
+  {}
+)
