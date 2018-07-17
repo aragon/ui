543f261b4c47f40489720812196a33b0bed5bc9c
Pierre Bertet <hello@pierre.world>
2018-07-17 09:56:49 +0100

    Gallery page

---
+++ gallery/src/pages/PageNavigationBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+import { AppBar, Button, NavigationBar } from '@aragon/ui'
+import Page from 'comps/Page/Page'
+import Container from 'comps/Page/DemoContainer'
+import readme from 'ui-src/components/AragonApp/AppBar.md'
+
+const StyledContainer = styled(Container)`
+  width: 100%;
+`
+
+const PageAppBar = ({ title }) => (
+  <Page title={title} readme={readme}>
+    <Page.Demo>
+      <StyledContainer>
+        <AppBar
+          endContent={
+            <Button mode="strong" compact>
+              Log In
+            </Button>
+          }
+        >
+          <NavigationBar />
+        </AppBar>
+      </StyledContainer>
+    </Page.Demo>
+  </Page>
+)
+
+export default PageAppBar
