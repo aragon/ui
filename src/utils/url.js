6563f9948d67a3901652d5ed1345cdc9127d8cd4
Huan Zhang <huan.zhang@trinitymirror.com>
2018-08-14 12:46:02 +0100

    add comment + refactor export pattern to be in line with other utils

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,9 +1,8 @@
 // prefix helper
-const prefixUrl = (url, publicUrl) =>
+export const prefixUrl = (url, publicUrl) =>
   url.startsWith('data:') ? url : publicUrl + url
 
-const ensureSlash = path => {
+// trailing slash helper
+export const ensureSlash = path => {
   return path.endsWith('/') ? `${path}/` : path
 }
-
-export { prefixUrl, ensureSlash }
