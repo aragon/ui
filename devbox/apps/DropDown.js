391395851e4ebddcd53255eefe4777359f6bd4b7
Pierre Bertet <hello@pierre.world>
2018-09-04 11:20:39 +0100

    DropDown: add devbox app

---
+++ devbox/apps/DropDown.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import { AragonApp, DropDown, unselectable } from '@aragon/ui'
+
+const items = ['Strawberry', 'Banana', 'Apple', 'Cherry']
+
+class App extends React.Component {
+  state = { active: 0 }
+  handleChange = index => {
+    this.setState({ active: index })
+  }
+  render() {
+    const { active } = this.state
+    return (
+      <AragonApp publicUrl="/aragon-ui/">
+        <Main>
+          <Container>
+            <DropDown
+              items={items}
+              active={active}
+              onChange={this.handleChange}
+            />
+          </Container>
+        </Main>
+      </AragonApp>
+    )
+  }
+}
+
+const Main = styled.div`
+  ${unselectable};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #fff;
+`
+
+const Container = styled.div`
+  width: 150px;
+`
+
+export default App
