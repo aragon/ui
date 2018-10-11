0fd533c6f3f8f2fe1c4039df31977f7e866ac0bc
Pierre Bertet <hello@pierre.world>
2018-10-11 19:11:24 +0100

    Button size (#214)
    A smaller button size was needed. This commit introduces a new prop, `size`, that can be set to `normal` (default), `small` or `mini` (the new size).
    
    - `small` is the equivalent to the `compact` prop, and should be used instead.
    - `mini` is the new size, which is smaller than the `small` size.

---
+++ devbox/apps/Button.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import styled from 'styled-components'
+import { AragonApp, Button, Text, theme } from '@aragon/ui'
+
+const MODES = ['normal', 'secondary', 'strong', 'outline', 'text']
+const SIZES = ['normal', 'small', 'mini']
+const EMPHASES = ['none', 'positive', 'negative']
+const DISABLED = [false, true]
+
+class App extends React.Component {
+  renderAllButtons() {
+    return EMPHASES.map((emphasis, emphasisIndex) => (
+      <div key={[emphasisIndex].join('')} className="emphasis">
+        <h2>
+          <Text size="xlarge">
+            emphasis=
+            {emphasis}
+          </Text>
+        </h2>
+        <div>
+          {DISABLED.map((disabled, disabledIndex) =>
+            SIZES.map((size, sizeIndex) => (
+              <div
+                key={[emphasisIndex, disabledIndex, sizeIndex].join('')}
+                className="row"
+              >
+                {MODES.map((mode, modeIndex) => (
+                  <span
+                    key={[
+                      emphasisIndex,
+                      disabledIndex,
+                      sizeIndex,
+                      modeIndex,
+                    ].join('')}
+                  >
+                    <Button
+                      size={size}
+                      mode={mode}
+                      emphasis={emphasis}
+                      disabled={disabled}
+                    >
+                      {mode}
+                      {disabled && size !== 'normal' ? ` (${size} & off)` : ''}
+                      {disabled && size === 'normal' ? ` (off)` : ''}
+                      {!disabled && size !== 'normal' ? ` (${size})` : ''}
+                    </Button>
+                  </span>
+                ))}
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    ))
+  }
+  renderSomeButtons() {
+    return (
+      <div style={{ padding: '100px 0' }}>
+        {['normal', 'strong', 'secondary'].map(mode => (
+          <div className="row" key={mode}>
+            {SIZES.map(size => (
+              <span key={mode + size}>
+                <Button mode={mode} size={size}>
+                  {size}
+                </Button>
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+    )
+  }
+  render() {
+    // const buttons = this.renderSomeButtons()
+    const buttons = this.renderAllButtons()
+    return (
+      <AragonApp publicUrl="/aragon-ui/">
+        <Main>
+          <Container>
+            <h1>
+              <Text size="xxlarge">Button variations</Text>
+            </h1>
+            {buttons}
+          </Container>
+        </Main>
+      </AragonApp>
+    )
+  }
+}
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  background: hsl(200, 30%, 85%);
+`
+
+const Container = styled.div`
+  min-height: 0;
+  h1 {
+    margin: 40px 0;
+    text-align: center;
+  }
+  h2 {
+    margin: 20px 0;
+    text-align: center;
+  }
+  .emphasis {
+    padding: 20px 0 80px;
+  }
+  .row {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+    > span {
+      margin-left: 10px;
+    }
+  }
+`
+
+export default App
--- devbox/index.js
+++ devbox/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 // import App from './apps/LinkedSliders'
 // import App from './apps/RadioButton'
 // import App from './apps/SidePanel'
-import App from './apps/NavigationBar'
+// import App from './apps/NavigationBar'
+import App from './apps/Button'
 
 ReactDOM.render(<App />, document.getElementById('app'))
