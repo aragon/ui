f24c416670cf98d5fe84d190de09a47c5a805d85
Pierre Bertet <hello@pierre.world>
2018-10-18 14:58:51 +0100

    Fix the size of IconAttention (icons should all be 22x22)

--- src/icons/components/Attention.js
+++ src/icons/components/Attention.js
@@ -1,13 +1,12 @@
 import React from "react"
 
 const Attention = props => (
-  <svg width={14} height={14} viewBox="0 0 14 14" {...props}>
-    <g fill="none" fillRule="evenodd">
+  <svg width={22} height={22} viewBox="0 0 22 22" {...props}>
+    <g transform="translate(4 4)" fill="none" fillRule="evenodd">
       <rect fill="#DAEAEF" width={14} height={14} rx={7} />
       <path
         d="M6.155 8.547h1.298V3.3H6.155v5.247zM6.045 11h1.529V9.537H6.045V11z"
         fill="#6D777B"
-        opacity={0.7}
       />
     </g>
   </svg>
