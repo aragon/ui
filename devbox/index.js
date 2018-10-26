265ac9713c46bea0b5f037ce51efe9e6e993ec49
Pierre Bertet <hello@pierre.world>
2018-10-26 16:33:36 +0100

    devbox: select the app to run from the CLI
    The app is now set using the DEVBOX_APP environment variable.
    
    Example:
    
    $ DEVBOX_APP=Button npm start

--- devbox/index.js
+++ devbox/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import App from './apps/LinkedSliders'
-// import App from './apps/RadioButton'
-// import App from './apps/SidePanel'
-// import App from './apps/NavigationBar'
-// import App from './apps/Button'
-// import App from './apps/EmptyStateCard'
-// import App from './apps/TabBar'
-import App from './apps/IdentityBadge'
+import LinkedSliders from './apps/LinkedSliders'
+import RadioButton from './apps/RadioButton'
+import SidePanel from './apps/SidePanel'
+import NavigationBar from './apps/NavigationBar'
+import Button from './apps/Button'
+import EmptyStateCard from './apps/EmptyStateCard'
+import TabBar from './apps/TabBar'
+import IdentityBadge from './apps/IdentityBadge'
+
+// Set DEVBOX_APP to one of these apps (e.g. `DEVBOX_APP=Button npm start`)
+const MODULES = {
+  LinkedSliders,
+  RadioButton,
+  SidePanel,
+  NavigationBar,
+  Button,
+  EmptyStateCard,
+  TabBar,
+  IdentityBadge,
+}
+
+const App =
+  MODULES[process.env.DEVBOX_APP] ||
+  (() => <div>DEVBOX_APP is not set correctly (see devbox/index.js)</div>)
 
 ReactDOM.render(<App />, document.getElementById('app'))
