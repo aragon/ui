34dd2b5d570a8644e47ec2fdbe5236050a9edfa2
Pierre Bertet <hello@pierre.world>
2018-11-06 21:03:04 +0000

    Upgrade to react-spring 6

--- src/utils/styles/spring.js
+++ src/utils/styles/spring.js
@@ -3,48 +3,51 @@
 //   - `spring()` is deprecated and will be removed in the future (react-motion).
 //   - `springs` should be used from now on (react-spring).
 
+import { warn } from '..'
+
 export const springs = {
   // Super slow spring, for debugging purposes
-  debug: { tension: 10, friction: 40 },
+  debug: { mass: 1, tension: 10, friction: 40 },
 
   // Slow spring, can be used to move large things (e.g. a side panel).
-  lazy: { tension: 50, friction: 10 },
+  lazy: { mass: 1, tension: 120, friction: 20, precision: 0.001 },
 
   // Medium speed spring, can be used to move small objects.
-  smooth: { tension: 120, friction: 12 },
+  smooth: { mass: 0.7, tension: 300, friction: 25, precision: 0.001 },
 
   // Fast speed spring, for actions that need to feel almost instant.
-  swift: { tension: 400, friction: 28 },
+  swift: { mass: 0.5, tension: 400, friction: 30, precision: 0.001 },
 
   // These springs (slow, normal, fast) were originally created for
-  // react-motion. While they can be used with react-spring, their use is not
-  // recommended. New springs will be added later as we move everything to
-  // use react-spring.
-  slow: { tension: 150, friction: 18 },
-  normal: { tension: 190, friction: 22 },
-  fast: { tension: 220, friction: 24 },
+  // react-motion. Their use is deprecated.
+  get slow() {
+    warn(
+      'springs.slow is deprecated. Please use another spring (like springs.lazy).'
+    )
+    return { mass: 1, tension: 150, friction: 18 }
+  },
+  get normal() {
+    warn(
+      'springs.normal is deprecated. Please use another spring (like springs.smooth).'
+    )
+    return { mass: 1, tension: 190, friction: 22 }
+  },
+  get fast() {
+    warn(
+      'springs.fast is deprecated. Please use another spring (like springs.swift).'
+    )
+    return { mass: 1, tension: 220, friction: 24 }
+  },
 }
 
-// Convert to react-motion springs:
-//
-//   stiffness => tension
-//   damping => friction
-//
-const reactMotionSprings = Object.entries(springs).reduce(
-  (springs, [name, spring]) => ({
-    ...springs,
-    [name]: {
-      stiffness: spring.tension,
-      damping: spring.friction,
-      precision: 0.001,
-    },
-  }),
-  {}
-)
+// Deprecated react-motion springs (see above)
+const reactMotionSprings = {
+  slow: { stiffness: 150, friction: 18 },
+  normal: { stiffness: 190, friction: 22 },
+  fast: { stiffness: 220, friction: 24 },
+}
 
-// Deprecated, see above
 export const spring = name => {
-  // TODO: propagate process.env.NODE_ENV to Aragon UI
   if (process.env.NODE_ENV === 'development') {
     console.warn(
       'spring(name) is deprecated. Please use springs[name] instead.'
