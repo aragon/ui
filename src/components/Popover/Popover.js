1ee2d24b29c71fa02f502b51c84a32b41e56d4fe
Pierre Bertet <hello@pierre.world>
2018-11-08 01:00:53 +0000

    Move the card style + transition to the Popover component

--- src/components/Popover/Popover.js
+++ src/components/Popover/Popover.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM, { findDOMNode } from 'react-dom'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Popper from 'popper.js'
+import { Transition, animated } from 'react-spring'
 
-import { Consumer } from '../RootProvider/RootProvider'
+import theme from '../../theme'
+import { springs } from '../../utils/styles'
+import { Root } from '../../providers'
 
-class Popover extends React.Component {
+class PopoverBase extends React.Component {
   static propTypes = {
     openerRef: PropTypes.instanceOf(Element).isRequired,
     containerRef: PropTypes.instanceOf(Element),
@@ -21,28 +25,27 @@ class Popover extends React.Component {
     gutter: '20px',
   }
 
-  state = {
-    thisRef: null,
-  }
+  _element = React.createRef()
 
   componentDidMount() {
+    const ownerDocument = this._element.current.ownerDocument
+
     this.initPopper()
-    this.state.thisRef = findDOMNode(this)
-    const ownerDocument = this.state.thisRef.ownerDocument
     ownerDocument.addEventListener('keydown', this.handleEscape)
     ownerDocument.addEventListener('click', this.handleClick, true)
     ownerDocument.addEventListener('touchend', this.handleClick, true)
   }
 
   componentWillUnmount() {
+    const ownerDocument = this._element.current.ownerDocument
+
     this.destroyPopper()
-    const ownerDocument = this.state.thisRef.ownerDocument
     ownerDocument.removeEventListener('keydown', this.handleEscape)
     ownerDocument.removeEventListener('click', this.handleClick, true)
     ownerDocument.removeEventListener('touchend', this.handleClick, true)
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps, prevState) {
     const { placement, children } = this.props
     if (prevProps.placement !== placement || prevProps.children !== children) {
       this.destroyPopper()
@@ -64,13 +67,12 @@ class Popover extends React.Component {
 
   handleClick = e => {
     const { onClose, openerRef } = this.props
-    const { thisRef } = this.state
 
-    if (!openerRef) {
+    if (!openerRef || !this._element.current) {
       return
     }
 
-    const thisRefContent = thisRef.getBoundingClientRect()
+    const thisRefContent = this._element.current.getBoundingClientRect()
     const openerRefContent = openerRef.getBoundingClientRect()
 
     const insidePopover = this.insideRect(thisRefContent, e.clientX, e.clientY)
@@ -84,7 +86,7 @@ class Popover extends React.Component {
   initPopper() {
     const { openerRef, placement, gutter } = this.props
     if (placement && !this.popper) {
-      this.popper = new Popper(openerRef, findDOMNode(this), {
+      this.popper = new Popper(openerRef, this._element.current, {
         placement,
         modifiers: {
           offset: { offset: `0, ${gutter}` },
@@ -103,21 +105,44 @@ class Popover extends React.Component {
   render() {
     const { top, left, zIndex, children, containerRef } = this.props
     return ReactDOM.createPortal(
-      <div style={{ position: 'absolute', top, left, zIndex }}>{children}</div>,
+      <div
+        ref={this._element}
+        style={{ position: 'absolute', top, left, zIndex }}
+      >
+        <Transition
+          config={springs.swift}
+          from={{ scale: 0.9, opacity: 0 }}
+          enter={{ scale: 1, opacity: 1 }}
+          leave={{ scale: 0.9, opacity: 0 }}
+          native
+        >
+          {({ scale, opacity }) => (
+            <Card
+              style={{
+                opacity,
+                transform: scale.interpolate(t => `scale3d(${t},${t},1)`),
+              }}
+            >
+              {children}
+            </Card>
+          )}
+        </Transition>
+      </div>,
       containerRef
     )
   }
 }
 
-class PopoverWithProvider extends React.Component {
-  static propTypes = Popover.propTypes
-  render() {
-    return (
-      <Consumer>
-        {({ el }) => <Popover containerRef={el} {...this.props} />}
-      </Consumer>
-    )
-  }
-}
+const Card = styled(animated.div)`
+  background: ${theme.contentBackground};
+  border: 1px solid #e6e6e6;
+  border-radius: 3px;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.06);
+`
 
-export default PopoverWithProvider
+const Popover = props => (
+  <Root>{el => <PopoverBase containerRef={el} {...props} />}</Root>
+)
+Popover.propTypes = PopoverBase.propTypes
+
+export default Popover
