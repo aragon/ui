ec45bb4fc5d764879d473434719bea8517ff06c1
Paul Henschel <drcmda@gmail.com>
2018-11-13 19:31:29 +0100

    #215 add ToastHub component (#249)
    Adds a ToastHub component, documentation and running example included.

---
+++ gallery/src/pages/PageToastHub.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ToastHub, Toast, Button } from '@aragon/ui'
+
+import Page from 'comps/Page/Page'
+import DemoContainer from 'comps/Page/DemoContainer'
+import readme from 'ui-src/components/ToastHub/README.md'
+
+const Container = styled(DemoContainer)`
+  p {
+    margin: 0;
+  }
+  p + p {
+    margin-top: 20px;
+  }
+`
+
+const PageToastHub = ({ title }) => (
+  <ToastHub>
+    <Page title={title} readme={readme}>
+      <Page.Demo opaque>
+        <Container>
+          <div>
+            <Toast>
+              {toast => (
+                <Button mode="strong" compact onClick={() => toast('hello')}>
+                  Add toast
+                </Button>
+              )}
+            </Toast>
+          </div>
+        </Container>
+      </Page.Demo>
+    </Page>
+  </ToastHub>
+)
+
+export default PageToastHub
--- src/components/index.js
+++ src/components/index.js
@@ -33,6 +33,7 @@ export { default as TableHeader } from './Table/TableHeader'
 export { default as TableRow } from './Table/TableRow'
 export { default as Text } from './Text/Text'
 export { default as TextInput } from './Input/TextInput'
+export { ToastHub, Toast } from './ToastHub/ToastHub'
 export {
   default as TransactionProgress,
 } from './TransactionProgress/TransactionProgress'
