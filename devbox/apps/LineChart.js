4eb08ef9fe8734ce0c7d2b4d5ae70767148b0425
Pierre Bertet <hello@pierre.world>
2019-01-06 22:00:23 +0100

    Fix behavior + API changes
    The chart now behave properly following the value set to
    `durationSlices`, now renamed `total`.
    
    Prop changes:
    
    - `durationSlices` has been renamed `total`.
    - `captionsHeight` has been removed as a prop.
    - `labelColor` has been added to change the labels color.
    - `settings` has been renamed `lines`.

--- devbox/apps/LineChart.js
+++ devbox/apps/LineChart.js
@@ -2,43 +2,73 @@ import React from 'react'
 import styled from 'styled-components'
 import { AragonApp, LineChart, unselectable } from '@aragon/ui'
 
-const data = [
-  {
-    id: 1,
-    values: [0.1, 0.3, 0.5, 0.4, 0.6, 0.9],
-    color: '#028CD1',
-  },
-  {
-    id: 2,
-    values: [0.5, 0.1, 0.9, 0.01, 0.6, 0.3],
-    color: '#bdedef',
-  }
-]
-
-const labels = ['1', '2', '3','4','5','6','7','8']
-
 class App extends React.Component {
-  state = { active: 0 }
-  handleChange = index => {
-    this.setState({ active: index })
+  _cachedLines = [[], [], []]
+  state = { count: 5, total: 10 }
+  updateCount(increment) {
+    this.setState(({ count }) => ({ count: Math.max(0, count + increment) }))
+  }
+  updateTotal(increment) {
+    this.setState(({ total }) => ({ total: Math.max(0, total + increment) }))
+  }
+  getLines(count) {
+    let lines = this._cachedLines
+    if (lines[0].length < count) {
+      while (lines[0].length < count) {
+        lines = lines.map(values => [...values, Math.random()])
+      }
+    }
+    this._cachedLines = lines
+    return lines.map(values => values.slice(0, count))
   }
   render() {
-    const { active } = this.state
+    const { count, total } = this.state
+    const lines = this.getLines(count)
     return (
       <AragonApp publicUrl="/aragon-ui/">
         <Main>
           <Container>
-            <LineChart
-              settings={data}
-              durationSlices={8}
-              captionsHeight={20}
-            />
+            <ButtonGroup>
+              <button onClick={() => this.updateCount(-1)}>-</button>
+              <span>Values: {count}</span>
+              <button onClick={() => this.updateCount(1)}>+</button>
+            </ButtonGroup>
+            <ButtonGroup>
+              <button onClick={() => this.updateTotal(-1)}>-</button>
+              <span>Total: {total}</span>
+              <button onClick={() => this.updateTotal(1)}>+</button>
+            </ButtonGroup>
+            <LineChart lines={lines} total={total} captionsHeight={20} />
           </Container>
         </Main>
       </AragonApp>
     )
   }
 }
+
+const ButtonGroup = styled.div`
+  display: flex;
+  gap: 10px;
+  margin: 10px 0;
+  width: '100%';
+  height: 40px;
+  justify-content: space-between;
+  align-items: center;
+  color: #999;
+  font-weight: 300;
+  button {
+    cursor: pointer;
+    border: 0;
+    background: #bed;
+    width: 40px;
+    height: 40px;
+    color: #fff;
+    border-radius: 3px;
+    &:active {
+      background: #adc;
+    }
+  }
+`
 
 const Main = styled.div`
   ${unselectable};
--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -5,18 +5,20 @@ import memoize from 'lodash-es/memoize'
 import PropTypes from '../../proptypes'
 import { unselectable, springs } from '../../utils/styles'
 
+const LABELS_HEIGHT = 30
+
 class LineChart extends React.Component {
   static propTypes = {
     springConfig: PropTypes._spring,
-    durationSlices: PropTypes.number,
+    total: PropTypes.number,
     width: PropTypes.number,
     height: PropTypes.number,
-    captionsHeight: PropTypes.number,
     dotRadius: PropTypes.number,
     animDelay: PropTypes.number,
     borderColor: PropTypes.string,
+    labelColor: PropTypes.string,
     reset: PropTypes.bool,
-    settings: PropTypes.arrayOf(
+    lines: PropTypes.arrayOf(
       PropTypes.oneOfType([
         PropTypes.shape({
           id: PropTypes.number,
@@ -32,63 +34,86 @@ class LineChart extends React.Component {
   }
 
   static defaultProps = {
-    durationSlices: 16,
     springConfig: springs.lazy,
+    total: -1,
     width: 300,
     height: 200,
-    captionsHeight: 20,
     dotRadius: 7 / 2,
     animDelay: 500,
     reset: false,
     borderColor: 'rgba(209, 209, 209, 0.5)',
-    settings: [],
+    labelColor: '#6d777b',
+    lines: [],
     label: index => index + 1,
-    color: (index, { settings }) =>
-      `hsl(${(index * (360 / settings.length) + 40) % 360}, 60%, 70%)`,
+    color: (index, { lines }) =>
+      `hsl(${(index * (360 / lines.length) + 40) % 360}, 60%, 70%)`,
   }
 
   getX(index) {
-    const { width, durationSlices } = this.props
-
-    const slice = width / (durationSlices - 1)
+    const { width } = this.props
+    const slice = width / Math.max(1, this.getTotalCount() - 1)
     return slice * index
   }
 
-  getY(percentage, progress) {
-    const { dotRadius, height } = this.props
+  getY(percentage, progress, height) {
+    const { dotRadius } = this.props
 
     const padding = dotRadius + 2
     return height - padding - (height - padding * 2) * percentage * progress
   }
 
-  convertSettings = memoize(settings => {
-    return settings.map(settingOrValues =>
-      Array.isArray(settingOrValues)
-        ? { values: settingOrValues }
-        : settingOrValues
+  getLines() {
+    const { lines } = this.props
+    return lines.map(lineOrValues =>
+      Array.isArray(lineOrValues) ? { values: lineOrValues } : lineOrValues
     )
-  })
+  }
+
+  getValuesCount() {
+    const lines = this.getLines()
+    // All the values have the same length, so we can use the first one.
+    return lines[0] ? lines[0].values.length : 0
+  }
+
+  getTotalCount() {
+    const { total } = this.props
+    const valuesCount = this.getValuesCount()
+    // If no total is provided, the total is the number of provided values.
+    return total > 0 && total > valuesCount ? total : valuesCount
+  }
+
+  getLabelPosition(index, length) {
+    if (index === 0) return 'start'
+    if (index === length - 1) return 'end'
+    return 'middle'
+  }
 
   render() {
     const {
       width,
       height,
-      captionsHeight,
       borderColor,
-      durationSlices,
       dotRadius,
       springConfig,
       label,
       reset,
       animDelay,
       color,
+      labelColor,
     } = this.props
 
-    const settings = this.convertSettings(this.props.settings)
+    const lines = this.getLines()
 
-    // All the settings' values should have same length
-    const valuesLength = settings.length ? settings[0].values.length : 0
-    const labels = label ? [...Array(valuesLength).keys()].map(label) : null
+    // the provided values, up to this point
+    const valuesCount = this.getValuesCount()
+
+    // the total amount of values
+    const totalCount = this.getTotalCount()
+
+    const labels =
+      label && totalCount > 0 ? [...Array(totalCount).keys()].map(label) : null
+
+    const chartHeight = height - (labels ? LABELS_HEIGHT : 0)
 
     return (
       <div>
@@ -101,83 +126,84 @@ class LineChart extends React.Component {
             reset={reset}
           >
             {({ progress }) => (
-              <svg viewBox={`0 0 ${width} ${height + captionsHeight}`}>
+              <svg viewBox={`0 0 ${width} ${height}`}>
                 <rect
                   width={width}
-                  height={height}
+                  height={chartHeight}
                   rx="3"
                   ry="3"
                   fill="#ffffff"
                   strokeWidth="1"
                   stroke={borderColor}
                 />
-                <path
-                  d={`
-                    M 0,${height}
-                    ${[...new Array(durationSlices)].reduce(
-                      (path = '', _, i) =>
-                        `${path} M ${this.getX(i)},${height} l 0,-8`
+                {totalCount > 0 && (
+                  <path
+                    d={`
+                    ${[...new Array(totalCount - 1)].reduce(
+                      (path, _, index) =>
+                        `${path} M ${this.getX(index)},${chartHeight} l 0,-8`,
+                      ''
                     )}
                   `}
-                  stroke={borderColor}
-                  strokeWidth="1"
-                />
-                {settings.map(
-                  (setting, index) =>
-                    valuesLength && (
-                      <g key={`line-plot-${setting.id || index}`}>
-                        <path
-                          d={`
+                    stroke={borderColor}
+                    strokeWidth="1"
+                  />
+                )}
+                {lines.map((line, lineIndex) => (
+                  <g key={`line-plot-${line.id || lineIndex}`}>
+                    <path
+                      d={`
                             M
                             ${this.getX(0)},
-                            ${this.getY(setting.values[0], progress)}
+                            ${this.getY(line.values[0], progress, chartHeight)}
 
-                            ${setting.values
+                            ${line.values
                               .slice(1)
                               .map(
-                                (val, i) =>
+                                (val, index) =>
                                   `L
-                                   ${this.getX((i + 1) * progress)},
-                                   ${this.getY(val, progress)}
+                                   ${this.getX((index + 1) * progress)},
+                                   ${this.getY(val, progress, chartHeight)}
                                   `
                               )
                               .join('')}
                           `}
-                          fill="transparent"
-                          stroke={setting.color || color(index, { settings })}
-                          strokeWidth="2"
-                        />
-                        {setting.values.slice(1, -1).map((val, i) => (
-                          <circle
-                            key={i}
-                            cx={this.getX(i + 1) * progress}
-                            cy={this.getY(val, progress)}
-                            r={dotRadius}
-                            fill="white"
-                            stroke={setting.color || color(index, { settings })}
-                            strokeWidth="1"
-                          />
-                        ))}
-                      </g>
-                    )
-                )}
+                      fill="transparent"
+                      stroke={line.color || color(lineIndex, { lines })}
+                      strokeWidth="2"
+                    />
+                    {line.values.slice(1, -1).map((val, index) => (
+                      <circle
+                        key={index}
+                        cx={this.getX(index + 1) * progress}
+                        cy={this.getY(val, progress, chartHeight)}
+                        r={dotRadius}
+                        fill="white"
+                        stroke={line.color || color(lineIndex, { lines })}
+                        strokeWidth="1"
+                      />
+                    ))}
+                  </g>
+                ))}
                 <line
-                  x1={this.getX(valuesLength - 1) * progress}
+                  x1={this.getX(valuesCount - 1) * progress}
                   y1="0"
-                  x2={this.getX(valuesLength - 1) * progress}
-                  y2={height}
+                  x2={this.getX(valuesCount - 1) * progress}
+                  y2={chartHeight}
                   stroke="#DAEAEF"
                   strokeWidth="3"
                 />
                 {labels && (
-                  <g transform={`translate(0,${height + 20})`}>
-                    {[...new Array(durationSlices - 1)].map((_, i) => (
+                  <g transform={`translate(0,${chartHeight})`}>
+                    {labels.map((label, index) => (
                       <LabelText
-                        key={i}
-                        x={this.getX(i) + (i === 0 ? 2 : 0)}
-                        textAnchor="middle"
+                        key={index}
+                        x={this.getX(index)}
+                        y={LABELS_HEIGHT / 2}
+                        textAnchor={this.getLabelPosition(index, labels.length)}
+                        fill={labelColor}
                       >
-                        {labels[i]}
+                        {label}
                       </LabelText>
                     ))}
                   </g>
@@ -198,9 +224,9 @@ const SvgWrapper = styled.div`
 `
 
 const LabelText = styled.text`
+  alignment-baseline: middle;
   font-size: 12px;
   font-weight: 300;
-  fill: #6d777b;
   ${unselectable};
 `
 
