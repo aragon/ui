6cc6c8dce412db02404e36620fff64d67c5670ca
Pierre Bertet <hello@pierre.world>
2019-01-16 16:26:34 +0100

    IdentityBadge: move from Blockies to EthIcon

--- src/components/EthIcon/EthIcon.js
+++ src/components/EthIcon/EthIcon.js
@@ -12,23 +12,27 @@ class EthIcon extends React.Component {
     address: PropTypes.string.isRequired,
     scale: PropTypes.number,
     radius: PropTypes.number,
+    soften: PropTypes.number,
   }
   static defaultProps = {
     scale: 1,
     radius: 0,
+    soften: 0.3,
   }
   render() {
-    const { address, scale, radius, ...props } = this.props
+    const { address, scale, radius, soften, ...props } = this.props
     const blockiesScale = scale * BASE_SCALE
     return (
       <Main size={BLOCKIES_SQUARES * blockiesScale} radius={radius} {...props}>
-        <BlockiesWrapper size={BLOCKIES_SQUARES * blockiesScale * PX_RATIO}>
-          <Blockies
-            seed={address.toLowerCase()}
-            size={BLOCKIES_SQUARES}
-            scale={blockiesScale * PX_RATIO}
-          />
-        </BlockiesWrapper>
+        <BlockiesScaling size={BLOCKIES_SQUARES * blockiesScale * PX_RATIO}>
+          <BlockiesOpacity soften={soften}>
+            <Blockies
+              seed={address.toLowerCase()}
+              size={BLOCKIES_SQUARES}
+              scale={blockiesScale * PX_RATIO}
+            />
+          </BlockiesOpacity>
+        </BlockiesScaling>
       </Main>
     )
   }
@@ -42,12 +46,17 @@ const Main = styled.div`
   border-radius: ${p => p.radius}px;
 `
 
-const BlockiesWrapper = styled.div`
+const BlockiesOpacity = styled.div`
+  opacity: ${p => 1 - p.soften};
+`
+
+const BlockiesScaling = styled.div`
   /* display:flex to remove the display:inline on the child without using a
   * selector (Blockies doesn’t allow the style prop to be passed). */
   display: flex;
   width: ${p => p.size}px;
   height: ${p => p.size}px;
+  background: #fff;
 
   /* add high-res screens support to Blockies */
   transform: scale(${1 / PX_RATIO}, ${1 / PX_RATIO});
