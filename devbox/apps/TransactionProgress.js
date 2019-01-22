39d6370047d563d3fd66c408b349bbc32e949448
Pierre Bertet <hello@pierre.world>
2019-01-22 20:25:38 +0100

    Popover changes
    - Supports a “center” placement.
    - Opening + closing transition (rather than opening only).
    - Throws an error if the component is not wrapped in <Root.Provider>.
    - The component closing behavior is now based on the focus rather than
      the click events outside, allowing to close it e.g. when a sandboxed iframe
      is clicked.
    
    API changes:
    
    - `openerRef` becomes `opener` (to better reflect that it’s a DOM
      element and not a React ref)
    - `left` and `right` were not playing nicely with Popper.js and have been
      removed.
    - A new prop, `visible` allows to declare the Popover element without
      condition, and to let the component handle its opening / close transitions.
    - A new placement, `center`, has been added, and centers the Popover on
      the opener element.

--- devbox/apps/TransactionProgress.js
+++ devbox/apps/TransactionProgress.js
@@ -11,53 +11,34 @@ import {
 
 class App extends React.Component {
   state = {
-    top: '0px',
-    left: '0px',
-    show: false,
+    showPopover: false,
   }
-  handleClose = () => {
-    this.setState({ show: false })
-  }
-  handleClick = () => {
-    this.setState(({ show }) => ({ show: !show }))
-  }
-  handleRef = ref => {
-    this.openerRef = ref
-  }
+  _opener = React.createRef()
   render() {
+    const { showPopover } = this.state
     const { title } = this.props
-    const { top, left, show } = this.state
     return (
       <AragonApp publicUrl="/aragon-ui/">
         <Root.Provider>
           <Wrapper>
             <div ref={this.handleRef}>
               <Button
+                ref={this._opener}
                 mode="secondary"
-                onClick={this.handleClick}
-                onClick={e =>
-                  this.setState({
-                    top: e.clientY + 'px',
-                    left: e.clientX + 'px',
-                    show: !show,
-                  })
-                }
                 compact
+                onClick={() => this.setState({ showPopover: true })}
               >
                 Open
               </Button>
-              {show && (
-                <TransactionProgress
-                  transactionHashUrl="https://etherscan.io/tx/0x5c504ed432cb51138bcf09aa5e8a410dd4a1e204ef84bfed1be16dfba1b22060"
-                  progress={0.3}
-                  endTime={new Date(Date.now() + 100000)}
-                  openerRef={this.openerRef}
-                  onClose={this.handleClose}
-                  top={top}
-                  left={left}
-                  slow
-                />
-              )}
+              <TransactionProgress
+                transactionHashUrl="https://etherscan.io/tx/0x5c504ed432cb51138bcf09aa5e8a410dd4a1e204ef84bfed1be16dfba1b22060"
+                progress={0.3}
+                endTime={new Date(Date.now() + 100000)}
+                onClose={() => this.setState({ showPopover: false })}
+                visible={showPopover}
+                opener={this._opener.current}
+                slow
+              />
             </div>
           </Wrapper>
         </Root.Provider>
