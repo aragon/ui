93124065d57d6bff9622ffe846193d774400019d
Pierre Bertet <hello@pierre.world>
2019-02-07 14:38:01 +0100

    Remove Viewport from this branch

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,18 +3,15 @@ import PropTypes from 'prop-types'
 import { ensureTrailingSlash } from '../../utils/url'
 import { PublicUrl } from '../../providers/PublicUrl'
 import { Root } from '../../providers/Root'
-import { Viewport } from '../../providers/Viewport'
 import BaseStyles from '../BaseStyles/BaseStyles'
 import { ToastHub } from '../ToastHub/ToastHub'
 
 const Main = ({ children, assetsUrl, legacyFonts }) => (
   <Root.Provider>
-    <Viewport.Provider>
-      <PublicUrl.Provider url={ensureTrailingSlash(assetsUrl)}>
-        <BaseStyles enableLegacyFonts={legacyFonts} />
-        <ToastHub>{children}</ToastHub>
-      </PublicUrl.Provider>
-    </Viewport.Provider>
+    <PublicUrl.Provider url={ensureTrailingSlash(assetsUrl)}>
+      <BaseStyles enableLegacyFonts={legacyFonts} />
+      <ToastHub>{children}</ToastHub>
+    </PublicUrl.Provider>
   </Root.Provider>
 )
 
