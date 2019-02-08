0a795a1a4937b641f125484ee79479f698a9c25b
Pierre Bertet <hello@pierre.world>
2019-02-08 14:16:31 +0100

    devbox: fix the RadioList demo

--- devbox/apps/Radio.js
+++ devbox/apps/Radio.js
@@ -7,50 +7,84 @@ const items = [
   ['Cherry', 'Strawberry', 'Banana', 'Apple'],
 ]
 
-const showRadioList = false
-
 class App extends React.Component {
   state = {
-    selected: [],
+    selected: [-1, -1],
+    showRadioList: false,
+  }
+  updateSelected(groupIndex, newId) {
+    this.setState(({ selected }) => ({
+      selected: selected.map((id, index) =>
+        index === groupIndex ? newId : id
+      ),
+    }))
   }
   render() {
-    const { selected } = this.state
+    const { showRadioList, selected } = this.state
     return (
       <Main>
-        <div style={{ display: 'flex' }}>
-          {!showRadioList &&
-            items.map((localItems, i) => (
-              <List key={i}>
-                <RadioGroup
-                  selected={selected[i]}
-                  onChange={id => {
-                    const newSelected = [...selected]
-                    newSelected[i] = id
-                    this.setState({ selected: newSelected })
-                  }}
-                >
-                  {localItems.map((item, i) => (
-                    <li key={item}>
-                      <Label>
-                        <Radio id={item} />
-                        <Item>{item}</Item>
-                      </Label>
-                    </li>
-                  ))}
-                </RadioGroup>
-              </List>
-            ))}
-        </div>
+        <div
+          css={`
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <h1
+            css={`
+              margin-bottom: 20px;
+              font-size: 20px;
+            `}
+            onClick={() =>
+              this.setState(({ showRadioList }) => ({
+                showRadioList: !showRadioList,
+              }))
+            }
+          >
+            {showRadioList ? 'RadioList' : 'RadioGroup'}
+          </h1>
 
-        {showRadioList && (
-          <RadioList
-            title="Action Requirement"
-            description="Here are some options you can use to perform it:"
-            items={items.map(item => ({ title: item, description: item }))}
-            onSelect={selected => this.setState({ selected })}
-            selected={selected}
-          />
-        )}
+          <div
+            css={`
+              display: flex;
+            `}
+          >
+            {!showRadioList &&
+              items.map((localItems, i) => (
+                <List key={i}>
+                  <RadioGroup
+                    selected={selected[i]}
+                    onChange={id => {
+                      this.updateSelected(i, id)
+                    }}
+                  >
+                    {localItems.map((item, i) => (
+                      <li key={item}>
+                        <Label>
+                          <Radio id={item} />
+                          <Item>{item}</Item>
+                        </Label>
+                      </li>
+                    ))}
+                  </RadioGroup>
+                </List>
+              ))}
+
+            {showRadioList && (
+              <RadioList
+                title="Action Requirement"
+                description="Here are some options you can use to perform it:"
+                items={items[0].map(item => ({
+                  title: item,
+                  description: item,
+                }))}
+                onChange={id => {
+                  this.updateSelected(0, id)
+                }}
+                selected={selected[0]}
+              />
+            )}
+          </div>
+        </div>
       </Main>
     )
   }
@@ -58,8 +92,11 @@ class App extends React.Component {
 
 const List = styled.ul`
   list-style: none;
-  margin: 0 20px;
+  margin: 0;
   padding: 0;
+  & + & {
+    margin-left: 20px;
+  }
 `
 
 const Label = styled.label`
