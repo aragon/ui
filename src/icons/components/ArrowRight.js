4099ffc37a933770f099727e10d4ba844e6f7f49
Gorka <gorka@aquigorka.com>
2019-02-13 10:58:19 +0100

    Icons: add arrow right component

---
+++ src/icons/components/ArrowRight.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+const ArrowRight = props => (
+  <svg width="12"
+    height="19"
+    viewBox="0 0 12 19"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M1.61309 1L10.5 8.98756"
+      stroke="#EAEAEA"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+    />
+    <path
+      d="M1.45117 17.7673L10.4512 8.97168"
+      stroke="#EAEAEA"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+    />
+  </svg>
+)
+
+export default ArrowRight
