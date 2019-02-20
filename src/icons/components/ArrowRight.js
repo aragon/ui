a4e712931f87af308048799c6c28be4baa83d0e9
Pierre Bertet <hello@pierre.world>
2019-02-20 18:30:46 +0100

    Re-generate icon components

--- src/icons/components/ArrowRight.js
+++ src/icons/components/ArrowRight.js
@@ -1,25 +1,8 @@
 import React from "react"
 
 const ArrowRight = props => (
-  <svg width="12"
-    height="19"
-    viewBox="0 0 12 19"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <path
-      d="M1.61309 1L10.5 8.98756"
-      stroke="#EAEAEA"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-    />
-    <path
-      d="M1.45117 17.7673L10.4512 8.97168"
-      stroke="#EAEAEA"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-    />
+  <svg width={12} height={19} viewBox="0 0 12 19" fill="none" {...props}>
+    <path d="M1.613 1L10.5 8.988m-9.049 8.779l9-8.795" stroke="currentColor" />
   </svg>
 )
 
--- src/icons/components/Plus.js
+++ src/icons/components/Plus.js
@@ -1,9 +1,8 @@
 import React from "react"
 
 const Plus = props => (
-  <svg width="24px" height="24px" viewBox="0 0 24 24" {...props}>
-     <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
-    <path d="M0 0h24v24H0z" fill="none" />
+  <svg width={24} height={24} viewBox="0 0 24 24" {...props}>
+    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="currentColor" />
   </svg>
 )
 
