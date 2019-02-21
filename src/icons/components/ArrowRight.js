c03b33fcb10934d99ee0750f2415dc2fa4f47ec4
Gorka Ludlow <gorka@aquigorka.com>
2019-02-21 09:42:41 +0100

    Add IconPlus and IconArrowRight components (#319)
    * SVGs: add plus svg
    
    * Icons: add plus icon component
    
    * Icons: add plus icon component to index
    
    * SVGs: add arrow right svg
    
    * Icons: add arrow right component
    
    * Icons: add arrow right component to index
    
    * Optimize SVG files
    
    * Use dynamic color
    
    * Re-generate icon components
    
    * Add icons to the gallery

---
+++ src/icons/components/ArrowRight.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ArrowRight = props => (
+  <svg width={12} height={19} viewBox="0 0 12 19" fill="none" {...props}>
+    <path d="M1.613 1L10.5 8.988m-9.049 8.779l9-8.795" stroke="currentColor" />
+  </svg>
+)
+
+export default ArrowRight
