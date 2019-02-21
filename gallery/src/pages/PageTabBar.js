de61ba0af90373308b8290e2a082a2f800a107b5
Pierre Bertet <hello@pierre.world>
2019-02-21 14:25:11 +0100

    TabBar gallery demo: move to useState()

--- gallery/src/pages/PageTabBar.js
+++ gallery/src/pages/PageTabBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { TabBar } from '@aragon/ui'
 import Page from 'comps/Page/Page'
@@ -7,37 +7,26 @@ import readme from 'ui-src/components/TabBar/README.md'
 
 const ITEMS = ['Strawberry', 'Banana', 'Apple', 'Cherry']
 
-class PageTabBar extends React.Component {
-  state = { selected: 0 }
-  handleChange = selected => {
-    this.setState({ selected })
-  }
-  render() {
-    const { title } = this.props
-    const { selected } = this.state
-    return (
-      <Page title={title} readme={readme}>
-        <Page.Demo opaque>
-          <div
-            css={`
-              display: flex;
-              width: 100%;
-              height: 200px;
-              overflow-x: auto;
-              justify-content: center;
-              align-items: center;
-            `}
-          >
-            <TabBar
-              items={ITEMS}
-              selected={selected}
-              onChange={this.handleChange}
-            />
-          </div>
-        </Page.Demo>
-      </Page>
-    )
-  }
+const PageTabBar = ({ title }) => {
+  const [selected, setSelected] = useState(0)
+  return (
+    <Page title={title} readme={readme}>
+      <Page.Demo opaque>
+        <div
+          css={`
+            display: flex;
+            width: 100%;
+            height: 200px;
+            overflow-x: auto;
+            justify-content: center;
+            align-items: center;
+          `}
+        >
+          <TabBar items={ITEMS} selected={selected} onChange={setSelected} />
+        </div>
+      </Page.Demo>
+    </Page>
+  )
 }
 
 export default PageTabBar
