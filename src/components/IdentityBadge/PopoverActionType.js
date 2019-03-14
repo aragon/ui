5a4f33e070c4b517abcc81eb7a4946f6cc4799e2
Gorka <gorka@aquigorka.com>
2019-03-14 13:23:08 +0000

    PopoverActionType: update shape

--- src/components/IdentityBadge/PopoverActionType.js
+++ src/components/IdentityBadge/PopoverActionType.js
@@ -3,5 +3,4 @@ import PropTypes from 'prop-types'
 export default PropTypes.shape({
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
-  title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
 })
