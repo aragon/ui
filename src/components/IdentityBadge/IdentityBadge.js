1b7181609171eab9c066b95f34a7156347d517a4
Pierre Bertet <hello@pierre.world>
2019-03-26 14:20:35 +0100

    ESLint fixes

--- src/components/IdentityBadge/IdentityBadge.js
+++ src/components/IdentityBadge/IdentityBadge.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 import { isAddress, shortenAddress, stylingProps } from '../../utils'
 import { theme } from '../../theme'
 import Text from '../Text/Text'
