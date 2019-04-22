4b023fddac24f6e10a8a14f8aca9c36141a525da
Pierre Bertet <hello@pierre.world>
2019-04-22 20:42:18 +0200

    Update devbox demo to test the positioning

--- devbox/apps/Popover.js
+++ devbox/apps/Popover.js
@@ -2,9 +2,22 @@ import React, { useRef, useState, useCallback } from 'react'
 import styled from 'styled-components'
 import { Popover, Root, Button } from '@aragon/ui'
 
+const POSITIONS = [
+  ['flex-start', 'flex-start'],
+  ['center', 'flex-start'],
+  ['flex-end', 'flex-start'],
+  ['flex-start', 'center'],
+  ['center', 'center'],
+  ['flex-end', 'center'],
+  ['flex-start', 'flex-end'],
+  ['center', 'flex-end'],
+  ['flex-end', 'flex-end'],
+]
+
 function App({ title }) {
   const [show, setShow] = useState(false)
   const [opener, setOpener] = useState(null)
+  const [position, setPosition] = useState(4)
 
   const handleOpenerRef = useCallback(node => {
     setOpener(node)
@@ -14,14 +27,52 @@ function App({ title }) {
     <div
       css={`
         display: grid;
-        width: 800px;
-        margin: 0 auto;
-        justify-content: center;
-        align-items: center;
+        width: 100vw;
         height: 100vh;
+        padding: 20px;
+        justify-content: ${POSITIONS[position][0]};
+        align-items: ${POSITIONS[position][1]};
       `}
     >
       <div>
+        <div
+          css={`
+            position: absolute;
+            top: ${position === 4 ? 'calc(50% - 150px)' : 'calc(50% - 50px)'};
+            left: calc(50% - 50px);
+            display: grid;
+            width: 100px;
+            height: 100px;
+            grid-template-columns: 1fr 1fr 1fr;
+            grid-template-rows: 1fr 1fr 1fr;
+            grid-gap: 5px;
+            div {
+              border-radius: 3px;
+              border: 2px solid hsl(200, 70%, 70%);
+              cursor: pointer;
+              &:active {
+                background: hsl(200, 70%, 70%);
+              }
+              &.active {
+                background: hsl(200, 70%, 70%);
+              }
+            }
+          `}
+          onClick={e => {
+            const index = Number(e.target.dataset.index)
+            if (!isNaN(index)) {
+              setPosition(index)
+            }
+          }}
+        >
+          {[...Array(9)].map((_, i) => (
+            <div
+              key={i}
+              data-index={i}
+              className={i === position ? 'active' : ''}
+            />
+          ))}
+        </div>
         <Button
           ref={handleOpenerRef}
           onClick={() => setShow(true)}
