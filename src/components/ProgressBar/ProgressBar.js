cbbf9529d364139488b288b7a30ae57c4357b56b
Pierre Bertet <hello@pierre.world>
2019-04-26 18:35:19 +0200

    ProgressBar: indeterminate state + prop change
    - The prop is now named “value” rather than “progress”, to align with
      the other components and with the <progress> element in HTML.
    - When set to -1 or unset, a new indeterminate state is displayed.
    - Other improvements related to style, transition and performances.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,55 +1,99 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from '../../proptypes'
 import styled from 'styled-components'
-import { Spring, animated } from 'react-spring'
+import { useSpring, animated } from 'react-spring'
 import { springs } from '../../utils/styles'
+import { pause } from '../../utils/miscellaneous'
+import { warn } from '../../utils/environment'
+import { lerp } from '../../utils/math'
 import { theme } from '../../theme'
 
-const ProgressBar = ({ color, progress }) => (
-  <Spring
-    native
-    config={springs.smooth}
-    from={{ progressValue: 0 }}
-    to={{ progressValue: progress }}
-  >
-    {({ progressValue }) => (
-      <Main>
-        <Base>
-          <Progress
-            color={color}
-            style={{
-              transform: progressValue.interpolate(t => `scaleX(${t})`),
-            }}
-          />
-        </Base>
-      </Main>
-    )}
-  </Spring>
-)
+const BAR_HEIGHT = 6
+const RADIUS = 2
+
+const transformCss = (scale, translate) =>
+  `scale3d(${scale}, 1, 1) translate3d(${translate}%, 0, 0)`
+
+const springNormal = value => ({
+  config: { ...springs.smooth, precision: 0.001 },
+  from: { transform: transformCss(0, 0) },
+  to: { transform: transformCss(value, 0) },
+  reset: false,
+})
+
+const springIndeterminate = () => ({
+  config: {
+    ...springs.sluggish,
+    precision: 0.01,
+    clamp: true,
+  },
+  from: { transform: transformCss(0.5, -100) },
+  to: async next => {
+    while (true) {
+      await next({ transform: transformCss(0.5, 200) })
+      await pause(800)
+    }
+  },
+  reset: true,
+})
+
+function ProgressBar({ color, progress, value }) {
+  // Support `progress` for a while
+  if (value === -1 && typeof progress === 'number') {
+    value = progress
+    if (!ProgressBar._warned) {
+      warn(
+        'The `progress` prop of ProgressBar is deprecated: please use `value` instead.'
+      )
+      ProgressBar._warned = true
+    }
+  }
+
+  // The indeterminate state can be triggered either by not setting the value
+  // (to mimic the <progress> element in HTML), or by setting the -1 value (for
+  // convenience in React).
+  const indeterminate = value === -1
+
+  const transition = useSpring(
+    indeterminate ? springIndeterminate() : springNormal(value)
+  )
+
+  return (
+    <div
+      css={`
+        width: 100%;
+        align-items: center;
+      `}
+    >
+      <div
+        css={`
+          width: 100%;
+          height: ${BAR_HEIGHT}px;
+          background: ${theme.secondaryBackground};
+          border-radius: ${RADIUS}px;
+          overflow: hidden;
+        `}
+      >
+        <Bar color={color} style={transition} />
+      </div>
+    </div>
+  )
+}
 
 ProgressBar.defaultProps = {
-  progress: 0,
+  value: -1,
 }
 
 ProgressBar.propTypes = {
   color: PropTypes.string,
-  progress: PropTypes.number,
+  progress: PropTypes._0to1,
+  value: PropTypes.oneOfType([PropTypes._0to1, PropTypes.oneOf([-1])]),
 }
 
-const Main = styled.div`
-  width: 100%;
-  align-items: center;
-`
-const Base = styled.div`
-  width: 100%;
-  height: 8px;
-  background: #edf3f6;
-  border-radius: 2px;
-`
-const Progress = styled(animated.div)`
-  height: 8px;
+const Bar = styled(animated.div)`
+  height: ${BAR_HEIGHT}px;
   background: ${({ color }) => color || theme.accent};
-  border-radius: 2px;
   transform-origin: 0 0;
 `
+
 export default ProgressBar
