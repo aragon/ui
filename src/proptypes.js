d03ef28563e72b6b7388de26f224497c47b3233d
Pierre Bertet <hello@pierre.world>
2019-04-26 18:33:41 +0200

    Add a new proptype to check 0 => 1 values

--- src/proptypes.js
+++ src/proptypes.js
@@ -1,5 +1,39 @@
 import PropTypes from 'prop-types'
 
+// Check if a value is “empty”, in the prop-types sense (null or undefined)
+function isEmpty(value) {
+  return value === undefined || value === null
+}
+
+// Require a prop to not be empty
+function requireProp(props, propName, componentName) {
+  return isEmpty()
+    ? new Error(
+        `The prop \`${propName}\` is required for \`${componentName}\`.`
+      )
+    : null
+}
+
+// Accept any number in the 0 => 1 range
+function _0to1(props, propName, componentName) {
+  if (isEmpty()) {
+    return null
+  }
+  if (
+    typeof props[propName] === 'number' &&
+    props[propName] >= 0 &&
+    props[propName] <= 1
+  ) {
+    return null
+  }
+  return new Error(
+    `Invalid prop \`${propName}\` supplied to \`${componentName}\`. Please provide a number in the 0-1 range.`
+  )
+}
+
+// Required version
+_0to1.isRequired = (...params) => requireProp(...params) || _0to1(...params)
+
 const ExtendedPropTypes = {
   ...PropTypes,
   _component: PropTypes.oneOfType([
@@ -14,6 +48,7 @@ const ExtendedPropTypes = {
     precision: PropTypes.number,
   }),
   _null: PropTypes.oneOf([null]),
+  _0to1,
 }
 
 export default ExtendedPropTypes
