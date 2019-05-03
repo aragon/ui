3599f33afac53b6b1d979dbb3c1f2dc3154d5736
Pierre Bertet <hello@pierre.world>
2019-05-03 12:51:47 +0200

    devbox: auto generate the apps index (#362)

--- devbox/index.js
+++ devbox/index.js
@@ -2,56 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Main } from '@aragon/ui'
 
-import AddressField from './apps/AddressField'
-import AppBarTabs from './apps/AppBarTabs'
-import AppView from './apps/AppView'
-import Button from './apps/Button'
-import Checkbox from './apps/Checkbox'
-import EmptyStateCard from './apps/EmptyStateCard'
-import EthIdenticon from './apps/EthIdenticon'
-import IdentityBadge from './apps/IdentityBadge'
-import Input from './apps/Input'
-import LineChart from './apps/LineChart'
-import LinkedSliders from './apps/LinkedSliders'
-import Modal from './apps/Modal'
-import NavigationBar from './apps/NavigationBar'
-import PartitionBar from './apps/PartitionBar'
-import Popover from './apps/Popover'
-import ProgressBar from './apps/ProgressBar'
-import Radio from './apps/Radio'
-import Scratchpad from './apps/Scratchpad'
-import SidePanel from './apps/SidePanel'
-import TabBar from './apps/TabBar'
-import Timer from './apps/Timer'
-import TransactionBadge from './apps/TransactionBadge'
-import TransactionProgress from './apps/TransactionProgress'
-import Viewport from './apps/Viewport'
-
-const APPS = {
-  AddressField,
-  AppBarTabs,
-  AppView,
-  Button,
-  Checkbox,
-  EmptyStateCard,
-  EthIdenticon,
-  IdentityBadge,
-  Input,
-  LineChart,
-  LinkedSliders,
-  Modal,
-  NavigationBar,
-  PartitionBar,
-  Popover,
-  ProgressBar,
-  Radio,
-  SidePanel,
-  TabBar,
-  Timer,
-  TransactionBadge,
-  TransactionProgress,
-  Viewport,
-}
+import * as APPS from './apps'
 
 class Index extends React.Component {
   state = {
