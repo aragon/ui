b725d0de9ee1f0f6eb65c1f15957604c8c71caf2
Pierre Bertet <hello@pierre.world>
2019-05-08 20:46:48 +0200

    ESLint fixes

--- src/hooks/useImageExists.js
+++ src/hooks/useImageExists.js
@@ -5,37 +5,34 @@ export function useImageExists(src) {
   const [exists, setExists] = useState(false)
   const [loading, setLoading] = useState(false)
 
-  useEffect(
-    () => {
-      let image = new Image()
+  useEffect(() => {
+    let image = new Image()
 
-      const done = () => {
-        if (image) {
-          image.removeEventListener('load', onLoad)
-          image = null
-        }
+    const done = () => {
+      if (image) {
+        image.removeEventListener('load', onLoad)
+        image = null
       }
+    }
 
-      const onLoad = () => {
-        setLoading(false)
-        setExists(true)
-        done()
-      }
+    const onLoad = () => {
+      setLoading(false)
+      setExists(true)
+      done()
+    }
 
+    setExists(false)
+    setLoading(true)
+
+    image.addEventListener('load', onLoad)
+    image.src = src
+
+    return () => {
+      setLoading(false)
       setExists(false)
-      setLoading(true)
-
-      image.addEventListener('load', onLoad)
-      image.src = src
-
-      return () => {
-        setLoading(false)
-        setExists(false)
-        done()
-      }
-    },
-    [src]
-  )
+      done()
+    }
+  }, [src])
 
   return useMemo(() => ({ src, exists, loading }), [src, exists, loading])
 }
