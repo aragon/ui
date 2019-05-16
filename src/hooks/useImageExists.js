affbfe037422d22b9440b366a252d871530e10c3
Pierre Bertet <hello@pierre.world>
2019-05-16 11:49:57 +0200

    useImageExists: set loading at true by default

--- src/hooks/useImageExists.js
+++ src/hooks/useImageExists.js
@@ -6,7 +6,7 @@ const srcCache = cachedMap()
 // Check if a remote image exists.
 export function useImageExists(src) {
   const [exists, setExists] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   useEffect(
     () => {
@@ -19,13 +19,13 @@ export function useImageExists(src) {
           return
         }
 
-        setExists(false)
-        setLoading(true)
-
         if (srcCache.get(src)) {
           success()
           return
         }
+
+        setExists(false)
+        setLoading(true)
 
         // TODO: ensure only one image is loading at a time for a given src.
         image.addEventListener('load', success)
@@ -48,14 +48,8 @@ export function useImageExists(src) {
 
       init()
 
-      return () => {
-        setLoading(false)
-        setExists(false)
-        done()
-      }
-    },
-    [src]
-  )
+      return done
+    }, [src])
 
   return useMemo(() => ({ src, exists, loading }), [src, exists, loading])
 }
