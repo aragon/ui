49501cc68251cd5e7721c08160f86bbe0e312de6
Pierre Bertet <hello@pierre.world>
2019-05-16 12:43:14 +0200

    useImageExists() initial state (#375)

--- src/hooks/useImageExists.js
+++ src/hooks/useImageExists.js
@@ -6,56 +6,49 @@ const srcCache = cachedMap()
 // Check if a remote image exists.
 export function useImageExists(src) {
   const [exists, setExists] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
 
-  useEffect(
-    () => {
-      let image = new Image()
+  useEffect(() => {
+    let image = new Image()
 
-      const init = () => {
-        if (!src) {
-          setExists(false)
-          setLoading(false)
-          return
-        }
-
+    const init = () => {
+      if (!src) {
         setExists(false)
-        setLoading(true)
-
-        if (srcCache.get(src)) {
-          success()
-          return
-        }
-
-        // TODO: ensure only one image is loading at a time for a given src.
-        image.addEventListener('load', success)
-        image.src = src
-      }
-
-      const success = () => {
         setLoading(false)
-        setExists(true)
-        srcCache.set(src, true)
-        done()
+        return
       }
 
-      const done = () => {
-        if (image) {
-          image.removeEventListener('load', success)
-          image = null
-        }
+      if (srcCache.get(src)) {
+        success()
+        return
       }
 
-      init()
+      setExists(false)
+      setLoading(true)
 
-      return () => {
-        setLoading(false)
-        setExists(false)
-        done()
+      // TODO: ensure only one image is loading at a time for a given src.
+      image.addEventListener('load', success)
+      image.src = src
+    }
+
+    const success = () => {
+      setLoading(false)
+      setExists(true)
+      srcCache.set(src, true)
+      done()
+    }
+
+    const done = () => {
+      if (image) {
+        image.removeEventListener('load', success)
+        image = null
       }
-    },
-    [src]
-  )
+    }
+
+    init()
+
+    return done
+  }, [src])
 
   return useMemo(() => ({ src, exists, loading }), [src, exists, loading])
 }
