3bd6f126d9f1d36bd5484a9d96d503905e1fa41c
Pierre Bertet <hello@pierre.world>
2019-06-12 12:13:38 +0200

    Update devbox Layout

--- devbox/apps/Layout.js
+++ devbox/apps/Layout.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import { Layout } from '@aragon/ui'
+import { Layout, useLayout } from '@aragon/ui'
 
-class App extends React.Component {
-  render() {
-    return <Layout>something</Layout>
-  }
+function App() {
+  const { name, width } = useLayout()
+  return (
+    <div
+      css={`
+        width: 100vw;
+        height: 100vh;
+        background: #789;
+      `}
+    >
+      <Layout>
+        <div
+          css={`
+            height: 100%;
+            width: 100%;
+            border: 2px solid #FFF;
+            color: #fff;
+            font-size: 30px;
+            padding: 40px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          `}
+        >
+          Layout: {name} ({width}px)
+        </div>
+      </Layout>
+    </div>
+  )
 }
 
 export default App
