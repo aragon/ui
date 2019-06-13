53d9f2e1cc77e107fb755ea93282c0a93a92ebe0
Pierre Bertet <hello@pierre.world>
2019-06-13 19:49:03 +0200

    color(): reject empty values

--- src/utils/color.js
+++ src/utils/color.js
@@ -9,7 +9,7 @@ function rgbFromRgbInt(rgbInt) {
 }
 
 function normalizeHexColor(value) {
-  const matches = value.match(RGB_HEX_RE)
+  const matches = value ? value.match(RGB_HEX_RE) : null
 
   if (matches === null) {
     throw new Error(
