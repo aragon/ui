14dbbb2f4abc9b29f73afb74d748d5797abb2119
Pierre Bertet <hello@pierre.world>
2019-06-13 19:50:04 +0200

    Add a Box component
    This component can optionally take a header, and represents the simplest
    interpretation of a surface, as defined in the design system.

---
+++ src/components/Box/Box.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { useTheme, GU, RADIUS } from '@aragon/ui'
+
+function Box({ heading, children, padding, ...props }) {
+  const theme = useTheme()
+
+  return (
+    <section
+      css={`
+        border-radius: ${RADIUS}px;
+        border: 1px solid ${theme.border};
+        background: ${theme.surface};
+        color: ${theme.surfaceContent};
+        & + & {
+          margin-top: ${2 * GU}px;
+        }
+      `}
+      {...props}
+    >
+      {heading && (
+        <div
+          css={`
+            display: flex;
+            align-items: center;
+            height: ${4 * GU}px;
+            border-bottom: 1px solid ${theme.border};
+          `}
+        >
+          <HeaderContent heading={heading} />
+        </div>
+      )}
+      <div
+        css={`
+          padding: ${padding ? 2 * GU : 0}px;
+        `}
+      >
+        <div>{children}</div>
+      </div>
+    </section>
+  )
+}
+
+Box.propTypes = {
+  heading: PropTypes.node,
+  children: PropTypes.node,
+  padding: PropTypes.bool,
+}
+
+Box.defaultProps = {
+  padding: true,
+}
+
+function HeaderContent({ heading }) {
+  const theme = useTheme()
+
+  if (!heading) {
+    return null
+  }
+
+  if (typeof heading !== 'string') {
+    return heading
+  }
+
+  return (
+    <h1
+      css={`
+        padding-left: ${2 * GU}px;
+        font-size: 12px;
+        text-transform: uppercase;
+        color: ${theme.surfaceContentSecondary};
+      `}
+    >
+      {heading}
+    </h1>
+  )
+}
+
+export { Box }
+export default Box
