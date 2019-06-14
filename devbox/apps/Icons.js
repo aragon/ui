26b96e66e8bbf00709ae49bb4d3b484436c5827b
Pierre Bertet <hello@pierre.world>
2019-06-14 22:06:31 +0200

    devbox: fix the #Icons layout

--- devbox/apps/Icons.js
+++ devbox/apps/Icons.js
@@ -1,74 +1,92 @@
 import React from 'react'
-import { AppView, GU, RADIUS, useTheme } from '@aragon/ui'
+import { AppView, GU, RADIUS, useTheme, useLayout } from '@aragon/ui'
 import * as icons from '../all-icons'
 import ToggleTheme from '../components/ToggleTheme'
 
+function useCols() {
+  const { name } = useLayout()
+  if (name === 'large') return 5
+  if (name === 'medium') return 4
+  return 2
+}
+
 function Icons() {
   const theme = useTheme()
+  const cols = useCols()
   return (
     <ToggleTheme>
-      <AppView>
+      <div
+        css={`
+          font-size: 26px;
+          color: ${theme.content};
+          text-align: center;
+          padding: ${4 * GU}px 0;
+        `}
+      >
+        aragonUI icons
+      </div>
+
+      <div
+        css={`
+          display: grid;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
         <div
           css={`
             display: grid;
-            justify-content: center;
-            align-items: center;
+            grid-template-columns: repeat(${cols}, ${18 * GU}px);
+            grid-template-rows: repeat(auto-fill, ${12 * GU}px);
+            grid-gap: ${2 * GU}px;
+            padding: 0 0 ${4 * GU}px;
           `}
         >
-          <div
-            css={`
-              display: grid;
-              grid-template-columns: repeat(5, ${20 * GU}px);
-              grid-template-rows: repeat(auto-fill, ${12 * GU}px);
-              grid-gap: ${2 * GU}px;
-              padding: ${2 * GU}px 0;
-            `}
-          >
-            {Object.entries(icons).map(([name, Icon]) => (
+          {Object.entries(icons).map(([name, Icon]) => (
+            <div
+              key={name}
+              css={`
+                display: flex;
+                flex-direction: column;
+                padding: ${2 * GU}px 0;
+                color: ${theme.surfaceContent};
+                background: ${theme.surface};
+                border: 1px solid ${theme.border};
+                border-radius: ${RADIUS}px;
+                height: ${12 * GU}px;
+              `}
+            >
+              <div
+                css={`
+                  display: grid;
+                  grid-template-columns: repeat(3, auto);
+                  grid-gap: ${1 * GU}px;
+                  width: 100%;
+                  justify-content: center;
+                  align-items: center;
+                  flex-grow: 1;
+                `}
+              >
+                <Icon size="tiny" />
+                <Icon size="small" />
+                <Icon size="medium" />
+              </div>
               <div
                 css={`
                   display: flex;
-                  flex-direction: column;
-                  padding: ${2 * GU}px 0;
-                  color: ${theme.surfaceContent};
-                  background: ${theme.surface};
-                  border: 1px solid ${theme.border};
-                  border-radius: ${RADIUS}px;
-                  height: ${12 * GU}px;
+                  width: 100%;
+                  justify-content: center;
+                  flex-shrink: 1;
+                  font-size: 14px;
                 `}
               >
-                <div
-                  css={`
-                    display: grid;
-                    grid-template-columns: repeat(3, auto);
-                    grid-gap: ${1 * GU}px;
-                    width: 100%;
-                    justify-content: center;
-                    align-items: center;
-                    flex-grow: 1;
-                  `}
-                >
-                  <Icon size="tiny" />
-                  <Icon size="small" />
-                  <Icon size="medium" />
-                </div>
-                <div
-                  css={`
-                    display: flex;
-                    width: 100%;
-                    justify-content: center;
-                    flex-shrink: 1;
-                    font-size: 14px;
-                  `}
-                >
-                  <span css="opacity: 0.3">Icon</span>
-                  {name.replace(/^Icon/, '')}
-                </div>
+                <span css="opacity: 0.3">Icon</span>
+                {name.replace(/^Icon/, '')}
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-      </AppView>
+      </div>
     </ToggleTheme>
   )
 }
