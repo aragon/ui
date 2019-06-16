02a1bb2fbd1230a445428925d07f339ff5d5c7af
Pierre Bertet <hello@pierre.world>
2019-06-16 11:29:53 +0200

    BaseStyles: remove support for legacy fonts

--- src/components/BaseStyles/BaseStyles.js
+++ src/components/BaseStyles/BaseStyles.js
@@ -4,51 +4,31 @@ import { createGlobalStyle } from 'styled-components'
 import { useTheme } from '../../theme'
 import { PublicUrl } from '../../providers/PublicUrl'
 
-import overpassLightWoff from './assets/overpass/overpass-light.woff'
 import overpassLightWoff2 from './assets/overpass/overpass-light.woff2'
-import overpassRegularWoff from './assets/overpass/overpass-regular.woff'
 import overpassRegularWoff2 from './assets/overpass/overpass-regular.woff2'
-import overpassSemiBoldWoff from './assets/overpass/overpass-semibold.woff'
 import overpassSemiBoldWoff2 from './assets/overpass/overpass-semibold.woff2'
-
-import overpassMonoLightWoff from './assets/overpass-mono/overpass-mono-light.woff'
 import overpassMonoLightWoff2 from './assets/overpass-mono/overpass-mono-light.woff2'
 
 // Using generic names so we can change the font if needed, while using these
 // generic names in our components.
-const DEFAULT_FONT_FAMILY = 'aragon-ui'
-const MONOSPACE_FONT_FAMILY = 'aragon-ui-monospace'
+export const DEFAULT_FONT_FAMILY = 'aragon-ui'
+export const MONOSPACE_FONT_FAMILY = 'aragon-ui-monospace'
 
 const DEFAULT_FONTS = {
-  '400': [
-    { url: overpassLightWoff2, format: 'woff2' },
-    { url: overpassLightWoff, format: 'woff', legacy: true },
-  ],
-  '600': [
-    { url: overpassRegularWoff2, format: 'woff2' },
-    { url: overpassRegularWoff, format: 'woff', legacy: true },
-  ],
-  '800': [
-    { url: overpassSemiBoldWoff2, format: 'woff2' },
-    { url: overpassSemiBoldWoff, format: 'woff', legacy: true },
-  ],
+  '400': { url: overpassLightWoff2, format: 'woff2' },
+  '600': { url: overpassRegularWoff2, format: 'woff2' },
+  '800': { url: overpassSemiBoldWoff2, format: 'woff2' },
 }
 
 const MONOSPACE_FONTS = {
-  '400': [
-    { url: overpassMonoLightWoff2, format: 'woff2' },
-    { url: overpassMonoLightWoff, format: 'woff', legacy: true },
-  ],
+  '400': { url: overpassMonoLightWoff2, format: 'woff2' },
 }
 
-function fontSrc(sources, { publicUrl, enableLegacyFonts }) {
-  return sources
-    .filter(({ legacy }) => !legacy || (legacy && enableLegacyFonts))
-    .map(({ url, format }) => `url(${publicUrl + url}) format('${format}')`)
-    .join(', ')
+function fontSrc(publicUrl, { url, format }) {
+  return `url(${publicUrl + url}) format('${format}')`
 }
 
-function fontFaceDeclarations({ fontFamily, publicUrl, enableLegacyFonts }) {
+function fontFaceDeclarations({ fontFamily, publicUrl }) {
   // No need to declare the font faces if the font family has changed.
   if (fontFamily !== BaseStyles.defaultProps.fontFamily) {
     return ''
@@ -56,28 +36,25 @@ function fontFaceDeclarations({ fontFamily, publicUrl, enableLegacyFonts }) {
   return `
       @font-face {
         font-family: ${DEFAULT_FONT_FAMILY};
-        src: ${fontSrc(DEFAULT_FONTS['400'], { publicUrl, enableLegacyFonts })};
+        src: ${fontSrc(publicUrl, DEFAULT_FONTS['400'])};
         font-weight: 400;
         font-style: normal;
       }
       @font-face {
         font-family: ${DEFAULT_FONT_FAMILY};
-        src: ${fontSrc(DEFAULT_FONTS['600'], { publicUrl, enableLegacyFonts })};
+        src: ${fontSrc(publicUrl, DEFAULT_FONTS['600'])};
         font-weight: 600;
         font-style: normal;
       }
       @font-face {
         font-family: ${DEFAULT_FONT_FAMILY};
-        src: ${fontSrc(DEFAULT_FONTS['800'], { publicUrl, enableLegacyFonts })};
+        src: ${fontSrc(publicUrl, DEFAULT_FONTS['800'])};
         font-weight: 800;
         font-style: normal;
       }
       @font-face {
         font-family: ${MONOSPACE_FONT_FAMILY};
-        src: ${fontSrc(MONOSPACE_FONTS['400'], {
-          publicUrl,
-          enableLegacyFonts,
-        })};
+        src: ${fontSrc(publicUrl, MONOSPACE_FONTS['400'])};
         font-weight: 400;
         font-style: normal;
       }
@@ -97,21 +74,20 @@ const BaseStyles = React.memo(function BaseStyles(props) {
 
 BaseStyles.propTypes = {
   publicUrl: PropTypes.string,
-  enableLegacyFonts: PropTypes.bool,
   fontFamily: PropTypes.string,
 }
 
 BaseStyles.defaultProps = {
   publicUrl: '/',
-  enableLegacyFonts: false,
   fontFamily: `${DEFAULT_FONT_FAMILY}, sans-serif`,
 }
 
 const GlobalStyle = createGlobalStyle`
+
+  // @font-face declarations
   ${p => (p.fontFaces ? p.fontFaces : '')}
-  *,
-  *:before,
-  *:after {
+
+  *, *:before, *:after {
     box-sizing: border-box;
   }
   html {
@@ -127,42 +103,20 @@ const GlobalStyle = createGlobalStyle`
     color: ${p => p.theme.content};
     background: ${p => p.theme.background};
   }
-  body,
-  ul,
-  p,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
+  body, ul, p, h1, h2, h3, h4, h5, h6 {
     margin: 0;
     padding: 0;
   }
-  button,
-  select,
-  input,
-  textarea,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
+  button, select, input, textarea, h1, h2, h3, h4, h5, h6 {
     font-size: inherit;
     font-family: inherit;
     font-weight: inherit;
     line-height: inherit;
   }
-  a,
-  button,
-  select,
-  input,
-  textarea {
+  a, button, select, input, textarea {
     color: inherit;
   }
-  strong,
-  b {
+  strong, b {
     font-weight: 600;
   }
   ::selection {
