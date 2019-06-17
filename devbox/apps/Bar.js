b5e4f9dc82aefdf5fcefd9831f3844bb577a966d
Pierre Bertet <hello@pierre.world>
2019-06-17 18:35:23 +0200

    devbox: Bar layout

--- devbox/apps/Bar.js
+++ devbox/apps/Bar.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { Button, AppView, Bar, TabBar } from '@aragon/ui'
 
 export default () => (
-  <AppView>
+  <div
+    css={`
+      display: flex;
+      align-items: center;
+      height: 100vh;
+    `}
+  >
     <Bar>
       <TabBar items={['App permissions', 'System permissions']} />
     </Bar>
-  </AppView>
+  </div>
 )
