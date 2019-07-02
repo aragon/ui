aragon/ui

commit 3599f33afac53b6b1d979dbb3c1f2dc3154d5736

devbox: auto generate the apps index (#362)

devbox/index.js
@@ -2,56 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Main } from '@aragon/ui'
 
-import AddressField from './apps/AddressField'
-import AppBarTabs from './apps/AppBarTabs'
-import AppView from './apps/AppView'
-import Button from './apps/Button'
-import Checkbox from './apps/Checkbox'
-import EmptyStateCard from './apps/EmptyStateCard'
-import EthIdenticon from './apps/EthIdenticon'
-import IdentityBadge from './apps/IdentityBadge'
-import Input from './apps/Input'
-import LineChart from './apps/LineChart'
-import LinkedSliders from './apps/LinkedSliders'
-import Modal from './apps/Modal'
-import NavigationBar from './apps/NavigationBar'
-import PartitionBar from './apps/PartitionBar'
-import Popover from './apps/Popover'
-import ProgressBar from './apps/ProgressBar'
-import Radio from './apps/Radio'
-import Scratchpad from './apps/Scratchpad'
-import SidePanel from './apps/SidePanel'
-import TabBar from './apps/TabBar'
-import Timer from './apps/Timer'
-import TransactionBadge from './apps/TransactionBadge'
-import TransactionProgress from './apps/TransactionProgress'
-import Viewport from './apps/Viewport'
-
-const APPS = {
-  AddressField,
-  AppBarTabs,
-  AppView,
-  Button,
-  Checkbox,
-  EmptyStateCard,
-  EthIdenticon,
-  IdentityBadge,
-  Input,
-  LineChart,
-  LinkedSliders,
-  Modal,
-  NavigationBar,
-  PartitionBar,
-  Popover,
-  ProgressBar,
-  Radio,
-  SidePanel,
-  TabBar,
-  Timer,
-  TransactionBadge,
-  TransactionProgress,
-  Viewport,
-}
+import * as APPS from './apps'
 
 class Index extends React.Component {
   state = {

devbox/apps/index.js
@@ -1,0 +1,29 @@
+
+// This file has been generated by running scripts/update-apps-index.js.
+// Do not edit, manual changes will be overwritten.
+
+export { default as AddressField } from './AddressField'
+export { default as AppBarTabs } from './AppBarTabs'
+export { default as AppView } from './AppView'
+export { default as Button } from './Button'
+export { default as Checkbox } from './Checkbox'
+export { default as DropDown } from './DropDown'
+export { default as EmptyStateCard } from './EmptyStateCard'
+export { default as EthIdenticon } from './EthIdenticon'
+export { default as IdentityBadge } from './IdentityBadge'
+export { default as Input } from './Input'
+export { default as LineChart } from './LineChart'
+export { default as LinkedSliders } from './LinkedSliders'
+export { default as Modal } from './Modal'
+export { default as NavigationBar } from './NavigationBar'
+export { default as PartitionBar } from './PartitionBar'
+export { default as Popover } from './Popover'
+export { default as ProgressBar } from './ProgressBar'
+export { default as Radio } from './Radio'
+export { default as Scratchpad } from './Scratchpad'
+export { default as SidePanel } from './SidePanel'
+export { default as TabBar } from './TabBar'
+export { default as Timer } from './Timer'
+export { default as TransactionBadge } from './TransactionBadge'
+export { default as TransactionProgress } from './TransactionProgress'
+export { default as Viewport } from './Viewport'

devbox/scripts/update-apps-index.js
@@ -1,0 +1,26 @@
+const { readdirSync, writeFileSync } = require('fs')
+const { basename, join } = require('path')
+
+const green = '\x1b[32m'
+const white = '\x1b[37m'
+
+const filePath = join(__dirname, '../apps/index.js')
+
+let content = ''
+
+content += `
+// This file has been generated by running scripts/update-apps-index.js.
+// Do not edit, manual changes will be overwritten.
+
+`
+
+content += readdirSync(__dirname + '/../apps')
+  .filter(f => f !== 'index.js' && /\.js$/.test(f))
+  .map(f => basename(f, '.js'))
+  .map(f => `export { default as ${f} } from './${f}'`)
+  .join('\n')
+
+writeFileSync(filePath, `${content}\n`)
+
+console.log(`${green}Updated apps index${white} − ${filePath}`)
+console.log('')

commit 976cdc9d1e6bd32de72e68c19e7e3080e9c8d5fe

devbox: fix Scratchpad (#397)
Fixes a bug introduced in 3599f33afac53b6b1d979dbb3c1f2dc3154d5736.

Import style changed and `Scratchpad` is no longer defined in `devbox/index.js`.

devbox/index.js
@@ -24,7 +24,7 @@ class Index extends React.Component {
   }
   render() {
     const { appName } = this.state
-    const CurrentApp = appName === 'Scratchpad' ? Scratchpad : APPS[appName]
+    const CurrentApp = APPS[appName]
     return CurrentApp ? (
       <Main>
         <CurrentApp />
@@ -41,6 +41,7 @@ class Index extends React.Component {
 
             {Object.keys(APPS)
               .sort()
+              .filter(appName => appName !== 'Scratchpad')
               .map(appName => (
                 <li key={appName}>
                   <a href={`#${appName}`}>{appName}</a>