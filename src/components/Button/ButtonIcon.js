753345f4e4b2cdf1b5c8fe289966fecdf32833f7
Pierre Bertet <hello@pierre.world>
2019-07-02 02:20:26 +0200

    ButtonIcon: move to a function component

--- src/components/Button/ButtonIcon.js
+++ src/components/Button/ButtonIcon.js
@@ -2,29 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import ButtonBase from './ButtonBase'
 
-class ButtonIcon extends React.PureComponent {
-  render() {
-    const { label, children, ...props } = this.props
-    return (
-      <ButtonBase
-        title={label}
-        css={`
-          display: inline-flex;
-          justify-content: center;
-          align-items: center;
-          width: 32px;
-          height: 32px;
-          &:active {
-            background: rgba(220, 234, 239, 0.3);
-          }
-        `}
-        {...props}
-      >
-        {children}
-      </ButtonBase>
-    )
-  }
-}
+const ButtonIcon = React.memo(function ButtonIcon({
+  label,
+  children,
+  ...props
+}) {
+  return (
+    <ButtonBase
+      title={label}
+      css={`
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        width: 32px;
+        height: 32px;
+        &:active {
+          background: rgba(220, 234, 239, 0.3);
+        }
+      `}
+      {...props}
+    >
+      {children}
+    </ButtonBase>
+  )
+})
 
 ButtonIcon.propTypes = {
   label: PropTypes.string.isRequired,
