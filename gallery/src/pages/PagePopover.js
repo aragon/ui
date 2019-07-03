f46d70fe7bc9651b7f7a5133f3aea3dc366e4fca
cd10012 <ced361@nyu.edu>
2019-07-03 15:09:55 +0200

    Use hooks to fix ref issue with popover component

--- gallery/src/pages/PagePopover.js
+++ gallery/src/pages/PagePopover.js
@@ -1,59 +1,49 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Button, Popover, Root } from '@aragon/ui'
 import Page from 'comps/Page/Page'
 import Container from 'comps/Page/DemoContainer'
 import readme from 'ui-src/components/Popover/README.md'
 
-class PagePopover extends React.Component {
-  state = {
-    hide: true,
-  }
-  render() {
-    const { title } = this.props
-    const { hide } = this.state
-    return (
-      <Page title={title} readme={readme}>
-        <Page.Demo>
-          <Root.Provider>
-            <Container>
-              <Wrapper>
-                <div
-                  ref={ref => {
-                    this.openerRef = ref
-                  }}
-                >
-                  <Button
-                    onClick={() => {
-                      this.setState({
-                        hide: !hide,
-                      })
-                    }}
-                  >
-                    Show component
-                  </Button>
-                </div>
+function PagePopover({ title }) {
+  const [hide, setHide] = useState(false)
+  const [opener, setOpener] = useState(null)
+
+  const handleOpenerRef = useCallback(node => {
+    setOpener(node)
+  }, [])
+
+  return (
+    <Page title={title} readme={readme}>
+      <Page.Demo>
+        <Root.Provider>
+          <Container>
+            <Wrapper>
+              <div>
+                <Button ref={handleOpenerRef} onClick={() => setHide(true)}>
+                  Show component
+                </Button>
+              </div>
+              <div>
                 <div>
-                  <div>
-                    {!hide && (
-                      <Popover
-                        placement="top-start"
-                        gutter="20px"
-                        openerRef={this.openerRef}
-                        handleClose={() => this.setState({ hide: true })}
-                      >
-                        <Box>Popover</Box>
-                      </Popover>
-                    )}
-                  </div>
+                  <Popover
+                    opener={opener}
+                    placement="top-start"
+                    gutter="20px"
+                    visible={hide}
+                    onClose={() => setHide(false)}
+                  >
+                    <Box>Popover</Box>
+                  </Popover>
                 </div>
-              </Wrapper>
-            </Container>
-          </Root.Provider>
-        </Page.Demo>
-      </Page>
-    )
-  }
+              </div>
+            </Wrapper>
+          </Container>
+        </Root.Provider>
+      </Page.Demo>
+    </Page>
+  )
 }
 
 const Wrapper = styled.div`
