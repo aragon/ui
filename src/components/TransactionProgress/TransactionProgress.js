46fb3ea7b096cc6f43baaa9be7fe15f1eaf6a069
Franco Petra <franco.petraz@gmail.com>
2019-07-09 16:27:03 -0300

    PageTransaction Fix based on gh1dra Popover fix

--- src/components/TransactionProgress/TransactionProgress.js
+++ src/components/TransactionProgress/TransactionProgress.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import { theme } from '../../theme'
 import { noop } from '../../utils'
 import Text from '../Text/Text'
-import SafeLink from '../Link/SafeLink'
+import ExternalLink from '../Link/ExternalLink'
 import Countdown from '../Countdown/Countdown'
 import Info from '../Info'
 import ProgressBar from '../ProgressBar/ProgressBar'
@@ -98,7 +98,7 @@ const FooterWrapper = styled.div`
   padding-top: 1rem;
 `
 
-const Link = styled(SafeLink)`
+const Link = styled(ExternalLink)`
   color: #21aae7;
   text-decoration: none;
 `
