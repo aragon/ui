f941750ea58ae3d229660a31e4d89099b268c0b5
Gorka <gorka@aquigorka.com>
2019-07-15 13:24:32 +0200

    Info: update component to use new spacing and theme colors

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,27 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { theme } from '../../theme-legacy'
+import { useTheme } from '../../theme'
+import { GU, RADIUS } from '../../style'
 
-const Info = ({ children, title, ...props }) => (
-  <Main {...props}>
-    {title && <Title>{title}</Title>}
-    {children}
-  </Main>
-)
+const Info = ({ children, title, ...props }) => {
+  const theme = useTheme()
+
+  return (
+    <Main
+      background={theme.infoSurface}
+      color={theme.infoContent}
+      accent={theme.infoBorder}
+      {...props}
+    >
+      {title && <Title>{title}</Title>}
+      {children}
+    </Main>
+  )
+}
+
 Info.propTypes = {
   background: PropTypes.string,
   children: PropTypes.node,
   title: PropTypes.node,
 }
-Info.defaultProps = {
-  background: theme.infoBackground,
-}
 
 const Main = styled.section`
+  color: ${({ color }) => color};
   background: ${({ background }) => background};
-  padding: 15px;
-  border-radius: 3px;
+  border-left: ${({ accent }) => `2px solid ${accent}`};
+  padding: ${2 * GU}px;
+  border-radius: ${RADIUS}px;
   word-wrap: break-word;
 `
 
