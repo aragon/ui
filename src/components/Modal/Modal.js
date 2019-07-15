d036505d2ef3961aaed7c854c1a8c4576b6e5eab
Gorka <gorka@aquigorka.com>
2019-07-15 12:49:25 +0200

    Modal: use background from theme

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,67 +5,77 @@ import { Transition, animated } from 'react-spring'
 import { Viewport } from '../../providers/Viewport'
 import { noop } from '../../utils'
 import { springs, GU, RADIUS } from '../../style'
+import { useTheme } from '../../theme'
 import EscapeOutside from '../EscapeOutside/EscapeOutside'
 import RootPortal from '../RootPortal/RootPortal'
 
 const cssPx = value => (typeof value === 'number' ? value + 'px' : value)
 
-const Modal = ({ children, onClose, padding, visible, width }) => (
-  <RootPortal>
-    <Viewport>
-      {viewport => (
-        <Transition
-          native
-          items={visible}
-          from={{ opacity: 0, scale: 0.98 }}
-          enter={{ opacity: 1, scale: 1 }}
-          leave={{ opacity: 0, scale: 0.98 }}
-          config={springs.smooth}
-        >
-          {show =>
-            show &&
-            /* eslint-disable react/prop-types */
-            (({ opacity, scale }) => (
-              <Overlay style={{ opacity }}>
-                <ContentWrapper
-                  style={{
-                    pointerEvents: visible ? 'auto' : 'none',
-                    transform: scale.interpolate(v => `scale3d(${v}, ${v}, 1)`),
-                  }}
-                >
-                  <div
-                    css={`
-                      padding: ${3 * GU}px ${1.5 * GU}px;
-                    `}
+const Modal = ({ children, onClose, padding, visible, width }) => {
+  const theme = useTheme()
+
+  return (
+    <RootPortal>
+      <Viewport>
+        {viewport => (
+          <Transition
+            native
+            items={visible}
+            from={{ opacity: 0, scale: 0.98 }}
+            enter={{ opacity: 1, scale: 1 }}
+            leave={{ opacity: 0, scale: 0.98 }}
+            config={springs.smooth}
+          >
+            {show =>
+              show &&
+              /* eslint-disable react/prop-types */
+              (({ opacity, scale }) => (
+                <Overlay style={{ opacity }}>
+                  <ContentWrapper
+                    style={{
+                      pointerEvents: visible ? 'auto' : 'none',
+                      transform: scale.interpolate(
+                        v => `scale3d(${v}, ${v}, 1)`
+                      ),
+                    }}
                   >
-                    <Content
-                      role="alertdialog"
-                      onEscapeOutside={onClose}
-                      style={{
-                        width: cssPx(
-                          typeof width === 'function' ? width(viewport) : width
-                        ),
-                        padding: cssPx(
-                          typeof padding === 'function'
-                            ? padding(viewport)
-                            : padding
-                        ),
-                        borderRadius: `${RADIUS}px`,
-                      }}
+                    <div
+                      css={`
+                        padding: ${3 * GU}px ${1.5 * GU}px;
+                      `}
                     >
-                      {children}
-                    </Content>
-                  </div>
-                </ContentWrapper>
-              </Overlay>
-            ))
-          /* eslint-enable react/prop-types */
-          }
-        </Transition>
-      )}
-    </Viewport>
-  </RootPortal>
-)
+                      <Content
+                        role="alertdialog"
+                        background={theme.surface}
+                        onEscapeOutside={onClose}
+                        style={{
+                          width: cssPx(
+                            typeof width === 'function'
+                              ? width(viewport)
+                              : width
+                          ),
+                          padding: cssPx(
+                            typeof padding === 'function'
+                              ? padding(viewport)
+                              : padding
+                          ),
+                          borderRadius: `${RADIUS}px`,
+                        }}
+                      >
+                        {children}
+                      </Content>
+                    </div>
+                  </ContentWrapper>
+                </Overlay>
+              ))
+            /* eslint-enable react/prop-types */
+            }
+          </Transition>
+        )}
+      </Viewport>
+    </RootPortal>
+  )
+}
 
 Modal.defaultProps = {
   onClose: noop,
@@ -105,7 +115,7 @@ const ContentWrapper = styled(animated.div)`
 const Content = styled(EscapeOutside)`
   overflow: hidden;
   min-width: ${360 - 4 * GU}px;
-  background: #fff;
+  background: ${({ background }) => background};
   box-shadow: 0 10px 28px rgba(0, 0, 0, 0.2);
 `
 
