24f74103df4de074775af5f4502821de65a1102a
Pierre Bertet <hello@pierre.world>
2019-07-25 13:55:55 +0200

    ESLint

--- src/components/Popover/Popover.js
+++ src/components/Popover/Popover.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Popper from 'popper.js'
 import { Transition, animated } from 'react-spring'
