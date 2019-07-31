7746fefb6bbc305d4397a5d350211a00d8c825b9
Gorka Ludlow <gorka@aquigorka.com>
2019-07-31 11:04:43 +0200

    Empty state card new styles (#415)
    * Components: add experimental empty state card to index
    
    * EmptyStateCard: add component
    
    * EmptyStateCard: add prop to remove button
    
    * Swap experimental component for the real deal
    
    * EmptyStateCard: update readme
    
    * Remove experimental export
    
    * PageEmptyStateCard: update items
    
    * EmptyStateCard: update devbox page
    
    * EmptyStateCard: action prop + tweaks (#426)
    
    - devbox: EmptyStateCard tweaks + add file-loader.
    - Add usePublicUrl()
    - Button: support the wide prop.
    - Button inside EmptyStateCard: enforce wide + strong.
    - EmptyCard style tweaks (height of card + illustration).
    - EmptyStateCard: provide an illustration by default.
    
    EmptyStateCard API tweaks:
    
    - `icon` has been renamed `illustration`, since it should not contain an
      icon anymore.
    - `action` has been added again.
    - `actionButton`, `actionText` and `onClick` have been removed (replaced
      by `action`).

--- devbox/apps/EmptyStateCard.js
+++ devbox/apps/EmptyStateCard.js
@@ -1,62 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Button, EmptyStateCard, Text, theme, IconBlank } from '@aragon/ui'
+import { Button, EmptyStateCard, Text, theme, IconFile } from '@aragon/ui'
+import emptyStatePng from '../assets/voting-empty-state.png'
 
 class App extends React.Component {
   render() {
-    const text = 'You seem to not have any content on your wall.'
-    const icon = <IconBlank width={40} height={40} />
+    const text1 = 'You seem to not have any content on your wall.'
+    const text2 = 'Nothing to see here.'
     const button = (
-      <Button mode="secondary" style={{ width: '150px' }}>
-        Custom button
-      </Button>
+      <Button onClick={() => console.log('Click on button')}>Create</Button>
     )
+    const illustration = <img src={emptyStatePng} alt="" height="160" />
     return (
-      <Main>
-        <Items>
-          <Item>
-            <EmptyStateCard />
-          </Item>
-          <Item>
-            <EmptyStateCard text={text} icon={icon} action={button} />
-          </Item>
-          <Item>
-            <EmptyStateCard
-              text={text}
-              icon={() => icon}
-              actionText="Click me"
-              actionButton={Button}
-            />
-          </Item>
-          <Item>
-            <EmptyStateCard icon={() => icon} actionText="Click me" />
-          </Item>
-          <Item>
-            <EmptyStateCard icon={() => icon} />
-          </Item>
-        </Items>
-      </Main>
+      <div
+        css={`
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr;
+          grid-gap: 48px;
+          justify-items: center;
+          align-items: center;
+          padding-top: 48px;
+        `}
+      >
+        <EmptyStateCard text={text1} action={button} />
+        <EmptyStateCard text={text1} />
+        <EmptyStateCard text={text2} action={button} />
+        <EmptyStateCard illustration={illustration} text={text1} />
+        <EmptyStateCard
+          illustration={illustration}
+          text={text2}
+          action={button}
+        />
+        <EmptyStateCard illustration={illustration} text={text2} />
+        <EmptyStateCard
+          illustration={illustration}
+          text={text1}
+          action={button}
+        />
+      </div>
     )
   }
 }
-
-const Main = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 0;
-`
-
-const Items = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  min-height: 0;
-`
-
-const Item = styled.div`
-  margin-left: 40px;
-  margin-top: 40px;
-`
 
 export default App
--- src/components/Card/EmptyStateCard.js
+++ src/components/Card/EmptyStateCard.js
@@ -1,151 +1,80 @@
 import React from 'react'
-import PropTypes from '../../proptypes'
-import styled from 'styled-components'
-import { theme } from '../../theme-legacy'
-import { noop, warn } from '../../utils'
-import { Button } from '../Button/Button'
-import Text from '../Text/Text'
+import PropTypes from 'prop-types'
+import { GU, textStyle } from '../../style'
+import { Inside, warnOnce } from '../../utils'
+import { usePublicUrl } from '../../providers/PublicUrl'
+import { useTheme } from '../../theme'
 import Card from './Card'
-import omit from 'lodash-es/omit'
+import illustrationDefault from './assets/empty-state-card-illustration-default.png'
 
-class EmptyStateCard extends React.Component {
-  static propTypes = {
-    action: PropTypes.element,
-    actionButton: PropTypes._component,
-    actionDisabled: PropTypes.bool,
-    actionText: PropTypes.string,
-    icon: PropTypes.oneOfType([PropTypes.element, PropTypes._component]),
-    onActivate: PropTypes.func,
-    text: PropTypes.string,
-    title: PropTypes.string,
-  }
-  static defaultProps = {
-    title: 'Nothing here.',
-    actionDisabled: false,
-    onActivate: noop,
-  }
+const EmptyStateCard = React.memo(function EmptyStateCard({
+  action,
+  icon,
+  illustration,
+  text,
+}) {
+  const theme = useTheme()
+  const publicUrl = usePublicUrl()
 
-  constructor(props) {
-    super(props)
-    this.displayWarnings({}, props)
-  }
-
-  componentDidUpdate(prevProps) {
-    this.displayWarnings(prevProps, this.props)
-  }
-
-  displayWarnings(prevProps, props) {
-    // Deprecated warnings
-    if (prevProps.actionButton !== props.actionButton && props.actionButton) {
-      warn(
-        'The “actionButton” prop is deprecated. Please use “action” to pass ' +
-          'a custom element instead.'
-      )
-    }
-    if (
-      prevProps.icon !== props.icon &&
-      props.icon &&
-      !React.isValidElement(props.icon)
-    ) {
-      warn(
-        'Passing a component to the “icon” prop is deprecated. Please pass ' +
-          'an element instead (i.e. use icon={<Icon />} rather than ' +
-          'icon={Icon}).'
-      )
-    }
-
-    // Display a warning if `action` is used to override the button, while also
-    // using one of the props that apply to the default button component.
-    const anyActionParam =
-      props.actionDisabled || props.actionText || props.onActivate
-    if (prevProps.action !== props.action && props.action && anyActionParam) {
-      warn(
-        'Using `action` takes priority over `actionDisabled`, `actionText`, ' +
-          'and `onActivate`. Please use either `action` or any of these ' +
-          'props.'
-      )
+  if (icon !== undefined) {
+    warnOnce(
+      'EmptyStateCard:icon',
+      'EmptyStateCard: the `icon` prop is deprecated, please use `illustration` instead.'
+    )
+    if (illustration === undefined) {
+      illustration = icon
     }
   }
 
-  // Render icon, even when provided as a component (deprecated)
-  iconElement() {
-    const { icon } = this.props
-    return !icon || React.isValidElement(icon)
-      ? icon
-      : React.createElement(icon)
-  }
-
-  // Render the action (usually a button)
-  actionElement() {
-    const {
-      action,
-      actionButton,
-      actionDisabled,
-      actionText,
-      onActivate,
-    } = this.props
-
-    // If an action node is provided, it takes priority above the other props
-    if (action) {
-      return action
-    }
-
-    // If no label is provided, the button is not displayed
-    if (!actionText) {
-      return null
-    }
-
-    // Use the provided actionButton if it exists, or a local component.
-    const Button = actionButton || DefaultActionButton
-
-    return (
-      <Button disabled={actionDisabled} mode="strong" onClick={onActivate} wide>
-        {actionText}
-      </Button>
+  // default illustration
+  if (!illustration) {
+    illustration = (
+      <img src={publicUrl + illustrationDefault} alt="" height={20 * GU} />
     )
   }
 
-  render() {
-    const { title, text, ...props } = this.props
-    const extraProps = omit(props, Object.keys(EmptyStateCard.propTypes))
-    return (
-      <Main {...extraProps}>
-        <section>
-          {this.iconElement()}
-          <Heading>
-            <Text color={theme.accent} weight="bold" size="large">
-              {title}
-            </Text>
-          </Heading>
-          {text && <Text.Block>{text}</Text.Block>}
-          <ActionWrapper>{this.actionElement()}</ActionWrapper>
-        </section>
-      </Main>
-    )
-  }
+  return (
+    <Inside name="EmptyStateCard">
+      <Card
+        css={`
+          display: grid;
+          grid-template-columns: 1fr;
+          grid-template-rows: ${20 * GU}px 1fr auto;
+          height: ${42 * GU}px;
+          padding: ${2 * GU}px;
+          text-align: center;
+        `}
+      >
+        <div
+          css={`
+            display: flex;
+            justify-content: center;
+            overflow: hidden;
+          `}
+        >
+          {illustration}
+        </div>
+        <div
+          css={`
+            color: ${theme.surfaceContent};
+            ${textStyle('title4')};
+          `}
+        >
+          {text}
+        </div>
+        <div>{action}</div>
+      </Card>
+    </Inside>
+  )
+})
+
+EmptyStateCard.propTypes = {
+  action: PropTypes.node,
+  illustration: PropTypes.node,
+  text: PropTypes.node.isRequired,
+
+  // deprecated
+  icon: PropTypes.node,
 }
-
-const Main = styled(Card)`
-  display: flex;
-  padding: 40px 60px;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  section {
-    padding-top: 20px;
-  }
-`
-
-const DefaultActionButton = styled(Button)`
-  width: 150px;
-`
-
-const Heading = styled.h1`
-  margin: 20px 0 5px;
-`
-
-const ActionWrapper = styled.div`
-  margin-top: 20px;
-`
 
 export default EmptyStateCard
