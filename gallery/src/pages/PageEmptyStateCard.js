032eeae5b7068cb54e826f88fed71366892a42b2
Gorka Ludlow <gorka@aquigorka.com>
2019-07-31 12:24:44 +0200

    Empty state card update gallery (#428)
    * EmptyStateCard: update gallery page
    
    * EmptyStateCard: remove icon from gallery

--- gallery/src/pages/PageEmptyStateCard.js
+++ gallery/src/pages/PageEmptyStateCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Page from 'comps/Page/Page'
 
 import readme from 'ui-src/components/Card/EmptyStateCard.md'
-import { EmptyStateCard, IconHome } from '@aragon/ui'
+import { Button, EmptyStateCard } from '@aragon/ui'
 import Container from '../components/Page/DemoContainer'
 
 const PageEmptyStateCard = ({ title }) => (
@@ -11,19 +11,11 @@ const PageEmptyStateCard = ({ title }) => (
       <Container>
         <EmptyStateCard
           text="You seem to not have any content on your wall."
-          icon={
-            <IconHome
-              color="blue"
-              css={`
-                margin: auto;
-                width: 140px;
-                height: auto;
-              `}
-            />
+          action={
+            <Button onClick={() => console.log('Button was clicked')}>
+              Click me
+            </Button>
           }
-          actionButton={true}
-          actionText="Click me"
-          onClick={() => console.log('Button was clicked')}
         />
       </Container>
     </Page.Demo>
