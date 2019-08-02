06d14b9ae9a4f124350550e633e63477d122906f
Pierre Bertet <hello@pierre.world>
2019-08-02 10:27:36 +0200

    devbox: update Info

--- devbox/apps/Info.js
+++ devbox/apps/Info.js
@@ -3,19 +3,14 @@ import { Split, Box, Info } from '@aragon/ui'
 
 const MODES = ['info', 'warning', 'error', 'description']
 
-function InfoDemo({ content, title = '' }) {
+function InfoDemo({ content, title }) {
   return (
-    <Box
-      css={`
-        margin-top: 24px;
-        flex-grow: 2;
-      `}
-    >
+    <Box css="margin-top: 48px">
       {MODES.map((mode, i) => (
         <Info
           key={i}
           mode={mode}
-          title={title}
+          title={title ? mode : ''}
           css={`
             & + & {
               margin-top: 16px;
@@ -39,13 +34,15 @@ export default () => (
           sed diam voluptua.
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua.
+          sed diam voluptua, consetetur sadipscing elitr et dolore magna
+          aliquyam erat aliquyam consetetur sadipscing tempor magna
+          aliquyam erat invidunt.
         "
       />
     }
     secondary={
       <InfoDemo
-        title="Lorem ipsum dolor sit amet"
+        title
         content="
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, nonumy
           eirmod tempor.
