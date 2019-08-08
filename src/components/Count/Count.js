50cd0b8b5f7bc00b34501d00bafa903151743290
Pierre Bertet <hello@pierre.world>
2019-08-08 10:59:47 +0200

    Count: extend Tag prop types

--- src/components/Count/Count.js
+++ src/components/Count/Count.js
@@ -2,16 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Tag } from '../Tag/Tag'
 
-const NORMAL = 'normal'
-const SMALL = 'small'
-const INFO = 'info'
-const WARNING = 'warning'
-const HELP = 'help'
-const TAG = 'tag'
-const APP = 'app'
-const IDENTITY = 'identity'
-const NOTIFICATION = 'notification'
-
 function Count({ value, digits, ...props }) {
   const parsed = parseInt(value, 10)
   const max = Math.pow(10, digits) - 1
@@ -21,24 +11,13 @@ function Count({ value, digits, ...props }) {
 }
 
 Count.propTypes = {
+  ...Tag.propTypes,
   value: PropTypes.number.isRequired,
   digits: PropTypes.number,
 }
 
 Count.defaultProps = {
   digits: 2,
-  mode: PropTypes.oneOf([
-    INFO,
-    WARNING,
-    HELP,
-    TAG,
-    APP,
-    IDENTITY,
-    NOTIFICATION,
-  ]),
-  color: PropTypes.string,
-  background: PropTypes.string,
-  size: PropTypes.oneOf([NORMAL, SMALL]),
 }
 
 export { Count }
