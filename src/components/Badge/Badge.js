26588114ad484676499aa8e2fcd179bfb7d12540
Pierre Bertet <hello@pierre.world>
2019-08-08 13:28:42 +0200

    Tweak deprecated badges
    More compatibility + group them all in the same file.

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -1,9 +1,84 @@
 import React from 'react'
 import { Tag } from '../Tag/Tag'
+import { Count } from '../Count/Count'
 import { warnOnce } from '../../utils/environment'
+import { useTheme } from '../../theme'
 
-export default props => {
-  warnOnce('"Badge" has been deprecated. Please use "Tag" instead.')
-
-  return <Tag {...props} />
+function deprecationWarning() {
+  warnOnce(
+    'Badge',
+    '"Badge" and its variants have been deprecated. Please use "Tag" instead.'
+  )
 }
+
+function Badge({ background, foreground, shape, children, ...props }) {
+  deprecationWarning()
+  return (
+    <Tag
+      background={background}
+      color={foreground}
+      size={shape === 'smalldisc' || shape === 'compact' ? 'small' : 'normal'}
+      {...props}
+    >
+      {children}
+    </Tag>
+  )
+}
+
+function BadgeNumber({
+  background,
+  children,
+  foreground,
+  label,
+  shape,
+  small,
+  ...props
+}) {
+  deprecationWarning()
+  if (!children && typeof label === 'number') {
+    return (
+      <Count
+        background={background}
+        color={foreground}
+        value={label}
+        size={small ? 'small' : 'normal'}
+        {...props}
+      />
+    )
+  }
+  return (
+    <Tag background={background} color={foreground} {...props}>
+      {children || label}
+    </Tag>
+  )
+}
+
+function BadgeInfo(props) {
+  return <BadgeNumber {...props} />
+}
+
+function BadgeIdentity(props) {
+  return <Badge {...props} />
+}
+
+function BadgeApp(props) {
+  return <Badge {...props} mode="tag" />
+}
+
+function BadgeNotification(props) {
+  const theme = useTheme()
+  return (
+    <BadgeNumber
+      background={String(theme.positive)}
+      foreground={String(theme.positiveContent)}
+      {...props}
+    />
+  )
+}
+
+Badge.Info = BadgeInfo
+Badge.Notification = BadgeNotification
+Badge.Identity = BadgeIdentity
+Badge.App = BadgeApp
+
+export default Badge
