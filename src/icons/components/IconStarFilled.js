0517c31ce8859d49f6b65d1d920a25c846419815
Pierre Bertet <hello@pierre.world>
2019-08-12 12:34:52 +0200

    Add IconStarFilled (#453)

---
+++ src/icons/components/IconStarFilled.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import iconSize from '../icon-size'
+import IconPropTypes from '../IconPropTypes'
+
+function IconStarFilled({ size, ...props }) {
+  const sizeValue = iconSize(size)
+  return (
+    <svg
+      width={sizeValue}
+      height={sizeValue}
+      fill="none"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M19.968 10.066a.656.656 0 00-.52-.46l-4.746-.727-2.122-4.502A.644.644 0 0012 4a.644.644 0 00-.58.377L9.299 8.88l-4.745.726a.655.655 0 00-.521.461.7.7 0 00.163.694l3.434 3.502-.81 4.947a.692.692 0 00.257.662.622.622 0 00.68.051L12 17.585l4.244 2.337a.622.622 0 00.68-.051.692.692 0 00.258-.662l-.81-4.947 3.433-3.502a.7.7 0 00.163-.694z"
+      />
+    </svg>
+  )
+}
+
+IconStarFilled.propTypes = IconPropTypes
+export default IconStarFilled
