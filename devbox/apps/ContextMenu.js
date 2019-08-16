86a8a4aefb60b48cd808b491320e0987582b2888
Brett Sun <qisheng.brett.sun@gmail.com>
2019-08-16 14:13:58 +0200

    Devbox: add ContextMenu demo

---
+++ devbox/apps/ContextMenu.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import {
+  Box,
+  ContextMenu,
+  ContextMenuItem,
+  IconAdd,
+  IconRemove,
+  IconLabel,
+  GU,
+  textStyle,
+  useTheme,
+} from '@aragon/ui'
+
+const logAction = action => () => console.log(action)
+const actions = [
+  [logAction('add'), IconAdd, 'Add tokens'],
+  [logAction('remove'), IconRemove, `Remove token`],
+  [logAction('edit'), IconLabel, 'Edit label'],
+]
+
+export default function() {
+  const theme = useTheme()
+  return (
+    <div
+      css={`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+      `}
+    >
+      <Box>
+        <div
+          css={`
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            width: 200px;
+          `}
+        >
+          <p
+            css={`
+              ${textStyle('body2')}
+            `}
+          >
+            Actions
+          </p>
+          <ContextMenu>
+            {actions.map(([onClick, Icon, label], index) => (
+              <ContextMenuItem onClick={onClick} key={index}>
+                <span
+                  css={`
+                    position: relative;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    color: ${theme.surfaceContentSecondary};
+                  `}
+                >
+                  <Icon />
+                </span>
+                <span
+                  css={`
+                    margin-left: ${1 * GU}px;
+                  `}
+                >
+                  {label}
+                </span>
+              </ContextMenuItem>
+            ))}
+          </ContextMenu>
+        </div>
+      </Box>
+    </div>
+  )
+}
