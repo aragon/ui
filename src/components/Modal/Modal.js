a224a036093f63012d0d36f84b5bbe2006d5558f
Pierre Bertet <hello@pierre.world>
2019-08-19 00:46:55 +0200

    Modal: add a prop to display a close button (#494)

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,112 +1,122 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Transition, animated } from 'react-spring'
-import { Viewport } from '../../providers/Viewport'
 import { noop } from '../../utils'
 import { springs, GU, RADIUS } from '../../style'
 import { useTheme } from '../../theme'
+import { useViewport } from '../../providers/Viewport'
+import { IconCross } from '../../icons'
+import { ButtonIcon } from '../Button/ButtonIcon'
 import EscapeOutside from '../EscapeOutside/EscapeOutside'
 import RootPortal from '../RootPortal/RootPortal'
 
 const cssPx = value => (typeof value === 'number' ? value + 'px' : value)
 
-const Modal = ({ children, onClose, padding, visible, width }) => {
+function Modal({ children, onClose, padding, visible, width, closeButton }) {
   const theme = useTheme()
+  const viewport = useViewport()
 
   return (
     <RootPortal>
-      <Viewport>
-        {viewport => (
-          <Transition
-            native
-            items={visible}
-            from={{ opacity: 0, scale: 0.98 }}
-            enter={{ opacity: 1, scale: 1 }}
-            leave={{ opacity: 0, scale: 0.98 }}
-            config={springs.smooth}
-          >
-            {show =>
-              show &&
-              /* eslint-disable react/prop-types */
-              (({ opacity, scale }) => (
-                <animated.div
+      <Transition
+        native
+        items={visible}
+        from={{ opacity: 0, scale: 0.98 }}
+        enter={{ opacity: 1, scale: 1 }}
+        leave={{ opacity: 0, scale: 0.98 }}
+        config={springs.smooth}
+      >
+        {show =>
+          show &&
+          /* eslint-disable react/prop-types */
+          (({ opacity, scale }) => (
+            <animated.div
+              css={`
+                position: absolute;
+                z-index: 1;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                background: ${theme.overlay.alpha(0.9)};
+              `}
+              style={{ opacity }}
+            >
+              <animated.div
+                css={`
+                  position: absolute;
+                  z-index: 1;
+                  top: 0;
+                  left: 0;
+                  right: 0;
+                  bottom: 0;
+                  display: grid;
+                  align-items: center;
+                  justify-items: center;
+                  overflow: auto;
+                `}
+                style={{
+                  pointerEvents: visible ? 'auto' : 'none',
+                  transform: scale.interpolate(v => `scale3d(${v}, ${v}, 1)`),
+                }}
+              >
+                <div
                   css={`
-                    position: absolute;
-                    z-index: 1;
-                    top: 0;
-                    left: 0;
-                    right: 0;
-                    bottom: 0;
-                    background: ${theme.overlay.alpha(0.8)};
+                    padding: ${2 * GU}px;
                   `}
-                  style={{ opacity }}
                 >
-                  <animated.div
+                  <EscapeOutside
+                    role="alertdialog"
+                    background={theme.surface}
+                    onEscapeOutside={onClose}
                     css={`
-                      position: absolute;
-                      z-index: 1;
-                      top: 0;
-                      left: 0;
-                      right: 0;
-                      bottom: 0;
-                      display: grid;
-                      align-items: center;
-                      justify-items: center;
-                      overflow: auto;
+                      position: relative;
+                      overflow: hidden;
+                      min-width: ${360 - 4 * GU}px;
+                      background: ${theme.surface};
+                      box-shadow: 0 10px 28px rgba(0, 0, 0, 0.2);
                     `}
                     style={{
-                      pointerEvents: visible ? 'auto' : 'none',
-                      transform: scale.interpolate(
-                        v => `scale3d(${v}, ${v}, 1)`
+                      width: cssPx(
+                        typeof width === 'function' ? width(viewport) : width
                       ),
+                      padding: cssPx(
+                        typeof padding === 'function'
+                          ? padding(viewport)
+                          : padding
+                      ),
+                      borderRadius: `${RADIUS}px`,
                     }}
                   >
-                    <div
-                      css={`
-                        padding: ${2 * GU}px ${2 * GU}px;
-                      `}
-                    >
-                      <EscapeOutside
-                        role="alertdialog"
-                        background={theme.surface}
-                        onEscapeOutside={onClose}
+                    {closeButton && (
+                      <ButtonIcon
+                        label="Close"
+                        onClick={onClose}
                         css={`
-                          overflow: hidden;
-                          min-width: ${360 - 4 * GU}px;
-                          background: ${theme.surface};
-                          box-shadow: 0 10px 28px rgba(0, 0, 0, 0.2);
+                          position: absolute;
+                          top: ${2 * GU}px;
+                          right: ${2 * GU}px;
                         `}
-                        style={{
-                          width: cssPx(
-                            typeof width === 'function'
-                              ? width(viewport)
-                              : width
-                          ),
-                          padding: cssPx(
-                            typeof padding === 'function'
-                              ? padding(viewport)
-                              : padding
-                          ),
-                          borderRadius: `${RADIUS}px`,
-                        }}
                       >
-                        {children}
-                      </EscapeOutside>
-                    </div>
-                  </animated.div>
-                </animated.div>
-              ))
-            /* eslint-enable react/prop-types */
-            }
-          </Transition>
-        )}
-      </Viewport>
+                        <IconCross />
+                      </ButtonIcon>
+                    )}
+                    <div>{children}</div>
+                  </EscapeOutside>
+                </div>
+              </animated.div>
+            </animated.div>
+          ))
+        /* eslint-enable react/prop-types */
+        }
+      </Transition>
     </RootPortal>
   )
 }
 
 Modal.propTypes = {
   children: PropTypes.node.isRequired,
+  closeButton: PropTypes.bool,
   onClose: PropTypes.func,
   padding: PropTypes.oneOfType([
     PropTypes.func,
@@ -122,9 +132,10 @@ Modal.propTypes = {
 }
 
 Modal.defaultProps = {
+  closeButton: true,
   onClose: noop,
   padding: 3 * GU,
-  width: viewport => Math.min(viewport.width - 48, 600),
+  width: viewport => Math.min(viewport.width - 6 * GU, 600),
 }
 
 export default Modal
