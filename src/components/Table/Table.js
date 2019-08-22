5030fa9fa7ff3ef237ec82a0d676ff66c0bd7f62
Pierre Bertet <hello@pierre.world>
2019-08-22 10:33:14 +0200

    Table: new style update (useTheme() + GU) (#523)

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,17 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import TableRow from './TableRow'
-import { theme } from '../../theme-legacy'
+import { useTheme } from '../../theme'
 
-const { contentBorder } = theme
+function Table({ header, children, noSideBorders, ...props }) {
+  const theme = useTheme()
+  return (
+    <table
+      {...props}
+      css={`
+        width: 100%;
+        border-spacing: 0;
 
-const Table = ({ header, children, ...props }) => (
-  <StyledTable {...props}>
-    {header && <thead>{header}</thead>}
-    <tbody>{children}</tbody>
-  </StyledTable>
-)
+        td {
+          border-bottom: 1px solid ${theme.border};
+        }
+
+        /* First and last cell styling */
+        tr:first-child td {
+          border-top: 1px solid ${theme.border};
+        }
+        td:first-child {
+          border-left: ${noSideBorders ? '0' : `1px solid ${theme.border}`};
+        }
+        td:last-child {
+          border-right: ${noSideBorders ? '0' : `1px solid ${theme.border}`};
+        }
+
+        /* First and last row styling */
+        tr:first-child td:first-child {
+          border-top-left-radius: ${noSideBorders ? '0' : '3px'};
+        }
+        tr:first-child td:last-child {
+          border-top-right-radius: ${noSideBorders ? '0' : '3px'};
+        }
+        tr:last-child td:first-child {
+          border-bottom-left-radius: ${noSideBorders ? '0' : '3px'};
+        }
+        tr:last-child td:last-child {
+          border-bottom-right-radius: ${noSideBorders ? '0' : '3px'};
+        }
+      `}
+    >
+      {header && <thead>{header}</thead>}
+      <tbody>{children}</tbody>
+    </table>
+  )
+}
 
 Table.propTypes = {
   children: PropTypes.node,
@@ -22,45 +56,5 @@ Table.propTypes = {
 Table.defaultProps = {
   noSideBorders: false,
 }
-
-const StyledTable = styled.table`
-  width: 100%;
-  border-spacing: 0;
-
-  td {
-    border-bottom: 1px solid ${contentBorder};
-  }
-
-  /* First and last cell styling */
-  ${TableRow}:first-child td {
-    border-top: 1px solid ${contentBorder};
-  }
-  td:first-child {
-    border-left: ${({ noSideBorders }) =>
-      noSideBorders ? 0 : `1px solid ${contentBorder}`};
-  }
-  td:last-child {
-    border-right: ${({ noSideBorders }) =>
-      noSideBorders ? 0 : `1px solid ${contentBorder}`};
-  }
-
-  /* First and last row styling */
-  ${TableRow}:first-child td:first-child {
-    border-top-left-radius: ${({ noSideBorders }) =>
-      noSideBorders ? 0 : '3px'};
-  }
-  ${TableRow}:first-child td:last-child {
-    border-top-right-radius: ${({ noSideBorders }) =>
-      noSideBorders ? 0 : '3px'};
-  }
-  ${TableRow}:last-child td:first-child {
-    border-bottom-left-radius: ${({ noSideBorders }) =>
-      noSideBorders ? 0 : '3px'};
-  }
-  ${TableRow}:last-child td:last-child {
-    border-bottom-right-radius: ${({ noSideBorders }) =>
-      noSideBorders ? 0 : '3px'};
-  }
-`
 
 export default Table
