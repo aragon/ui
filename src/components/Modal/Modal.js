88d43ae6af2fd4b7c875e5e7e5e22a42b1411e65
Pierre Bertet <hello@pierre.world>
2019-08-23 20:38:51 +0200

    Various fixes (#530)
    - Update devbox Tabs.
    - FocusVisible is now a non-pure component.
    - Fix LinkBase import.
    - Improve Modal transition.
    - Ensure Split is always 100% wide.
    - Components index order.
    - Use body2 as a default text style.
    - Update package-lock.json.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -30,7 +30,7 @@ function Modal({
         from={{ opacity: 0, scale: 0.98 }}
         enter={{ opacity: 1, scale: 1 }}
         leave={{ opacity: 0, scale: 0.98 }}
-        config={springs.smooth}
+        config={{ ...springs.smooth, precision: 0.001 }}
       >
         {show =>
           show &&
