3e66984084241e3d876738d04d0afb7c53662893
Pierre Bertet <hello@pierre.world>
2019-08-23 20:11:33 +0200

    Link: add styles, move the core features to LinkBase (#525)

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
+import LinkBase from './LinkBase'
+import { useTheme } from '../../theme'
 
-function Link({ children, external, ...props }) {
-  const passedProps = { ...props }
+const KEYCODE_ENTER = 13
 
-  if (external) {
-    passedProps._target = 'blank'
-  }
+function Link({ onClick, href, ...props }) {
+  const theme = useTheme()
 
-  return <a {...passedProps}>{children}</a>
+  const handleKeyDown = useCallback(
+    event => {
+      if (event.keyCode === KEYCODE_ENTER && onClick) {
+        onClick()
+      }
+    },
+    [onClick]
+  )
+
+  return (
+    <LinkBase
+      href={href}
+      onClick={onClick}
+      onKeyDown={handleKeyDown}
+      {...props}
+      css={`
+        color: ${theme.link};
+        text-decoration: none;
+        font-size: inherit;
+        cursor: pointer;
+      `}
+    />
+  )
 }
 
 Link.propTypes = {
-  children: PropTypes.node,
-  external: PropTypes.bool,
-  rel: PropTypes.string,
-}
-
-Link.defaultProps = {
-  external: false,
-  rel: 'noopener noreferrer',
+  ...LinkBase.propTypes,
+  href: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Link
