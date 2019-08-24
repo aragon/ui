2229fa7ee8c407e50824d084ebf6decf8f517e81
Pierre Bertet <hello@pierre.world>
2019-08-24 20:59:52 +0200

    Devbox: update Layout-related behavior

--- devbox/apps/Layout.js
+++ devbox/apps/Layout.js
@@ -2,32 +2,21 @@ import React from 'react'
 import { Layout, useLayout } from '@aragon/ui'
 
 function App() {
-  const { name, width } = useLayout()
+  const { layoutName, layoutWidth } = useLayout()
   return (
     <div
       css={`
-        width: 100vw;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         height: 100vh;
-        background: #789;
+        font-size: 20px;
+        border: 2px solid #000;
+        border-top: 0;
+        border-bottom: 0;
       `}
     >
-      <Layout>
-        <div
-          css={`
-            height: 100%;
-            width: 100%;
-            border: 2px solid #FFF;
-            color: #fff;
-            font-size: 30px;
-            padding: 40px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          `}
-        >
-          Layout: {name} ({width}px)
-        </div>
-      </Layout>
+      Layout: {layoutName} ({layoutWidth}px)
     </div>
   )
 }
