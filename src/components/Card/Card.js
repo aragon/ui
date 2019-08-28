1f9cf88b596e08f88badbbcd60a407879a0ed822
Pierre Bertet <hello@pierre.world>
2019-08-28 23:50:48 +0200

    Card: fix the layout differences in interactive mode (#541)
    Move to an inner <div> element to avoid any layout specificity with <button>.
    
    Also add a new `element` prop to ButtonBase to force a specific HTML element to be used.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { css } from 'styled-components'
 import { GU, RADIUS } from '../../style'
 import { useTheme } from '../../theme'
-import { unselectable, useInside } from '../../utils'
+import { useInside } from '../../utils'
 import ButtonBase from '../ButtonBase/ButtonBase'
 
 const DEFAULT_WIDTH = 35 * GU
@@ -25,35 +25,43 @@ function Card({ children, width, height, onClick, ...props }) {
   const interactive = Boolean(onClick)
 
   const interactiveProps = interactive
-    ? { as: ButtonBase, focusRingRadius: RADIUS, onClick }
+    ? { as: ButtonBase, element: 'div', focusRingRadius: RADIUS, onClick }
     : {}
+
+  const cssWidth = dimension(insideCardLayout, width, `${DEFAULT_WIDTH}px`)
+  const cssHeight = dimension(insideCardLayout, height, `${DEFAULT_HEIGHT}px`)
 
   return (
     <div
       {...interactiveProps}
       css={`
         position: relative;
-        width: ${dimension(insideCardLayout, width, `${DEFAULT_WIDTH}px`)};
-        height: ${dimension(insideCardLayout, height, `${DEFAULT_HEIGHT}px`)};
+        width: ${cssWidth};
+        height: ${cssHeight};
         background: ${theme.surface};
         border: 1px solid ${theme.border};
         border-radius: ${RADIUS}px;
         cursor: ${interactive ? 'pointer' : 'default'};
+
+        // Default layout
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
 
         ${interactive &&
           css`
-            border: 0;
             box-shadow: 0px 1px 3px rgba(51, 77, 117, 0.15);
-            transition-property: top, box-shadow;
+            transition-property: transform, box-shadow;
             transition-duration: 50ms;
             transition-timing-function: ease-in-out;
-            ${unselectable};
+
+            // Reset some styles applied by ButtonBase
+            text-align: left;
+            white-space: normal;
 
             &:active {
-              top: 1px;
+              transform: translateY(1px);
               box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.075);
             }
           `}
