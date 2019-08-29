cdb937083467e7cc21ecbdde8168d72f5ef47489
Brett Sun <qisheng.brett.sun@gmail.com>
2019-08-29 17:27:55 +0200

    Popover: add Root to devbox app

--- devbox/apps/Popover.js
+++ devbox/apps/Popover.js
@@ -1,6 +1,5 @@
-import React, { useRef, useState, useCallback } from 'react'
-import styled from 'styled-components'
-import { Popover, Root, Button } from '@aragon/ui'
+import React, { useState, useCallback } from 'react'
+import { Button, Popover, Root, useTheme } from '@aragon/ui'
 
 const POSITIONS = [
   ['flex-start', 'flex-start'],
@@ -14,7 +13,9 @@ const POSITIONS = [
   ['flex-end', 'flex-end'],
 ]
 
-function App({ title }) {
+function App() {
+  const theme = useTheme()
+
   const [show, setShow] = useState(false)
   const [opener, setOpener] = useState(null)
   const [position, setPosition] = useState(4)
@@ -24,72 +25,79 @@ function App({ title }) {
   }, [])
 
   return (
-    <div
-      css={`
-        display: grid;
-        height: 100vh;
-        padding: 20px;
-        justify-content: ${POSITIONS[position][0]};
-        align-items: ${POSITIONS[position][1]};
-      `}
-    >
-      <div>
-        <div
-          css={`
-            position: absolute;
-            top: ${position === 4 ? 'calc(50% - 150px)' : 'calc(50% - 50px)'};
-            left: calc(50% - 50px);
-            display: grid;
-            width: 100px;
-            height: 100px;
-            grid-template-columns: 1fr 1fr 1fr;
-            grid-template-rows: 1fr 1fr 1fr;
-            grid-gap: 5px;
-            div {
-              border-radius: 3px;
-              border: 2px solid hsl(200, 70%, 70%);
-              cursor: pointer;
-              &:active {
-                background: hsl(200, 70%, 70%);
+    <Root.Provider>
+      <div
+        css={`
+          display: grid;
+          height: calc(100vh - 40px);
+          margin: 20px;
+          border: 2px solid ${theme.border};
+          justify-content: ${POSITIONS[position][0]};
+          align-items: ${POSITIONS[position][1]};
+        `}
+      >
+        <div>
+          <div
+            css={`
+              position: absolute;
+              top: ${position === 4 ? 'calc(50% - 150px)' : 'calc(50% - 50px)'};
+              left: calc(50% - 50px);
+              display: grid;
+              width: 100px;
+              height: 100px;
+              grid-template-columns: 1fr 1fr 1fr;
+              grid-template-rows: 1fr 1fr 1fr;
+              grid-gap: 5px;
+              div {
+                border-radius: 3px;
+                border: 2px solid hsl(200, 70%, 70%);
+                cursor: pointer;
+                &:active {
+                  background: hsl(200, 70%, 70%);
+                }
+                &.active {
+                  background: hsl(200, 70%, 70%);
+                }
               }
-              &.active {
-                background: hsl(200, 70%, 70%);
+            `}
+            onClick={e => {
+              const index = Number(e.target.dataset.index)
+              if (!isNaN(index)) {
+                setPosition(index)
               }
-            }
-          `}
-          onClick={e => {
-            const index = Number(e.target.dataset.index)
-            if (!isNaN(index)) {
-              setPosition(index)
-            }
-          }}
-        >
-          {[...Array(9)].map((_, i) => (
-            <div
-              key={i}
-              data-index={i}
-              className={i === position ? 'active' : ''}
-            />
-          ))}
-        </div>
-        <Button
-          ref={handleOpenerRef}
-          onClick={() => setShow(true)}
-          size="small"
-          mode="strong"
-        >
-          Open
-        </Button>
-        <Popover opener={opener} visible={show} onClose={() => setShow(false)}>
-          <div css="padding: 50px">
-            <span>hello </span>
-            <Button mode="secondary" size="mini">
-              focus me
-            </Button>
+            }}
+          >
+            {[...Array(9)].map((_, i) => (
+              <div
+                key={i}
+                data-index={i}
+                className={i === position ? 'active' : ''}
+              />
+            ))}
           </div>
-        </Popover>
+          <Button
+            ref={handleOpenerRef}
+            onClick={() => setShow(true)}
+            size="small"
+            mode="strong"
+          >
+            Open
+          </Button>
+          <Popover
+            opener={opener}
+            visible={show}
+            onClose={() => setShow(false)}
+          >
+            <div css="padding: 50px">
+              <span>hello </span>
+              <Button mode="secondary" size="mini">
+                focus me
+              </Button>
+            </div>
+          </Popover>
+        </div>
       </div>
-    </div>
+    </Root.Provider>
   )
 }
 
