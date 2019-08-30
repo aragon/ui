03efbab9d65480625ead825c7a9745b0ede8af40
Pierre Bertet <hello@pierre.world>
2019-08-30 09:35:22 +0200

    Box: make padding content-only, and only accept numbers

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,54 +1,9 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { GU, RADIUS, textStyle } from '../../style'
 import { useTheme } from '../../theme/Theme'
 import { useLayout } from '../Layout/Layout'
-import { useInside } from '../../utils'
-
-function getHeadingPadding(padding, defaultPadding) {
-  // only follow the shared padding if not 0
-  if (typeof padding === 'number') {
-    return padding === 0 ? defaultPadding : padding
-  }
-
-  // always follow the heading padding if set independently
-  if (typeof padding === 'object' && typeof padding.heading === 'number') {
-    return padding.heading
-  }
-
-  // 0 if heading is set to false independently
-  if (typeof padding === 'object' && padding.heading === false) {
-    return 0
-  }
-
-  // default padding in all the other cases
-  return defaultPadding
-}
-
-function getContentPadding(padding, defaultPadding) {
-  // always follow the shared padding if set
-  if (typeof padding === 'number') {
-    return padding
-  }
-
-  // use the default padding if set to true, 0 otherwise
-  if (typeof padding === 'boolean') {
-    return padding ? defaultPadding : 0
-  }
-
-  // always follow the content padding if set independently
-  if (typeof padding === 'object' && typeof padding.content === 'number') {
-    return padding.content
-  }
-
-  // always 0 if content is set to false independently
-  if (typeof padding === 'object' && padding.content === false) {
-    return 0
-  }
-
-  // default padding in all the other cases
-  return defaultPadding
-}
+import { useInside, warnOnce } from '../../utils'
 
 function Box({ heading, children, padding, ...props }) {
   const theme = useTheme()
@@ -56,14 +11,24 @@ function Box({ heading, children, padding, ...props }) {
   const { layoutName } = useLayout()
   const fullWidth = layoutName === 'small'
 
-  const [headingPadding, contentPadding] = useMemo(() => {
-    const defaultPadding = (fullWidth ? 2 : insideSplitPrimary ? 5 : 3) * GU
+  const defaultPadding = (fullWidth ? 2 : insideSplitPrimary ? 5 : 3) * GU
 
-    return [
-      getHeadingPadding(padding, defaultPadding),
-      getContentPadding(padding, defaultPadding),
-    ]
-  }, [fullWidth, insideSplitPrimary, padding])
+  if (padding === true) {
+    warnOnce(
+      'Box:padding:true',
+      'Box: setting true on the padding prop is deprecated. Omit it, or set it to undefined instead.'
+    )
+    padding = defaultPadding
+  }
+  if (padding === false) {
+    warnOnce(
+      'Box:padding:false',
+      'Box: setting false on the padding prop is deprecated. Use 0.'
+    )
+    padding = 0
+  }
+
+  const contentPadding = padding === undefined ? defaultPadding : padding
 
   return (
     <div
@@ -96,7 +61,7 @@ function Box({ heading, children, padding, ...props }) {
           ) : (
             <h1
               css={`
-                padding: 0 ${headingPadding}px;
+                padding: 0 ${defaultPadding}px;
                 ${textStyle('label2')};
                 color: ${theme.surfaceContentSecondary};
               `}
@@ -121,17 +86,11 @@ Box.propTypes = {
   heading: PropTypes.node,
   children: PropTypes.node,
   padding: PropTypes.oneOfType([
-    PropTypes.bool,
     PropTypes.number,
-    PropTypes.shape({
-      heading: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
-      content: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
-    }),
-  ]),
-}
 
-Box.defaultProps = {
-  padding: true,
+    // deprecated
+    PropTypes.bool,
+  ]),
 }
 
 export { Box }
