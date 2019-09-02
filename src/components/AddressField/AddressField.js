776af114961e872564c204fd580a95903f83c946
Brett Sun <qisheng.brett.sun@gmail.com>
2019-09-02 12:51:31 +0200

    AddressField: update for new styles

--- src/components/AddressField/AddressField.js
+++ src/components/AddressField/AddressField.js
@@ -1,124 +1,130 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
-import { IconCopy } from '../../icons/components'
+import { IconCopy } from '../../icons/'
+import { GU, RADIUS } from '../../style'
+import { useTheme } from '../../theme'
+import { monospace, noop, warn } from '../../utils'
 import TextInput from '../Input/TextInput'
 import { ButtonIcon } from '../Button/ButtonIcon'
 import EthIdenticon from '../EthIdenticon/EthIdenticon'
 import { Toast } from '../ToastHub/ToastHub'
-import { theme } from '../../theme-legacy'
-import { font, noop, warn } from '../../utils'
 
-class AddressFieldBase extends React.PureComponent {
-  static propTypes = {
-    address: PropTypes.string.isRequired,
-    onCopy: PropTypes.func,
-    icon: PropTypes.node,
-  }
-  _input = React.createRef()
-  componentDidMount() {
-    setTimeout(() => {
-      this._input.current.focus()
-    }, 0)
-  }
-  handleFocus = () => {
-    this._input.current.select()
-  }
-  handleCopy = () => {
-    const { onCopy } = this.props
-    this._input.current.focus()
-    this._input.current.select()
+const HEIGHT_ADJUSTED_FOR_BORDER = 5 * GU - 2
 
-    try {
-      document.execCommand('copy')
-      onCopy('Address copied')
-    } catch (err) {
-      warn(err)
+const AddressFieldBase = React.memo(function AddressFieldBase({
+  address,
+  icon,
+  onCopy,
+}) {
+  const theme = useTheme()
+  const inputRef = useRef(null)
+
+  const handleFocus = useCallback(() => {
+    inputRef.current && inputRef.current.select()
+  }, [])
+  const handleCopy = useCallback(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+      inputRef.current.select()
+
+      try {
+        document.execCommand('copy')
+        onCopy('Address copied')
+      } catch (err) {
+        warn(err)
+      }
+
+      inputRef.current.focus()
     }
+  }, [onCopy])
 
-    this._input.current.focus()
-  }
-  render() {
-    const { address, icon } = this.props
-    return (
+  useEffect(() => {
+    setTimeout(() => {
+      inputRef.current && inputRef.current.focus()
+    }, 0)
+  }, [])
+
+  return (
+    <div
+      css={`
+        position: relative;
+        display: inline-flex;
+        max-width: 100%;
+        height: ${5 * GU}px;
+        padding-left: ${5 * GU}px;
+        background: ${theme.surface};
+        border: 1px solid ${theme.border};
+        border-radius: ${RADIUS}px;
+        box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.06);
+      `}
+    >
       <div
         css={`
-          display: inline-flex;
-          max-width: 100%;
+          position: absolute;
+          top: -1px;
+          left: -1px;
           height: 40px;
-          position: relative;
-          background: ${theme.contentBackground};
-          border: 1px solid ${theme.contentBorder};
-          border-radius: 3px;
-          box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.06);
-          padding-left: 40px;
-          padding-right: 30px;
+          overflow: hidden;
+          border-radius: ${RADIUS}px 0 0 ${RADIUS}px;
+          border: 1px solid transparent;
         `}
       >
-        <div
-          css={`
-            position: absolute;
-            top: -1px;
-            left: -1px;
-            height: 40px;
-            overflow: hidden;
-            border-radius: 3px 0 0 3px;
-            border: 1px solid transparent;
-          `}
-        >
-          {icon || (
-            <EthIdenticon
-              address={address}
-              scale={2}
-              css={`
-                transform: scale(calc(38 / 48));
-                transform-origin: 0 0;
-              `}
-            />
-          )}
-        </div>
-        <TextInput
-          ref={this._input}
-          value={address}
-          onFocus={this.handleFocus}
-          readOnly
-          css={`
-            text-overflow: ellipsis;
-            width: 390px;
-            max-width: 100%;
-            border: 0;
-            box-shadow: none;
-            background: transparent;
-            ${icon ? 'padding-left: 8px' : ''};
-            ${font({ monospace: true })};
-            &:read-only {
-              color: ${theme.textPrimary};
-              text-shadow: none;
-            }
-          `}
-        />
-        <ButtonIcon
-          onClick={this.handleCopy}
-          label="Copy"
-          css={`
-            position: absolute;
-            top: 0;
-            right: 0;
-            width: 39px;
-            height: 38px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            border-radius: 0 3px 3px 0;
-            &:active {
-              background: rgba(220, 234, 239, 0.3);
-            }
-          `}
-        >
-          <IconCopy />
-        </ButtonIcon>
+        {icon || (
+          <EthIdenticon
+            address={address}
+            scale={2}
+            css={`
+              transform: scale(calc(38 / 48));
+              transform-origin: 0 0;
+            `}
+          />
+        )}
       </div>
-    )
-  }
+      <TextInput
+        ref={inputRef}
+        value={address}
+        onFocus={handleFocus}
+        readOnly
+        adornment={
+          <ButtonIcon
+            onClick={handleCopy}
+            label="Copy"
+            css={`
+              width: ${HEIGHT_ADJUSTED_FOR_BORDER}px;
+              height: ${HEIGHT_ADJUSTED_FOR_BORDER}px;
+              color: ${theme.surfaceIcon};
+            `}
+          >
+            <IconCopy />
+          </ButtonIcon>
+        }
+        adornmentPosition="end"
+        adornmentSettings={{
+          // Keep the button square
+          width: HEIGHT_ADJUSTED_FOR_BORDER,
+          padding: 0,
+        }}
+        css={`
+          text-overflow: ellipsis;
+          height: ${HEIGHT_ADJUSTED_FOR_BORDER}px;
+          width: ${52.5 * GU}px;
+          max-width: 100%;
+          border: 0;
+          ${icon ? `padding-left: ${1 * GU}px` : ''};
+          ${monospace};
+          &:read-only {
+            color: ${theme.surfaceContent};
+            text-shadow: none;
+          }
+        `}
+      />
+    </div>
+  )
+})
+AddressFieldBase.propTypes = {
+  address: PropTypes.string.isRequired,
+  icon: PropTypes.node,
+  onCopy: PropTypes.func,
 }
 
 const AddressField = props =>
