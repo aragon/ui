c78714407ebb6feb850e09ddc9eabfad0c5c8302
Brett Sun <qisheng.brett.sun@gmail.com>
2019-09-04 23:44:50 +0200

    Badges: fix alignment in devbox (#575)

--- devbox/apps/AppBadge.js
+++ devbox/apps/AppBadge.js
@@ -29,7 +29,7 @@ class App extends React.Component {
                 <div
                   css={`
                     display: flex;
-                    align-content: center;
+                    align-items: center;
                   `}
                 >
                   <IconLabel
