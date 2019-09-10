b1df48358f9979545b3cd47641f6e587d8b77c7d
Brett Sun <qisheng.brett.sun@gmail.com>
2019-09-10 02:45:18 +0100

    AppBadge: rename internal variable names to be AppBadge (#590)
    * AppBadge: rename internal variable names to be AppBadge
    * AppBadge: mark appAddress as non-required

--- src/components/AppBadge/AppBadge.js
+++ src/components/AppBadge/AppBadge.js
@@ -9,7 +9,7 @@ import { Tag } from '../Tag/Tag'
 import AppBadgePopover from './AppBadgePopover'
 import iconDefaultSvg from './assets/app-default.svg'
 
-const IdentityBadge = React.memo(function IdentityBadge({
+const AppBadge = React.memo(function AppBadge({
   appAddress,
   badgeOnly,
   compact,
@@ -99,8 +99,8 @@ const IdentityBadge = React.memo(function IdentityBadge({
     </BadgeBase>
   )
 })
-IdentityBadge.propTypes = {
-  appAddress: PropTypes.string.isRequired,
+AppBadge.propTypes = {
+  appAddress: PropTypes.string,
   badgeOnly: PropTypes.bool,
   compact: PropTypes.bool,
   iconSrc: PropTypes.string,
@@ -136,4 +136,4 @@ Icon.propTypes = {
   src: PropTypes.string.isRequired,
 }
 
-export default IdentityBadge
+export default AppBadge
