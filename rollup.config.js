c356ab680ac096f06f5a849cb59a842f9e45066e
Pierre Bertet <hello@pierre.world>
2019-10-09 16:10:21 +0200

    Add back the CommonJS build

--- rollup.config.js
+++ rollup.config.js
@@ -19,6 +19,12 @@ export default {
       preserveModules: true,
       sourcemap: true,
     },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      preserveModules: false,
+      sourcemap: true,
+    },
   ],
   external: ['react', 'react-dom', 'styled-components'],
   treeshake: production,
