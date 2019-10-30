21aaa1c417d127297c29a79fe6c0eb678985322c
Pierre Bertet <hello@pierre.world>
2019-10-30 19:20:41 +0100

    TextInput: fix placeholder and readonly colors (#631)

--- devbox/apps/Input.js
+++ devbox/apps/Input.js
@@ -7,44 +7,54 @@ function App() {
   return (
     <div
       css={`
-        display: flex;
-        flex-direction: column;
-        gap: 20px;
+        display: grid;
         height: 100vh;
         align-items: center;
         justify-content: center;
       `}
     >
-      <TextInput
-        css="width: 80px"
-        adornment="ETH"
-        adornmentPosition="end"
-        adornmentSettings={{
-          width: 55,
-          padding: 8,
-        }}
-      />
-      <TextInput
-        css="width: 80px"
-        adornment="$"
-        adornmentPosition="start"
-        adornmentSettings={{
-          width: 36,
-          padding: 8,
-        }}
-      />
-      <TextInput />
-      <TextInput adornment={<IconBlank />} adornmentPosition="start" />
-      <TextInput adornment={<IconBlank />} adornmentPosition="end" />
-      <TextInput.Multiline />
-      <SearchInput
-        value={searchTerm}
-        onChange={(value, ev) => {
-          console.log('Search term changed: ', value)
-          console.log('Search change event: ', ev)
-          setSearchTerm(value)
-        }}
-      />
+      <div
+        css={`
+          display: grid;
+          grid-gap: 20px;
+          padding: 40px 0;
+        `}
+      >
+        <TextInput placeholder="Enter something" readOnly value="Enter" />
+        <form>
+          <TextInput placeholder="Enter something" pattern="[A-Za-z]+" />
+        </form>
+        <TextInput
+          css="width: 80px"
+          adornment="ETH"
+          adornmentPosition="end"
+          adornmentSettings={{
+            width: 55,
+            padding: 8,
+          }}
+        />
+        <TextInput
+          css="width: 80px"
+          adornment="$"
+          adornmentPosition="start"
+          adornmentSettings={{
+            width: 36,
+            padding: 8,
+          }}
+        />
+        <TextInput />
+        <TextInput adornment={<IconBlank />} adornmentPosition="start" />
+        <TextInput adornment={<IconBlank />} adornmentPosition="end" />
+        <TextInput.Multiline />
+        <SearchInput
+          value={searchTerm}
+          onChange={(value, ev) => {
+            console.log('Search term changed: ', value)
+            console.log('Search change event: ', ev)
+            setSearchTerm(value)
+          }}
+        />
+      </div>
     </div>
   )
 }
