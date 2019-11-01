a8aa3536f7dec7286726c25f969692e8b9d9219c
Pierre Bertet <hello@pierre.world>
2019-11-01 15:09:10 +0100

    BadgeBase: minor tweaks / coding style (#646)

--- src/components/BadgeBase/BadgeBase.js
+++ src/components/BadgeBase/BadgeBase.js
@@ -11,8 +11,8 @@ const BadgeBase = React.memo(function BadgeBase({
   className,
   compact,
   disabled,
-  icon,
   href,
+  icon,
   label,
   labelStyle,
   onClick,
@@ -22,28 +22,31 @@ const BadgeBase = React.memo(function BadgeBase({
 }) {
   const theme = useTheme()
   const [insideDropDownMenu] = useInside('DropDown')
-  const isDisabled = disabled || insideDropDownMenu
+
+  if (insideDropDownMenu) {
+    disabled = true
+  }
 
   return (
     <React.Fragment>
       <ButtonBase
         ref={badgeRef}
         title={title}
-        disabled={isDisabled}
-        element={href || insideDropDownMenu || isDisabled ? 'a' : 'button'}
-        onClick={!isDisabled ? onClick : undefined}
-        href={!isDisabled ? href : undefined}
+        disabled={disabled}
+        element={href || disabled ? 'a' : 'button'}
+        onClick={!disabled ? onClick : undefined}
+        href={!disabled ? href : undefined}
         focusRingRadius={RADIUS}
         css={`
           display: inline-flex;
           overflow: hidden;
           color: ${theme.badgeContent};
           height: ${3 * GU}px;
-          ${insideDropDownMenu ? 'cursor: pointer;' : ''}
+          background: ${compact ? 'transparent' : theme.badge};
+          ${insideDropDownMenu ? 'cursor: pointer' : ''};
+
           &:active {
-            ${compact && !isDisabled
-              ? `background: ${theme.badgePressed};`
-              : ''};
+            ${!disabled && compact ? `background: ${theme.badgePressed}` : ''};
           }
         `}
       >
@@ -52,18 +55,17 @@ const BadgeBase = React.memo(function BadgeBase({
             overflow: hidden;
             display: flex;
             align-items: center;
+            text-decoration: none;
             ${compact
               ? `
-                padding: 0 ${1 * GU}px;
-                border-radius: 2px;
-              `
+                  padding: 0 ${1 * GU}px;
+                  border-radius: 2px;
+                `
               : `
-                padding-left: ${(icon ? 0 : 1.5) * GU}px;
-                padding-right: ${(icon ? 1 : 1.5) * GU}px;
-                background: ${theme.badge};
-                border-radius: ${RADIUS}px;
-              `};
-            text-decoration: none;
+                  padding-left: ${(icon ? 0 : 1.5) * GU}px;
+                  padding-right: ${(icon ? 1 : 1.5) * GU}px;
+                  border-radius: ${RADIUS}px;
+                `};
           `}
           className={className}
           style={style}
@@ -83,11 +85,12 @@ const BadgeBase = React.memo(function BadgeBase({
         </div>
       </ButtonBase>
       {typeof children === 'function'
-        ? children(isDisabled) // whether popover is disabled
+        ? children(disabled) // whether popover is disabled
         : children}
     </React.Fragment>
   )
 })
+
 BadgeBase.propTypes = {
   badgeRef: PropTypes.any,
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
