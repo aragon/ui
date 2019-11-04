b1524d6c6c879a5b8c3000e072f4e13886c0052b
Pierre Bertet <hello@pierre.world>
2019-11-04 03:03:40 +0100

    devbox: tweak TextCopy demo

--- devbox/apps/TextCopy.js
+++ devbox/apps/TextCopy.js
@@ -1,42 +1,49 @@
 import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components'
-import { EthIdenticon, IconBlank, SearchCopy, TextCopy } from '@aragon/ui'
+import { EthIdenticon, IconBlank, SearchCopy, TextCopy, GU } from '@aragon/ui'
 
 const ADDRESS = '0x2c9341a52cfa3f2c2554ca1803134137b9366b3c'
-
-const GU = 8
 const HEIGHT = 5 * GU
+const IDENTICON_SIZE = 6 * GU
 
 function App() {
   return (
     <div
       css={`
         display: flex;
-        flex-direction: column;
-        gap: 20px;
-        height: 100vh;
         align-items: center;
         justify-content: center;
+        height: 100vh;
       `}
     >
-      <TextCopy value="Copy me!" />
-      <TextCopy value="Focused on mount" autofocus />
-      <TextCopy
-        adornment={
-          <EthIdenticon
-            address={ADDRESS}
-            scale={2}
-            css={`
-              transform: scale(calc(${HEIGHT} / 48));
-              transform-origin: 0 0;
-              position: absolute;
-              left: 0;
-              top: 0;
-            `}
+      <div
+        css={`
+          display: grid;
+          grid-gap: 20px;
+        `}
+      >
+        <div>
+          <TextCopy value="Copy me!" />
+        </div>
+        <div>
+          <TextCopy value="Focused on mount" autofocus />
+        </div>
+        <div>
+          <TextCopy
+            adornment={
+              <EthIdenticon
+                address={ADDRESS}
+                scale={2}
+                css={`
+                  transform: scale(${(HEIGHT - 2) / IDENTICON_SIZE});
+                  transform-origin: 0 0;
+                `}
+              />
+            }
+            value="With adornment"
           />
-        }
-        value="With an icon"
-      />
+        </div>
+      </div>
     </div>
   )
 }
