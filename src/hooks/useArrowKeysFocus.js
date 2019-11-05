88d376dedcf744d54c6f856c0513090ba62afdfd
Gorka Ludlow <gorka@aquigorka.com>
2019-11-05 02:38:49 +0100

    Autocomplete refactor to use SearchInput (#609)

--- src/hooks/useArrowKeysFocus.js
+++ src/hooks/useArrowKeysFocus.js
@@ -1,9 +1,7 @@
 import { useState, useCallback, useEffect } from 'react'
 import { KEY_UP, KEY_DOWN } from '../utils'
 
-/* eslint-disable react-hooks/rules-of-hooks */
 export function useArrowKeysFocus(refs) {
-  /* eslint-enable react-hooks/rules-of-hooks */
   const [highlightedIndex, setHighlightedIndex] = useState(-1)
 
   const cycleFocus = useCallback(
@@ -20,15 +18,21 @@ export function useArrowKeysFocus(refs) {
     },
     [highlightedIndex, refs.length]
   )
+
   const handleKeyDown = useCallback(
-    e => {
-      const { keyCode } = e
+    event => {
+      const { keyCode } = event
       if (keyCode === KEY_UP || keyCode === KEY_DOWN) {
-        cycleFocus(e, keyCode === KEY_UP ? -1 : 1)
+        cycleFocus(event, keyCode === KEY_UP ? -1 : 1)
       }
     },
     [cycleFocus]
   )
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown)
+    return () => document.removeEventListener('keydown', handleKeyDown)
+  }, [handleKeyDown])
 
   useEffect(() => {
     if (highlightedIndex === -1) {
@@ -39,13 +43,9 @@ export function useArrowKeysFocus(refs) {
     }
     refs[highlightedIndex].focus()
   }, [highlightedIndex, refs])
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown)
-    return () => document.removeEventListener('keydown', handleKeyDown)
-  }, [handleKeyDown])
 
   return {
     highlightedIndex,
-    setHighlightedIndex: index => () => setHighlightedIndex(index),
+    setHighlightedIndex,
   }
 }
