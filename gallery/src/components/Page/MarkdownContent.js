b5ca22e6e51b70bf5986ffc5eff43583785b792f
Pierre Bertet <hello@pierre.world>
2019-11-07 16:12:51 +0100

    Gallery: set the table styles on the markdown content only

--- gallery/src/components/Page/MarkdownContent.js
+++ gallery/src/components/Page/MarkdownContent.js
@@ -60,6 +60,56 @@ function StyledMarkdown(props) {
           position: relative;
           top: -1px;
         }
+
+        table {
+          margin-bottom: ${4 * GU}px;
+          border-collapse: separate;
+          border-radius: ${RADIUS}px;
+          border-spacing: 0;
+        }
+
+        tbody {
+          border-radius: ${RADIUS}px;
+        }
+
+        td {
+          padding: ${1 * GU}px;
+          background: ${theme.surface};
+        }
+
+        // Still no border-radius on tr / tbody
+        tbody tr:first-child td {
+          border-top: 1px solid ${theme.border};
+        }
+        tbody tr:last-child td {
+          border-bottom: 1px solid ${theme.border};
+        }
+        tbody tr td:first-child {
+          border-left: 1px solid ${theme.border};
+        }
+        tbody tr td:last-child {
+          border-right: 1px solid ${theme.border};
+        }
+        tbody tr:first-child td:first-child {
+          border-top-left-radius: ${RADIUS}px;
+        }
+        tbody tr:first-child td:last-child {
+          border-top-right-radius: ${RADIUS}px;
+        }
+        tbody tr:last-child td:first-child {
+          border-bottom-left-radius: ${RADIUS}px;
+        }
+        tbody tr:last-child td:last-child {
+          border-bottom-right-radius: ${RADIUS}px;
+        }
+
+        th {
+          padding: ${0 * GU}px ${2 * GU}px ${1 * GU}px ${1 * GU}px;
+          font-weight: 400;
+          text-align: left;
+          ${textStyle('label2')};
+          color: ${theme.surfaceContentSecondary};
+        }
       `}
       {...props}
     />
