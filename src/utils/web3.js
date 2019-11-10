3884be63bcc70b4c9616cea5b3829dfb9beddf1b
Pierre Bertet <hello@pierre.world>
2019-11-10 22:35:45 +0100

    tokenIconUrl(): new source format (checksum addresses)
    The format has changed on the source repository [1], and token icons can
    now only be accessed through checksum addresses.
    
    [1] https://github.com/trustwallet/assets/pull/502

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,3 +1,4 @@
+import sha3 from 'js-sha3'
 import { warn } from './environment'
 
 const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000'
@@ -38,6 +39,46 @@ const BLOCK_EXPLORERS = {
     const typePart = ETHERSCAN_TYPES.get(type)
     return `https://${subdomain}etherscan.io/${typePart}/${value}`
   },
+}
+
+/**
+ * Converts to a checksum address
+ *
+ * This function is taken from web3-utils:
+ * https://github.com/ethereum/web3.js/blob/22df832303e349f8ae02f0392e56abe10e1dfaac/packages/web3-utils/src/index.js#L287-L315
+ * And was adapted to use js-sha3 rather than soliditySha3.js from web3.js, in
+ * order to avoid adding the BN.js and underscore dependencies.
+ *
+ * @method toChecksumAddress
+ * @param {String} address the given HEX address
+ * @return {String}
+ */
+function toChecksumAddress(address) {
+  if (typeof address === 'undefined') {
+    return ''
+  }
+
+  if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+    throw new Error(
+      'Given address "' + address + '" is not a valid Ethereum address.'
+    )
+  }
+
+  address = address.toLowerCase().replace(/^0x/i, '')
+
+  const addressHash = sha3.keccak_256(address).replace(/^0x/i, '')
+  let checksumAddress = '0x'
+
+  for (let i = 0; i < address.length; i++) {
+    // If ith character is 9 to f then make it uppercase
+    if (parseInt(addressHash[i], 16) > 7) {
+      checksumAddress += address[i].toUpperCase()
+    } else {
+      checksumAddress += address[i]
+    }
+  }
+
+  return checksumAddress
 }
 
 /**
@@ -149,6 +190,12 @@ export function tokenIconUrl(address = '') {
   address = address.trim().toLowerCase()
 
   if (!address) {
+    return ''
+  }
+
+  try {
+    address = toChecksumAddress(address)
+  } catch (err) {
     return ''
   }
 
