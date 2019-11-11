f7517dbacd1b706fc502424a0335525c4aebd5cb
Pierre Bertet <hello@pierre.world>
2019-11-11 11:35:16 +0100

    devbox: update AddressField demo

--- devbox/apps/AddressField.js
+++ devbox/apps/AddressField.js
@@ -1,23 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
-import { AddressField } from '@aragon/ui'
+import { AddressField, tokenIconUrl } from '@aragon/ui'
 
-class App extends React.Component {
-  render() {
-    return (
-      <Main>
-        <AddressField address="0x2c9341a52cfa3f2c2554ca1803134137b9366b3c" />
-      </Main>
-    )
-  }
+function App() {
+  console.log('ABC', tokenIconUrl('0x0000000000000000000000000000000000000000'))
+  return (
+    <div
+      css={`
+        display: grid;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+      `}
+    >
+      <div
+        css={`
+          display: grid;
+          grid-gap: 24px;
+        `}
+      >
+        <div>
+          <AddressField address="0x2c9341a52cfa3f2c2554ca1803134137b9366b3c" />
+        </div>
+        <div>
+          <AddressField
+            address="0x0000000000000000000000000000000000000000"
+            icon={
+              <img
+                width="30"
+                height="30"
+                src={tokenIconUrl('0x0000000000000000000000000000000000000000')}
+                alt=""
+              />
+            }
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
-
-const Main = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  padding: 20px;
-`
 
 export default App
