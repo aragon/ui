993c37f8dbe9f5380580cd9ec7c424e370e10f5a
Pierre Bertet <hello@pierre.world>
2019-11-12 03:18:28 +0100

    Tweak gallery pages

--- gallery/src/pages/PageDataView.js
+++ gallery/src/pages/PageDataView.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { AddressField } from '@aragon/ui'
 
 import Page from 'comps/Page/Page'
 import readme from 'ui-src/components/DataView/README.md'
 
-const PageDataView = ({ title }) => <Page title={title} readme={readme} />
+function PageDataView({ title }) {
+  return <Page title={title} readme={readme} />
+}
 
 export default PageDataView
