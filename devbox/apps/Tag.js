c1c7c4b130747c558d15505903d3f31a8d42e8ae
Pierre Bertet <hello@pierre.world>
2019-11-18 13:54:49 +0100

    Tag: support 0 for numbers passed as labels without digit limit (#661)

--- devbox/apps/Tag.js
+++ devbox/apps/Tag.js
@@ -19,6 +19,8 @@ const ITEMS = new Map([
   [
     'activity',
     [
+      { label: 0 },
+      { label: <span>test</span> },
       { limitDigits: true, label: 1 },
       { limitDigits: true, label: 9879 },
       { limitDigits: 4, label: 9877987 },
