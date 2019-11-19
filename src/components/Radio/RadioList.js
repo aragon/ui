668fb54c0777a2581ccfcb34b49996aea927223d
Pierre Bertet <hello@pierre.world>
2019-11-19 22:51:44 +0100

    RadioList: remove Text referencese

--- src/components/Radio/RadioList.js
+++ src/components/Radio/RadioList.js
@@ -1,76 +1,85 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import Text from '../Text/Text'
+import { GU } from '../../style'
+import { noop } from '../../utils'
 import RadioGroup from './RadioGroup'
 import RadioListItem from './RadioListItem'
-import { noop, stylingProps } from '../../utils'
-import { GU } from '../../style'
 
-class RadioList extends React.Component {
-  static propTypes = {
-    description: PropTypes.node,
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        description: PropTypes.node.isRequired,
-        title: PropTypes.node.isRequired,
-      })
-    ),
-    onChange: PropTypes.func,
-    selected: ({ items, selected }, _, componentName) => {
-      if (!Number.isInteger(selected) || selected >= items.length) {
-        throw new Error(
-          `Invalid prop \`selected\` supplied to \`${componentName}\`. ` +
-            '`selected` must be an integer less than the length of `items`. ' +
-            `Given ${selected} instead.`
-        )
-      }
-    },
-    title: PropTypes.node,
-  }
-  static defaultProps = {
-    items: [],
-    onChange: noop,
-    selected: 0,
-  }
-  render() {
-    const { description, items, onChange, selected, title } = this.props
-    return (
-      <div {...stylingProps(this)}>
-        {title && (
-          <Title>
-            <Text size="large" weight="bold">
-              {title}
-            </Text>
-          </Title>
-        )}
-        {description && <Description>{description}</Description>}
-        <StyledRadioGroup onChange={onChange} selected={selected}>
-          {items.map(({ description, title }, i) => (
-            <RadioListItem
-              key={i}
-              description={description}
-              index={i}
-              title={title}
-            />
-          ))}
-        </StyledRadioGroup>
-      </div>
-    )
-  }
+function RadioList({
+  description,
+  items,
+  onChange,
+  selected,
+  title,
+  ...props
+}) {
+  return (
+    <div {...props}>
+      {title && (
+        <h2
+          css={`
+            margin-bottom: ${0.5 * GU}px;
+            font-weight: 600;
+          `}
+        >
+          {title}
+        </h2>
+      )}
+      {description && (
+        <div
+          css={`
+            margin-bottom: ${2.5 * GU}px;
+          `}
+        >
+          {description}
+        </div>
+      )}
+      <RadioGroup
+        onChange={onChange}
+        selected={selected}
+        css={`
+          display: flex;
+          flex-direction: column;
+        `}
+      >
+        {items.map(({ description, title }, i) => (
+          <RadioListItem
+            key={i}
+            description={description}
+            index={i}
+            title={title}
+          />
+        ))}
+      </RadioGroup>
+    </div>
+  )
 }
 
-const Title = styled.h2`
-  margin-bottom: ${0.5 * GU}px;
-`
+RadioList.propTypes = {
+  description: PropTypes.node,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      description: PropTypes.node.isRequired,
+      title: PropTypes.node.isRequired,
+    })
+  ),
+  onChange: PropTypes.func,
+  selected: ({ items, selected }, _, componentName) => {
+    if (!Number.isInteger(selected) || selected >= items.length) {
+      throw new Error(
+        `Invalid prop \`selected\` supplied to \`${componentName}\`. ` +
+          '`selected` must be an integer less than the length of `items`. ' +
+          `Given ${selected} instead.`
+      )
+    }
+  },
+  title: PropTypes.node,
+}
 
-const Description = styled(Text.Block)`
-  margin-bottom: ${2.5 * GU}px;
-`
-
-const StyledRadioGroup = styled(RadioGroup)`
-  display: flex;
-  flex-direction: column;
-`
+RadioList.defaultProps = {
+  items: [],
+  onChange: noop,
+  selected: 0,
+}
 
 export default RadioList
