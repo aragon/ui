3797950ad079f7511fe6c9db9fd31535e554cda1
Enrique Ortiz <hi@enriqueortiz.dev>
2019-11-25 15:03:04 -0400

    Gallery: solve DateRangePicker container bug + improve demo (#672)

--- gallery/src/pages/PageDateRangePicker.js
+++ gallery/src/pages/PageDateRangePicker.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DateRangePicker } from '@aragon/ui'
 
 import Page from 'comps/Page/Page'
@@ -6,14 +6,22 @@ import Container from '../components/Page/DemoContainer'
 import readme from 'ui-src/components/DateRangePicker/README.md'
 
 // eslint-disable-next-line react/prop-types
-const PageDateRangePicker = ({ title }) => (
-  <Page title={title} readme={readme}>
-    <Page.Demo opaque>
-      <Container>
-        <DateRangePicker />
-      </Container>
-    </Page.Demo>
-  </Page>
-)
+const PageDateRangePicker = ({ title }) => {
+  const [range, setRange] = useState({
+    start: null,
+    end: null,
+  })
+  return (
+    <Page title={title} readme={readme}>
+      <Page.Demo opaque container={false}>
+        <DateRangePicker
+          startDate={range.start}
+          endDate={range.end}
+          onChange={setRange}
+        />
+      </Page.Demo>
+    </Page>
+  )
+}
 
 export default PageDateRangePicker
