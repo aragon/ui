4c5eb3336692ad06ad59814b00ca2b4b5e1d4238
Pierre Bertet <hello@pierre.world>
2020-01-30 13:56:10 +0000

    Export modules in separate files for tree shaking

--- rollup.config.js
+++ rollup.config.js
@@ -1,62 +1,81 @@
+import { basename } from 'path'
 import url from '@rollup/plugin-url'
 import resolve from '@rollup/plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
-import filesize from 'rollup-plugin-filesize'
-import pkg from './package.json'
 import progress from 'rollup-plugin-progress'
-import sizes from 'rollup-plugin-sizes'
-import visualizer from 'rollup-plugin-visualizer'
+import analyze from 'rollup-plugin-analyzer'
+import glob from 'fast-glob'
 
 const production = !process.env.ROLLUP_WATCH
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.module,
-      format: 'esm',
-      preserveModules: true,
-      sourcemap: true,
-    },
-    // The CJS build is used by node tools we are using, like
-    // eslint-plugin-import.
-    {
-      file: pkg.main,
-      format: 'cjs',
-      preserveModules: false,
-      sourcemap: true,
-    },
-  ],
-  external: ['react', 'react-dom', 'styled-components'],
-  treeshake: production,
-  plugins: [
-    progress(),
-    production &&
-      filesize({
-        showMinifiedSize: true,
-        showGzippedSize: true,
-      }),
-    production && sizes(),
-    production && visualizer(),
-    url({
-      limit: 5 * 1024, // inline files smaller than 5k
-      publicPath: '',
-      include: [
-        '**/*.svg',
-        '**/*.png',
-        '**/*.jpg',
-        '**/*.gif',
-        '**/*.woff',
-        '**/*.woff2',
-      ],
-      emitFiles: true,
-    }),
-    babel({
-      runtimeHelpers: true,
-      exclude: 'node_modules/**',
-    }),
-    resolve(),
-    commonjs(),
-  ],
+async function getModules(dir, deep = 1) {
+  const files = await glob(
+    [`${dir}/**/*.js`, `!${dir}/**/index.js`, `!${dir}/**/*.test.js`],
+    { onlyFiles: true, deep }
+  )
+  return files.reduce(
+    (inputs, file) => ({ ...inputs, [basename(file, '.js')]: file }),
+    {}
+  )
 }
+
+export default (async () => {
+  return {
+    input: {
+      index: 'src/index.js',
+      components: 'src/components/index.js',
+      hooks: 'src/hooks/index.js',
+      providers: 'src/providers/index.js',
+      icons: 'src/icons/index.js',
+      themeLegacy: 'src/theme-legacy/index.js',
+      theme: 'src/theme/index.js',
+      utils: 'src/utils/index.js',
+      style: 'src/style/index.js',
+      ...(await getModules('src/components', 2)),
+      ...(await getModules('src/icons/components')),
+      ...(await getModules('src/utils')),
+    },
+    output: [
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        preserveModules: true,
+        sourcemap: true,
+      },
+      // The CJS build is used by node tools we are using, like
+      // eslint-plugin-import.
+      {
+        dir: 'dist',
+        format: 'cjs',
+        preserveModules: false,
+        sourcemap: true,
+      },
+    ],
+    external: ['react', 'react-dom', 'styled-components'],
+    treeshake: true,
+    plugins: [
+      process.env.ANALYZE && analyze(),
+      progress(),
+      url({
+        limit: 5 * 1024, // inline files smaller than 5k
+        publicPath: '',
+        include: [
+          '**/*.svg',
+          '**/*.png',
+          '**/*.jpg',
+          '**/*.gif',
+          '**/*.woff',
+          '**/*.woff2',
+        ],
+        emitFiles: true,
+      }),
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+      }),
+      resolve(),
+      commonjs(),
+    ],
+  }
+})()
