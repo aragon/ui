52641a6ba1e402666d4bf5719a05eaefa0da5263
StudioME - Left iMac <Elliott@valtechcreative.com>
2020-03-04 15:26:50 -0500

    Add number formatting utilities

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,3 +1,5 @@
+import { round } from './math';
+
 export const formatIntegerRange = (
   count = -1,
   min = 0,
@@ -12,4 +14,25 @@ export const formatIntegerRange = (
     return `${parseInt(max, 10)}${maxSuffix}`
   }
   return count.toString()
+}
+
+export function formatNumber(number) {
+  return new Intl.NumberFormat('en-US', {minimumFractionDigits: 0,}).format(number)
+}
+
+export function formatTokenAmount(
+  amount,
+  symbol,
+  decimals = 18,
+  rountToDecimals = 2,
+  isIncoming = true,
+  signDisplay = false
+) {
+  const number = round(
+    (amount / Math.pow(10, decimals)),
+    rountToDecimals
+  )
+  const formattedNumber = formatNumber(number)
+  const sign = signDisplay ? (isIncoming ? '+' : '-') : ''
+  return `${sign}${formattedNumber} ${symbol}`
 }
