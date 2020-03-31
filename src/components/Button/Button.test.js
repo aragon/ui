bba1edee21f963fc95697d941223f3c6ef0af07e
Pierre Bertet <hello@pierre.world>
2020-03-31 21:07:19 +0100

    Fix import

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 
-import { Button } from './Button'
+import Button from './Button'
 
 test('loads and displays greeting', async () => {
   const buttonProps = {}
