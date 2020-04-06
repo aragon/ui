f07352c01a9862648560fec5a4015f8f71e143fa
Pierre Bertet <hello@pierre.world>
2020-04-06 19:45:15 +0100

    Move to markdown-to-jsx
    It is lighter than react-markdown (5kb vs. 20kb), and doesn’t have the
    bundling issues that react-markdown has.
    
    Also:
    
    - Fix the theme colors.
    - Use Link.
    - Tweak NormalizedHtml styles.
    - Use aragonUI’s Checkbox.

--- src/components/Markdown/Markdown.js
+++ src/components/Markdown/Markdown.js
@@ -1,72 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ReactMarkdown from 'react-markdown'
-import NormalizedHtml, {
-  TASK_LIST_CLASSNAME,
-} from '../NormalizedHtml/NormalizedHtml'
+import MarkdownToJsx from 'markdown-to-jsx'
+import NormalizedHtml from '../NormalizedHtml/NormalizedHtml'
 import Link from '../Link/Link'
 import Checkbox from '../Input/Checkbox'
 
-const SafeLink = ({ children, ...props }) => (
-  <Link {...props} target="_blank">
-    {children}
-  </Link>
-)
+function CustomInput(props) {
+  props = { ...props }
+  if (props.type === 'checkbox') {
+    delete props.type
+    return <Checkbox {...props} />
+  }
+  return <input {...props} />
+}
+CustomInput.propTypes = { type: PropTypes.string }
 
-const ListItem = ({ checked, children, ...props }) => {
-  const checkbox = checked !== null
-  return (
-    <li className={checkbox ? TASK_LIST_CLASSNAME : ''}>
-      {checkbox ? <Checkbox checked={checked} /> : null}
-      {children}
-    </li>
+function Markdown({ allowHtml, content, unstyled }) {
+  const markdown = (
+    <MarkdownToJsx
+      options={{
+        disableParsingRawHTML: !allowHtml,
+        overrides: { a: Link, input: CustomInput },
+      }}
+    >
+      {content}
+    </MarkdownToJsx>
   )
+
+  return unstyled ? markdown : <NormalizedHtml>{markdown}</NormalizedHtml>
 }
 
-SafeLink.propTypes = {
-  children: PropTypes.node,
+Markdown.propTypes = {
+  allowHtml: PropTypes.bool,
+  content: PropTypes.string.isRequired,
+  unstyled: PropTypes.bool,
 }
 
-ListItem.propTypes = {
-  checked: PropTypes.bool,
-  children: PropTypes.node,
-}
-
-class Markdown extends React.Component {
-  static propTypes = {
-    reactMarkdownProps: PropTypes.shape({
-      ...ReactMarkdown.PropTypes,
-    }),
-    content: PropTypes.string.isRequired,
-    unstyled: PropTypes.bool,
-  }
-
-  static defaultProps = {
-    reactMarkdownProps: {
-      renderers: {
-        link: SafeLink,
-        listItem: ListItem,
-      },
-    },
-    unstyled: false,
-    content: '',
-  }
-
-  render() {
-    const { content, unstyled, reactMarkdownProps } = this.props
-
-    return (
-      <div>
-        {unstyled ? (
-          <ReactMarkdown source={content} {...reactMarkdownProps} />
-        ) : (
-          <NormalizedHtml>
-            <ReactMarkdown source={content} {...reactMarkdownProps} />
-          </NormalizedHtml>
-        )}
-      </div>
-    )
-  }
+Markdown.defaultProps = {
+  unstyled: false,
 }
 
 export default Markdown
