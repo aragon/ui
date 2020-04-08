9b528c0038ef9a40ff89943b66e3e97d9327dd45
Pierre Bertet <hello@pierre.world>
2020-04-08 12:24:40 +0100

    Export all the utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable prettier/prettier */
+export { default as cachedMap } from './cached-map'
 export { default as color } from './color'
 export * from './components'
 export * from './contains-component'
@@ -8,5 +8,7 @@ export * from './environment'
 export * from './font'
 export * from './format'
 export * from './keycodes'
+export * from './math'
 export * from './miscellaneous'
+export * from './url'
 export * from './web3'
