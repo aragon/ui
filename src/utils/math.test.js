43ce5377977c2c288b3a8a1c3d7cb2ac38fc7df8
Pierre Bertet <hello@pierre.world>
2020-04-14 11:24:16 +0100

    divideRoundBigInt(): add test that divides by something else than 10

--- src/utils/math.test.js
+++ src/utils/math.test.js
@@ -5,5 +5,6 @@ import { divideRoundBigInt } from './math'
 describe('divideRoundBigInt()', () => {
   test('should round the result', () => {
     expect(divideRoundBigInt(BigInt('479238'), BigInt('10'))).toEqual('47924')
+    expect(divideRoundBigInt(BigInt('479238'), BigInt('5'))).toEqual('95848')
   })
 })
