e88a74a600844d136ef376ab748e1ff0e524bf81
steven <stvtortora@gmail.com>
2020-05-23 22:31:33 -0400

    Update TextInput API

--- devbox/apps/Input.js
+++ devbox/apps/Input.js
@@ -27,25 +27,23 @@ function App() {
         </form>
         <TextInput
           css="width: 80px"
-          adornment="ETH"
-          adornmentPosition="end"
-          adornmentSettings={{
-            width: 55,
-            padding: 8,
+          adornment={{
+            end: 'ETH',
+            endWidth: 55,
+            endPadding: 8,
           }}
         />
         <TextInput
           css="width: 80px"
-          adornment="$"
-          adornmentPosition="start"
-          adornmentSettings={{
-            width: 36,
-            padding: 8,
+          adornment={{
+            start: '$',
+            startWidth: 36,
+            startPadding: 8,
           }}
         />
         <TextInput />
-        <TextInput adornment={<IconBlank />} adornmentPosition="start" />
-        <TextInput adornment={<IconBlank />} adornmentPosition="end" />
+        <TextInput adornment={{ start: <IconBlank /> }} />
+        <TextInput adornment={{ end: <IconBlank /> }} />
         <TextInput.Multiline />
         <SearchInput
           value={searchTerm}
