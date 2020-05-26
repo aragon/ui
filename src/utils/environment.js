31ed83eb5163881804fb93800f9738348d13be71
steven <stvtortora@gmail.com>
2020-05-25 23:29:06 -0400

    remove changes to environment.js

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -2,7 +2,6 @@ import { noop } from './miscellaneous'
 
 // Return a function that executes `cb` when on the dev environment
 export function devOnly(cb) {
-  return cb
   return process.env.NODE_ENV === 'development' ? cb : noop
 }
 
